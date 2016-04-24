e9afb4c10652a8249afa945b6f65301ee9ccfff4
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 16:14:15 +0100

    add mod & admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  get    'admin',                       :to => 'admin#index'
   get    'admin/settings',              :to => 'site_settings#index'
   get    'admin/settings/:id/edit',     :to => 'site_settings#edit'
   post   'admin/settings/:id/edit',     :to => 'site_settings#update'
   patch  'admin/settings/:id/edit',     :to => 'site_settings#update'
+
+  get    'mod',                         :to => 'moderator#index'
 
   get    'questions',                   :to => 'questions#index'
   get    'questions/:id',               :to => 'questions#show'
