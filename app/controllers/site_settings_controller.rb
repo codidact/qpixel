45b23c1a9390f0a85a1a9f29a462a4d985e377a2
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 12:44:09 +0100

    settings actions

--- app/controllers/site_settings_controller.rb
+++ app/controllers/site_settings_controller.rb
@@ -1,4 +1,22 @@
 class SiteSettingsController < ApplicationController
   before_action :authenticate_user!
   before_action :verify_admin
+
+  def index
+    @settings = SiteSetting.all
+  end
+
+  def edit
+    @setting = SiteSetting.find params[:id]
+  end
+
+  def update
+    @setting = SiteSetting.find params[:id]
+    @setting.update(setting_params)
+  end
+
+  private
+    def setting_params
+      params.require(:site_setting).permit(:name, :value)
+    end
 end
