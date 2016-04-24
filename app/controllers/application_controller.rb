4064294ebd357547c6b0bd4386d42194d6a06768
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 12:37:48 +0100

    user level verifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+    def verify_moderator
+      if !user_signed_in || !(user.is_moderator || user.is_admin)
+        raise ActionController::RoutingError.new('Not Found') and return
+      end
+    end
+
+    def verify_admin
+      if !user_signed_in || !user.is_admin
+        raise ActionController::RoutingError.new('Not Found') and return
+      end
+    end
 end
--- app/controllers/site_settings_controller.rb
+++ app/controllers/site_settings_controller.rb
@@ -1,2 +1,4 @@
 class SiteSettingsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :verify_admin
 end
