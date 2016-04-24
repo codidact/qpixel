cb8a8d8538b896de512c3c8cd986b8820a48b3b8
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 21:20:47 +0100

    voting logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,65 @@ $(document).on('ready page:load', function() {
 
   $(".vote-button").bind("click", function(ev) {
     ev.preventDefault();
-    var voteType = $(this).data("vote-type");
+    var self = $(this);
+
+    // Vote data is stored on the element as "VoteType/PostId/PostType/VoteId".
+    var vote = self.data("vote");
+    var voteSplat = vote.split("/");
+    var voteType = voteSplat[0],
+        postId = voteSplat[1],
+        postType = voteSplat[2],
+        voteId = voteSplat[3];
+
+    if(voteId > -1) {
+      // We've already voted; cancel the vote.
+      $.ajax({
+        'url': '/votes/' + voteId,
+        'type': 'DELETE',
+        'target': self
+      })
+      .done(function(data) {
+        if(data === "OK") {
+          $(this.target).attr('src', '/assets/' + (voteType == '0' ? 'up' : 'down') + '_clear.png');
+        }
+        else {
+          alert("Could not undo vote - please try again.");
+          console.error("Vote undo failed: " + data);
+        }
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        alert("Could not undo vote - please try again.");
+        console.error("Vote undo failed: status " + jqXHR.status);
+        console.log(jqXHR);
+      });
+    }
+    else {
+      // We have yet to vote, so cast one.
+      $.ajax({
+        'url': '/votes/new',
+        'type': 'POST',
+        'data': {
+          'post_type': postType,
+          'post_id': postId,
+          'vote_type': voteType
+        },
+        'target': self
+      })
+      .done(function(data) {
+        if(data === "OK") {
+          $(this.target).attr('src', '/assets/' + (voteType == '0' ? 'up' : 'down') + '_fill.png');
+        }
+        else {
+          alert("Could not cast vote - please try again.");
+          console.error("Vote cast failed: " + data);
+        }
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        alert("Could not cast vote - please try again.");
+        console.error("Vote cast failed: status " + jqXHR.status);
+        console.log(jqXHR);
+      });
+    }
   });
 
 });
