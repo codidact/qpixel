e85f5d31e49e5ed57070402cb159a27021e11531
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 15:20:32 +0100

    add patch route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get    'admin/settings',              :to => 'site_settings#index'
   get    'admin/settings/:id/edit',     :to => 'site_settings#edit'
   post   'admin/settings/:id/edit',     :to => 'site_settings#update'
+  patch  'admin/settings/:id/edit',     :to => 'site_settings#update'
 end
