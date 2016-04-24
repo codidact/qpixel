35dd2890d25a555b2081476f8c67c516e77e1ca3
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 18:31:38 +0100

    add answers to question view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,6 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find params[:id]
-    # @answers = Answer.where(:question_id => @question.id)
   end
 
   def tagged
