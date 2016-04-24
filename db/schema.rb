8d37dd09cc724273f7e62c6ce8224bf097171d87
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 17:46:12 +0000

    add devise views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424172323) do
+ActiveRecord::Schema.define(version: 20160424172807) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "body"
@@ -19,9 +19,11 @@ ActiveRecord::Schema.define(version: 20160424172323) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "question_id"
+    t.integer  "user_id"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
