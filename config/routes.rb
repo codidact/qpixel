0d78d1b9287698356753556dca6a2939eb3ef353
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 19:26:52 +0100

    use the custom controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'users/sessions' }
 
   get    'admin',                       :to => 'admin#index'
   get    'admin/settings',              :to => 'site_settings#index'
