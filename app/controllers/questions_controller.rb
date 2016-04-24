5fc8e969688946352a1506dbfc347b978e751d4e
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 17:39:06 +0100

    add tagged view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,7 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find params[:id]
+    # @answers = Answer.where(:question_id => @question.id)
   end
 
   def tagged
