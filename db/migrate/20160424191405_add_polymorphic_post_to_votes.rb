86718f9a5f3b122e7dd1e23fa1829b7652f5ed79
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 20:18:36 +0100

    no foreign keys on polys

--- db/migrate/20160424191405_add_polymorphic_post_to_votes.rb
+++ db/migrate/20160424191405_add_polymorphic_post_to_votes.rb
@@ -1,5 +1,5 @@
 class AddPolymorphicPostToVotes < ActiveRecord::Migration
   def change
-    add_reference :votes, :post, polymorphic: true, index: true, foreign_key: true
+    add_reference :votes, :post, polymorphic: true, index: true
   end
 end
