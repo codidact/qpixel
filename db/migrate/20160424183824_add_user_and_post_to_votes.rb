320acd015acd750d4048b273222de42b6ac08438
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 20:11:03 +0100

    revert migration mod

--- db/migrate/20160424183824_add_user_and_post_to_votes.rb
+++ db/migrate/20160424183824_add_user_and_post_to_votes.rb
@@ -1,5 +1,6 @@
 class AddUserAndPostToVotes < ActiveRecord::Migration
   def change
     add_reference :votes, :user, index: true, foreign_key: true
+    add_reference :votes, :post, index: true, foreign_key: true
   end
 end
