74eff28504f5a6965d75084a1dd6c0e5ab1d575c
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 15:58:49 +0100

    add actions & index view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,9 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all.paginate(:page => params[:page], :per_page => 50)
+  end
+
+  def show
+    @question = Question.find params[:id]
+  end
 end
