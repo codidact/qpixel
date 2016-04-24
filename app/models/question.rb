fcc16004eb4b89bb2c8d55949dd567d7061af373
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 15:39:58 +0100

    add associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
+  belongs_to :user
   serialize :tags, Array
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :questions
 end
