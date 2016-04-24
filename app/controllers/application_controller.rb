a9b62559a2058e8c027838ad47d78250d5438e53
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 18:52:24 +0100

    add username to permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username) }
+    end
+
     def verify_moderator
       if !user_signed_in? || !(current_user.is_moderator || current_user.is_admin)
         raise ActionController::RoutingError.new('Not Found') and return
