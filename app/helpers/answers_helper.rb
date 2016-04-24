7b47a9bfe15e9b4d5afea2e98893f7eb5d0d8ad0
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 20:44:25 +0100

    use integer types

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,4 +1,5 @@
 module AnswersHelper
+  # Returns the entire vote object. Vote type 0 is up, 1 is down.
   def my_vote(answer)
     if user_signed_in?
       return answer.votes.where(:user => current_user).first
