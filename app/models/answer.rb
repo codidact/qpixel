d8258ad0a7ffdd080c1d071271177dad636d052c
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 18:28:57 +0100

    add associations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,4 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :questions
+  has_many :answers
 end
