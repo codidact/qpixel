e21112050900b3c7652aff745e36aa4c057a4910
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 13:22:51 +0100

    copy helper to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,13 @@ class ApplicationController < ActionController::Base
         raise ActionController::RoutingError.new('Not Found') and return
       end
     end
+
+    def get_setting(name)
+      begin
+        setting = SiteSetting.find_by_name name
+        return setting.value
+      rescue
+        raise ActionController::RoutingError.new('Internal Server Error')
+      end
+    end
 end
