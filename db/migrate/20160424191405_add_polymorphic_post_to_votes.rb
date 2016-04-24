2c61e7c680f9c98d4b43b48bafd855d31df83f0e
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-24 19:14:14 +0000

    add polymorphic association

---
+++ db/migrate/20160424191405_add_polymorphic_post_to_votes.rb
@@ -0,0 +1,4 @@
+class AddPolymorphicPostToVotes < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424183824) do
+ActiveRecord::Schema.define(version: 20160424191151) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "body"
@@ -71,10 +71,8 @@ ActiveRecord::Schema.define(version: 20160424183824) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.integer  "post_id"
   end
 
-  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
   add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
