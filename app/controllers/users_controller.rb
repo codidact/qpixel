31c6f75a9bdde362d20050294373c95acd0470ba
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-25 18:40:58 +0100

    add order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.paginate(:page => params[:page], :per_page => 50)
+    @users = User.all.paginate(:page => params[:page], :per_page => 50, :order => params[:order])
   end
 
   def show
