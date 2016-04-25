6eff0c4519d3899d8b142007fb717d39a5fa3f39
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-25 18:36:10 +0100

    fill user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all.paginate(:page => params[:page], :per_page => 50)
   end
 
   def show
+    @user = User.find params[:id]
   end
 end
