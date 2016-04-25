2ea8eccef1aa386fed85803d2fa6b25ef5e8c4e6
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-25 22:42:11 +0100

    add default site settings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+default_settings = [
+  [ 'SiteName', 'QPixel' ],
+  [ 'SiteLogoPath', '/assets/logo.png' ],
+  [ 'QuestionUpVoteRep', '5' ],
+  [ 'QuestionDownVoteRep', '-2' ],
+  [ 'AnswerUpVoteRep', '10' ],
+  [ 'AnswerDownVoteRep', '-2' ],
+  [ 'AllowSelfVotes', 'false' ]
+]
+
+default_settings.each do |name, value|
+  SiteSetting.create(name: name, value: value)
+end
