1b19a5a82f2a3df9e8497c0df7f5d87d9b9fe6b4
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-27 19:51:02 +0100

    annotate and validate answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,17 @@
 # Represents one answer. Answers are attached to both a question and a user account; have a score, and can be voted on.
 class Answer < ActiveRecord::Base
+  # Attached to a question, otherwise it'll never display anywhere.
   belongs_to :question
+
+  # Attached to a user account.
   belongs_to :user
 
+  # Can be voted on as a Post.
   has_many :votes, :as => :post
+
+  # Can't be having answers without bodies.
+  validates :body, :presence => true
+
+  # 1-character answers aren't really answers, so let's set some limits.
+  validates :body, length: { minimum: 30, maximum: 30000 }
 end
