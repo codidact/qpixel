802af2fecaa0fafa5836c07e9e6ea39bff7f8d49
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-27 19:45:10 +0100

    validate tag length

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,6 +11,7 @@ class Question < ActiveRecord::Base
   validates :title, length: { minimum: 15, maximum: 255 }
   validates :body, length: { minimum: 30, maximum: 30000 }
   validate :maximum_tags
+  validate :maximum_tag_length
 
   private
     # Restricts the number of tags on any question to a maximum of 5, and a minimum of 1. This can't be achieved by any
@@ -20,6 +21,16 @@ class Question < ActiveRecord::Base
         errors.add(:tags, "can't have more than 5 tags")
       elsif tags.length < 1
         errors.add(:tags, "must have at least one tag")
+      end
+    end
+
+    # Restricts the length of a single tag to 20 characters. This avoids styling issues with the
+    # <tt>questions/question</tt> partial. Again, can't be done by regular validation helpers.
+    def maximum_tag_length
+      tags.each do |tag|
+        if tag.length > 20
+          errors.add(:tags, "can't be more than 20 characters long each")
+        end
       end
     end
 end
