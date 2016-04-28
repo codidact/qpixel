af6887e0ae4d2352ea56cc397f39e9d42a526de3
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-28 21:33:08 +0100

    only show undeleted questions by default

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,8 @@
 # Represents a question. Questions are attached to a user account, and have many answers, a score, and can be voted on.
 class Question < ActiveRecord::Base
+  # By default, we only want to display non-deleted questions.
+  default_scope { where(is_deleted: false) }
+
   # Attached to a user account.
   belongs_to :user
 
