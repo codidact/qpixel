b884723294c45fa141e9dba09265b6ed015bc1ec
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-29 16:23:55 +0100

    route file comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
+  # Offload user control onto Devise - doing that once was enough for me.
   devise_for :users, :controllers => { :registrations => 'users/registrations' }
 
+  # We can't have the default Rails welcome page, so let's just have a questions index as the front page.
   root                                  :to => 'questions#index'
 
+  # Admins are important, let's make sure their routes override anything else.
   get    'admin',                       :to => 'admin#index'
   get    'admin/settings',              :to => 'site_settings#index'
   get    'admin/settings/:id/edit',     :to => 'site_settings#edit'
   post   'admin/settings/:id/edit',     :to => 'site_settings#update'
   patch  'admin/settings/:id/edit',     :to => 'site_settings#update'
 
+  # Mods are also pretty important, I guess.
   get    'mod',                         :to => 'moderator#index'
 
+  # Questions have a lot of actions...
   get    'questions',                   :to => 'questions#index'
   get    'questions/ask',               :to => 'questions#new'
   post   'questions/ask',               :to => 'questions#create'
@@ -22,12 +27,15 @@ Rails.application.routes.draw do
   delete 'questions/:id/delete',        :to => 'questions#destroy'
   patch  'questions/:id/delete',        :to => 'questions#undelete'
 
+  # Most of the users stuff is Devised, but it doesn't provide an index or profile.
   get    'users',                       :to => 'users#index'
   get    'users/:id',                   :to => 'users#show'
 
+  # Surprisingly few routes for voting, considering its complexity.
   post   'votes/new',                   :to => 'votes#create'
   delete 'votes/:id',                   :to => 'votes#destroy'
 
+  # Answers don't have quite as many as questions.
   get    'questions/:id/answer',        :to => 'answers#new'
   post   'questions/:id/answer',        :to => 'answers#create'
   get    'answers/:id/edit',            :to => 'answers#edit'
@@ -36,6 +44,7 @@ Rails.application.routes.draw do
   delete 'answers/:id/delete',          :to => 'answers#delete'
   patch  'answers/:id/delete',          :to => 'answers#undelete'
 
+  # Nobody likes errors. Relegate them way down here.
   match  '/403',                        :to => 'errors#forbidden',                :via => :all
   match  '/404',                        :to => 'errors#not_found',                :via => :all
   match  '/409',                        :to => 'errors#conflict',                 :via => :all
