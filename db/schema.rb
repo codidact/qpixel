ead905126374b653aeed0d85d6c9f287d5ede6c5
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-30 08:48:13 +0000

    add notifications controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160429150239) do
+ActiveRecord::Schema.define(version: 20160429160144) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "body"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160429150239) do
     t.integer  "question_id"
     t.integer  "user_id"
     t.boolean  "is_deleted"
+    t.datetime "deleted_at"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id"
@@ -45,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160429150239) do
     t.datetime "updated_at",                 null: false
     t.integer  "user_id"
     t.boolean  "is_deleted", default: false
+    t.datetime "deleted_at"
   end
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id"
