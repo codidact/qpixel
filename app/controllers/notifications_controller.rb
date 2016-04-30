29151469675f932ee86bc31ab7555cec95dbf9bc
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-04-30 09:53:56 +0100

    add basic notifications actions

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,19 @@
+# Provides web and API actions relating to user notifications.
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!, :only => [:index]
+
+  # Authenticated web/API action. Retrieves a list of active notifications for the current user.
+  def index
+
+  end
+
+  # Authenticated web/API action. Marks a single notification as read.
+  def read
+
+  end
+
+  # Authenticated web/API action. Marks all the current user's unread notifications as read.
+  def read_all
+
+  end
 end
