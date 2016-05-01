bdbf1b76f9fb39ee39e5cbeafc798e321733cec8
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-01 11:29:24 +0100

    add remaining comment actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 # Provides mainly API actions for using and making comments.
 class CommentsController < ApplicationController
   before_action :auth_for_commenting
+  before_action :set_comment, :only => [:update, :destroy, :undelete]
 
   # Authenticated API action. Creates a comment based on the data passed.
   def create
@@ -9,6 +10,35 @@ class CommentsController < ApplicationController
       render :json => { :status => 'success' }, :status => 201
     else
       render :json => { :status => 'failed', :message => 'Comment failed to save.' }, :status => 500
+    end
+  end
+
+  # Authenticated API action. Updates an existing comment with new data, based on the parameters passed to the request.
+  def update
+    if @comment.update comment_params
+      render :json => { :status => 'success' }
+    else
+      render :json => { :status => 'failed', :message => 'Comment failed to update.' }, :status => 500
+    end
+  end
+
+  # Authenticated API action. Deletes a comment by setting the <tt>is_deleted</tt> field to true.
+  def destroy
+    @comment.is_deleted = true
+    if @comment.save
+      render :json => { :status => 'success' }
+    else
+      render :json => { :status => 'failed', :message => 'Comment marked deleted, but unsaved - status unknown.' }, :status => 500
+    end
+  end
+
+  # Authenticated API action. Undeletes a comment by returning the <tt>is_deleted</tt> field to false.
+  def undelete
+    @comment.is_deleted = false
+    if @comment.save
+      render :json => { :status => 'success' }
+    else
+      render :json => { :status => 'failed', :message => 'Comment undeleted, but unsaved - status unknown.' }, :status => 500
     end
   end
 
@@ -26,5 +56,10 @@ class CommentsController < ApplicationController
     # Sanitizes parameters for use in creating or updating comments.
     def comment_params
       params.require(:comment).permit(:content)
+    end
+
+    # Finds the comment with the given ID and sets it to the <tt>@comment</tt> variable.
+    def set_comment
+      @comment = Comment.find params[:id]
     end
 end
