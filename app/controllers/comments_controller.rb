2cc24c9126e400b904c5480b58a20c0e73ad54d3
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-01 11:54:01 +0100

    convert to web actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,61 +1,87 @@
-# Provides mainly API actions for using and making comments.
+# Provides mainly web actions for using and making comments.
 class CommentsController < ApplicationController
-  before_action :auth_for_commenting
+  before_action :authenticate_user!
   before_action :set_comment, :only => [:update, :destroy, :undelete]
 
-  # Authenticated API action. Creates a comment based on the data passed.
+  # Authenticated web action. Creates a comment based on the data passed.
   def create
     @comment = Comment.new comment_params
     post = params[:post_type] == 'Question' ? Question.find(params[:post_id]) : Answer.find(params[:post_id])
     @comment.post = post
     @comment.user = current_user
     if @comment.save
-      render :json => { :status => 'success' }, :status => 201
+      if params[:post_type] == 'Question'
+        redirect_to url_for(:controller => :questions, :action => :show, :id => post.id)
+      else
+        redirect_to url_for(:controller => :questions, :action => :show, :id => post.question.id)
+      end
     else
-      render :json => { :status => 'failed', :message => 'Comment failed to save.' }, :status => 500
+      flash[:error] = "Comment failed to save."
+      if params[:post_type] == 'Question'
+        redirect_to url_for(:controller => :questions, :action => :show, :id => post.id)
+      else
+        redirect_to url_for(:controller => :questions, :action => :show, :id => post.question.id)
+      end
     end
   end
 
-  # Authenticated API action. Updates an existing comment with new data, based on the parameters passed to the request.
+  # Authenticated web action. Updates an existing comment with new data, based on the parameters passed to the request.
   def update
     if @comment.update comment_params
-      render :json => { :status => 'success' }
+      if @comment.post_type == 'Question'
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.id)
+      else
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.question.id)
+      end
     else
-      render :json => { :status => 'failed', :message => 'Comment failed to update.' }, :status => 500
+      flash[:error] = "Comment failed to update."
+      if @comment.post_type == 'Question'
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.id)
+      else
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.question.id)
+      end
     end
   end
 
-  # Authenticated API action. Deletes a comment by setting the <tt>is_deleted</tt> field to true.
+  # Authenticated web action. Deletes a comment by setting the <tt>is_deleted</tt> field to true.
   def destroy
     @comment.is_deleted = true
     if @comment.save
-      render :json => { :status => 'success' }
+      if @comment.post_type == 'Question'
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.id)
+      else
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.question.id)
+      end
     else
-      render :json => { :status => 'failed', :message => 'Comment marked deleted, but unsaved - status unknown.' }, :status => 500
+      flash[:error] = "Comment marked deleted, but not saved - status unknown."
+      if @comment.post_type == 'Question'
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.id)
+      else
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.question.id)
+      end
     end
   end
 
-  # Authenticated API action. Undeletes a comment by returning the <tt>is_deleted</tt> field to false.
+  # Authenticated web action. Undeletes a comment by returning the <tt>is_deleted</tt> field to false.
   def undelete
     @comment.is_deleted = false
     if @comment.save
-      render :json => { :status => 'success' }
+      if @comment.post_type == 'Question'
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.id)
+      else
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.question.id)
+      end
     else
-      render :json => { :status => 'failed', :message => 'Comment undeleted, but unsaved - status unknown.' }, :status => 500
+      flash[:error] = "Comment marked undeleted, but not saved - status unknown."
+      if @comment.post_type == 'Question'
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.id)
+      else
+        redirect_to url_for(:controller => :questions, :action => :show, :id => @comment.post.question.id)
+      end
     end
   end
 
   private
-    # Ensures users are authenticated before being able to comment. This doesn't use the standard
-    # <tt>:authenticate_user!</tt> because that redirects if the user isn't logged in, which isn't the behaviour we
-    # want for comments.
-    def auth_for_commenting
-      unless user_logged_in?
-        render :json => { :status => 'failed', :message => 'You must be logged in to comment.' }, :status => 403
-        return
-      end
-    end
-
     # Sanitizes parameters for use in creating or updating comments.
     def comment_params
       params.require(:comment).permit(:content)
