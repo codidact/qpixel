90b0b13cda5068210a59b039c6427c19950c72a4
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-01 16:10:40 +0100

    add associations

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,2 +1,7 @@
+# Represents a flag. Flags are attached to both a user and a post, and have a single status.
 class Flag < ActiveRecord::Base
+  belongs_to :post, :polymorphic => true
+  belongs_to :user
+
+  has_one :flag_status
 end
--- app/models/flag_status.rb
+++ app/models/flag_status.rb
@@ -1,2 +1,4 @@
+# Represents a flag status. Flag statuses are attached to a flag.
 class FlagStatus < ActiveRecord::Base
+  belongs_to :flag
 end
