9a9997fcf5cef3c9c2655e72cf25b0074f2def6a
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-01 13:16:30 +0100

    add default scope

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 # Represents a comment. Comments are attached to both a post and a user.
 class Comment < ActiveRecord::Base
+  default_scope { where(is_deleted: false) }
+
   belongs_to :post, :polymorphic => true
   belongs_to :user
 end
