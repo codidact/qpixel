552cfe3c7a8fd07843481e5dfaf8a94894104624
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-01 17:14:29 +0100

    add queue & comments

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -3,10 +3,18 @@ class FlagsController < ApplicationController
   before_action :authenticate_user!
   before_action :verify_moderator, :only => [:resolve]
 
+  # Authenticated API action. Creates a new flag with a reason, assigns it a post and a user, and puts it in the queue.
   def new
 
   end
 
+  # Administrative web action. Provides a 'queue' of flags - i.e. a page containing any unresolved flags.
+  def queue
+
+  end
+
+  # Administrative API action. Provides a route for moderators and administrators to resolve flags - that is, apply a
+  # status to them.
   def resolve
 
   end
