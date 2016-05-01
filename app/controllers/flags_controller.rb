3fd5baa7f35d1d0fa75bacda1f1d449de0efdefa
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-01 17:11:53 +0100

    add base actions

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -1,2 +1,13 @@
+# Provides web and API actions that relate to flagging.
 class FlagsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :verify_moderator, :only => [:resolve]
+
+  def new
+
+  end
+
+  def resolve
+
+  end
 end
