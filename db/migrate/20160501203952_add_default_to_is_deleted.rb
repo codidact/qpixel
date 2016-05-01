3f3117dff7695b3657754c097a0903bd3ae3e9be
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-01 21:42:41 +0100

    fill in migration

--- db/migrate/20160501203952_add_default_to_is_deleted.rb
+++ db/migrate/20160501203952_add_default_to_is_deleted.rb
@@ -1,4 +1,6 @@
 class AddDefaultToIsDeleted < ActiveRecord::Migration
   def change
+    change_column :answers, :is_deleted, :boolean, :default => false
+    change_column :comments, :is_deleted, :boolean, :default => false
   end
 end
