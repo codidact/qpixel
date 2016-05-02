72c8709de815810f97c16fc03b9ebf13b0f150aa
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-02 16:43:41 +0000

    add generated docs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501150425) do
+ActiveRecord::Schema.define(version: 20160501203952) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "body"
