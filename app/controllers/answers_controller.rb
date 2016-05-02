86d0ec5ba73bf8aca3b623f1035f8982f5c11f10
quartata <quartata0@gmail.com>
2016-05-01 17:43:21 -0700

    Update answers_controller.rb

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,3 @@
-require 'redcarpet'
-
 # Web controller. Provides actions that relate to answers. Pretty much the standard set of resources, really - it's
 # questions that have a few more actions.
 class AnswersController < ApplicationController
