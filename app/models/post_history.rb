747a2ec240df1e1d556a849e8869fe3b52515564
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-07 15:02:52 +0100

    require user

--- app/models/post_history.rb
+++ app/models/post_history.rb
@@ -3,43 +3,43 @@ class PostHistory < ActiveRecord::Base
   has_one :user
   belongs_to :post, :polymorphic => true
 
-  def self.question_edited(question, user=nil)
+  def self.question_edited(question, user)
     self.new_question_event('Edit', question, user)
   end
 
-  def self.question_deleted(question, user=nil)
+  def self.question_deleted(question, user)
     self.new_question_event('Delete', question, user)
   end
 
-  def self.question_undeleted(question, user=nil)
+  def self.question_undeleted(question, user)
     self.new_question_event('Undelete', question, user)
   end
 
-  def self.answer_edited(answer, user=nil)
+  def self.answer_edited(answer, user)
     self.new_answer_event('Edit', answer, user)
   end
 
-  def self.answer_deleted(answer, user=nil)
+  def self.answer_deleted(answer, user)
     self.new_answer_event('Delete', answer, user)
   end
 
-  def self.answer_undeleted(answer, user=nil)
+  def self.answer_undeleted(answer, user)
     self.new_answer_event('Undelete', answer, user)
   end
 
   private
-    def self.new_question_event(type_name, question, user=nil)
+    def self.new_question_event(type_name, question, user)
       event = PostHistory.new
       event.post_history_type = PostHistoryType.find_by_name type_name
-      event.user = user || (current_user if user_signed_in?)
+      event.user = user
       event.post = question
       event.save!
     end
 
-    def self.new_answer_event(type_name, answer, user=nil)
+    def self.new_answer_event(type_name, answer, user)
       event = PostHistory.new
       event.post_history_type = PostHistoryType.find_by_name type_name
-      event.user = user || (current_user if user_signed_in?)
+      event.user = user
       event.post = answer
       event.save!
     end
