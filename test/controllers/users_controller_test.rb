82f9e294153bea231e0c5fdc3b7b96d6c6cf9ebb
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-10 15:28:37 +0100

    why did those tests even exist?

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
-
+  
 end
