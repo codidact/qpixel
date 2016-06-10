a4c49dc8d89ee4994665f6815e91be988d273612
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-10 18:03:17 +0100

    admin controller tests

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  test "should get index" do
+    sign_in users(:admin)
+    get :index
+    assert_response :success
+  end
+
+  test "should deny anonymous users access" do
+    sign_out :user
+    AdminController.action_methods.each do |path|
+      assert_raises ActionController::RoutingError do
+        get path
+      end
+    end
+  end
+
+  test "should deny standard users access" do
+    sign_in users(:standar_user)
+    AdminController.action_methods.each do |path|
+      assert_raises ActionController::RoutingError do
+        get path
+      end
+    end
+  end
+
+  test "should deny editors access" do
+    sign_in users(:editor)
+    AdminController.action_methods.each do |path|
+      assert_raises ActionController::RoutingError do
+        get path
+      end
+    end
+  end
+
+  test "should deny deleters access" do
+    sign_in users(:deleter)
+    AdminController.action_methods.each do |path|
+      assert_raises ActionController::RoutingError do
+        get path
+      end
+    end
+  end
+
+  test "should deny moderators access" do
+    sign_in users(:moderator)
+    AdminController.action_methods.each do |path|
+      assert_raises ActionController::RoutingError do
+        get path
+      end
+    end
+  end
 end
