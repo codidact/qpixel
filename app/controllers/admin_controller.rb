2364752e0ad291602e6b7d73e9aa584ee50ccbcd
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-10 20:40:38 +0100

    fix security bug

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,7 @@
 # Web controller. Provides authenticated actions for use by administrators.
 class AdminController < ApplicationController
+  before_action :verify_admin
+
   # Administrative web action. No dynamic content - this is purely representative of the existence of a (relatively)
   # static view for this path.
   def index
