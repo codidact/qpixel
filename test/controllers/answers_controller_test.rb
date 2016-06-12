1242448080e9e46fbf03d96b6edb3e4712d252f2
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-12 15:12:24 +0100

    sign in an authorized user first

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -4,6 +4,7 @@ class AnswersControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
   test "should get new answer page" do
+    sign_in users(:standard_user)
     get :new, :id => questions(:one).id
     assert_response(200)
     assert_equal assigns(:answer), Answer.new
