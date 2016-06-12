047ef836c62695a82f43d3e02a5ce5ebc356fada
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-12 14:55:02 +0100

    fixtures and initial test

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AnswersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new answer page" do
+    get :new, :id => questions(:one).id
+    assert_not_nil assigns(:answer)
+    assert_not_nil assigns(:question)
+    assert_response(200)
+  end
 end
