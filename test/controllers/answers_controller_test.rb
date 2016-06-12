21286dd61b4f9c75c4b9ad9d13bdf926e7f669ce
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-12 15:14:20 +0100

    back to not nil

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -7,7 +7,7 @@ class AnswersControllerTest < ActionController::TestCase
     sign_in users(:standard_user)
     get :new, :id => questions(:one).id
     assert_response(200)
-    assert_equal assigns(:answer), Answer.new
+    assert_not_nil assigns(:answer)
     assert_not_nil assigns(:question)
   end
 end
