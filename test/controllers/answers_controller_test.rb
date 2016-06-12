8c6acbf18f8a95b18862af7a2ee841fa1c1ecff9
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-12 15:04:49 +0100

    tell tests what to expect

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -5,7 +5,7 @@ class AnswersControllerTest < ActionController::TestCase
 
   test "should get new answer page" do
     get :new, :id => questions(:one).id
-    assert_not_nil assigns(:answer)
+    assert_equal assigns(:answer), Answer.new
     assert_not_nil assigns(:question)
     assert_response(200)
   end
