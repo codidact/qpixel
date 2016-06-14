69e227b664ba7a9a4b9919802e1d419592d4caa5
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-14 19:20:40 +0000

    actually fix seeding

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,14 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-Rails.application.load_seed
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  setup :load_seeds
+
+  protected
+    def load_seeds
+      Rails.application.load_seed
+    end
 end
