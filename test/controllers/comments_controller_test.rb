9adec2ec53c16d8c6d3dc9cff32a78a7088b6252
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-14 21:12:16 +0100

    include Devise helpers for comments tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   test "should create new comment" do
     sign_in users(:standard_user)
     post :create, :comment => { :post_id => answers(:two).id, :post_type => 'Answer', :content => "ABCDEF GHIJKL MNOPQR STUVWX YZ" }
