67edd4c9d64eb3ca5b65db768a34fedd9e4caa4f
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-15 21:23:44 +0100

    bring tests in line with design

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -12,45 +12,40 @@ class AdminControllerTest < ActionController::TestCase
   test "should deny anonymous users access" do
     sign_out :user
     AdminController.action_methods.each do |path|
-      assert_raises ActionController::RoutingError do
-        get path
-      end
+      get path
+      assert_response(302)
     end
   end
 
   test "should deny standard users access" do
     sign_in users(:standard_user)
     AdminController.action_methods.each do |path|
-      assert_raises ActionController::RoutingError do
-        get path
-      end
+      get path
+      assert_response(404)
     end
   end
 
   test "should deny editors access" do
     sign_in users(:editor)
     AdminController.action_methods.each do |path|
-      assert_raises ActionController::RoutingError do
-        get path
-      end
+      get path
+      assert_response(404)
     end
   end
 
   test "should deny deleters access" do
     sign_in users(:deleter)
     AdminController.action_methods.each do |path|
-      assert_raises ActionController::RoutingError do
-        get path
-      end
+      get path
+      assert_response(404)
     end
   end
 
   test "should deny moderators access" do
     sign_in users(:moderator)
     AdminController.action_methods.each do |path|
-      assert_raises ActionController::RoutingError do
-        get path
-      end
+      get path
+      assert_response(404)
     end
   end
 end
