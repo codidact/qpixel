490ae0d2f5205330b86068319ee177e202beeaeb
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-15 13:28:39 +0100

    assert post existence

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -7,6 +7,7 @@ class CommentsControllerTest < ActionController::TestCase
     sign_in users(:standard_user)
     post :create, :comment => { :post_id => answers(:two).id, :post_type => 'Answer', :content => "ABCDEF GHIJKL MNOPQR STUVWX YZ" }
     assert_not_nil assigns(:comment)
+    assert_not_nil assigns(:comment).post
     assert_response(302)
   end
 
@@ -14,6 +15,7 @@ class CommentsControllerTest < ActionController::TestCase
     sign_in users(:standard_user)
     patch :update, :id => comments(:one).id, :comment => { :content => "ABCDEF GHIJKL MNOPQR STUVWX YZ" }
     assert_not_nil assigns(:comment)
+    assert_not_nil assigns(:comment).post
     assert_response(302)
   end
 
@@ -21,6 +23,7 @@ class CommentsControllerTest < ActionController::TestCase
     sign_in users(:moderator)
     delete :destroy, :id => comments(:one).id
     assert_not_nil assigns(:comment)
+    assert_not_nil assigns(:comment).post
     assert_equal true, assigns(:comment).is_deleted
     assert_response(302)
   end
@@ -29,6 +32,7 @@ class CommentsControllerTest < ActionController::TestCase
     sign_in users(:moderator)
     delete :undelete, :id => comments(:one).id
     assert_not_nil assigns(:comment)
+    assert_not_nil assigns(:comment).post
     assert_equal false, assigns(:comment).is_deleted
     assert_response(302)
   end
