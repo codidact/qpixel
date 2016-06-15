b205c24d3cff5fe8068a7211091c424d7313093b
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-15 18:58:28 +0100

    add defaults

--- db/migrate/20160615175537_add_defaults_to_deleted.rb
+++ db/migrate/20160615175537_add_defaults_to_deleted.rb
@@ -1,4 +1,7 @@
 class AddDefaultsToDeleted < ActiveRecord::Migration
   def change
+    change_column :answers, :is_deleted, :boolean, default: false
+    change_column :questions, :is_deleted, :boolean, default: false
+    change_column :comments, :is_deleted, :boolean, default: false
   end
 end
