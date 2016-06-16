2330f3aeb6a78cb11fb2348311f30d2307a35e11
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-16 21:23:51 +0100

    users controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  
+  include Devise::TestHelpers
+
+  test "should get index" do
+    get :index
+    assert_not_nil assigns(:users)
+    assert_response(200)
+  end
+
+  test "should get show user page" do
+    get :show, :id => users(:standard_user).id
+    assert_not_nil assigns(:user)
+    assert_response(200)
+  end
 end
