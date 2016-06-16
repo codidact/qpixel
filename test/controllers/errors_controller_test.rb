3d3fce02cf9f901a2905dab9a7023781ba3f4127
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-16 22:11:48 +0100

    errors controller tests

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ErrorsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  test "should provide 404 response" do
+    get :not_found
+    assert_response(404)
+  end
+
+  test "should provide 403 response" do
+    get :forbidden
+    assert_response(403)
+  end
+
+  test "should provide 409 response" do
+    get :conflict
+    assert_response(409)
+  end
+
+  test "should provide 422 response" do
+    get :unprocessable_entity
+    assert_response(422)
+  end
+
+  test "should provide 500 response" do
+    get :internal_server_error
+    assert_response(500)
+  end
 end
