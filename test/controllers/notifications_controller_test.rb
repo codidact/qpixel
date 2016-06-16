bf2ce9fa128cf44d44a500de572db418890ad5de
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-16 15:30:08 +0100

    start notifications tests

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class NotificationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  test "should get index as JSON" do
+    sign_in users(:standard_user)
+    get :index, :format => :json
+    assert_not_nil assigns(:notifications)
+    assert_response(200)
+  end
 end
