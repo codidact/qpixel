11fae718409cf943e8a61ffee06bb53382e537e3
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-17 17:28:32 +0100

    add users_registrations controller tests

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 # defaults before completing actions.
 class Users::RegistrationsController < Devise::RegistrationsController
   # Extends the Devise default RegistrationsController#create. Additionally initializes a reputation score
-  # (defaults to 0) for the newly-created user.
+  # (defaults to 1) for the newly-created user.
   def create
     super
     @user.reputation = 1
---
+++ test/controllers/users/registrations_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UsersRegistrationsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  test "should initialize user repuatation to 1 on create" do
+    sign_out :user
+    post :create, :user => { :email => "new@qpixel-test.net", :password => "ABCDEFGH", :password_confirmation => "ABCDEFGH", :username => "ABCDEF" }
+    assert_not_nil assigns(:user)
+    assert_equal 1, assigns(:user).reputation
+    assert_response(302)
+  end
+end
