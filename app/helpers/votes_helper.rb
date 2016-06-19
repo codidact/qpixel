764c121466151c78bec37f54047e3cfd3c5308ba
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-19 18:21:39 +0100

    move vote fraud method

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,19 +1,3 @@
 # Provides helper methods for use by views under <tt>VotesController</tt>.
 module VotesHelper
-  def self.check_for_vote_fraud
-    User.all.each do |u|
-      votes = u.votes.group(:recv_user).count(:recv_user)
-      total = u.votes.count
-      votes.each do |recv_id, cnt|
-        if (total / cnt**2) < 0.5 && recv_id != -1
-          sv = SuspiciousVotes.new
-          sv.from_user = u.id
-          sv.to_user = recv_id
-          sv.suspicious_count = cnt
-          sv.total_count = total
-          sv.save
-        end
-      end
-    end
-  end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
 every 1.day do
-  runner 'VotesHelper.check_for_vote_fraud'
+  runner 'Vote.check_for_vote_fraud'
 end
