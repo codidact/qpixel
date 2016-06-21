a3c6e2fbfc4861775df011362cfde19ba6ff721e
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-21 11:16:42 +0100

    fix logic bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,10 +48,11 @@ class UsersController < ApplicationController
       if needs_transfer.include?(assoc)
         objects.each do |obj|
           # Keep posts that score above 0 (but transfer them to @transfer_user), destroy the rest.
-          unless obj.respond_to?(:score) && obj.score >= 0
+          if obj.respond_to?(:score) && obj.score < 0
             obj.destroy
           else
             obj.user_id = @transfer_user.id
+            obj.save
           end
         end
       else
