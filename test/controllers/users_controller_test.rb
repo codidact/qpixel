e51cb1237635e9a9da42cc2c4e62ee513485fb21
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-21 10:52:54 +0100

    user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,4 +43,48 @@ class UsersControllerTest < ActionController::TestCase
     assert_nil assigns(:user)
     assert_response(404)
   end
+
+  test "should destroy user" do
+    sign_in users(:moderator)
+    delete :destroy, :id => users(:standard_user).id
+    assert_not_nil assigns(:user)
+    assert_equal 'success', JSON.parse(response.body)['status']
+    assert_response(200)
+  end
+
+  test "should require authentication to destroy user" do
+    sign_out :user
+    delete :destroy, :id => users(:standard_user).id
+    assert_nil assigns(:user)
+    assert_response(404)
+  end
+
+  test "should require moderator status to destroy user" do
+    sign_in users(:standard_user)
+    delete :destroy, :id => users(:standard_user).id
+    assert_nil assigns(:user)
+    assert_response(404)
+  end
+
+  test "should soft-delete user" do
+    sign_in users(:admin)
+    delete :soft_delete, :id => users(:standard_user).id
+    assert_not_nil assigns(:user)
+    assert_equal 'success', JSON.parse(response.body)['status']
+    assert_response(200)
+  end
+
+  test "should require authentication to soft-delete user" do
+    sign_out :user
+    delete :soft_delete, :id => users(:standard_user).id
+    assert_nil assigns(:user)
+    assert_response(404)
+  end
+
+  test "should require admin status to soft-delete user" do
+    sign_in users(:standard_user)
+    delete :soft_delete, :id => users(:standard_user).id
+    assert_nil assigns(:user)
+    assert_response(404)
+  end
 end
