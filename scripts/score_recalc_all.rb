7e9ed1beb03450f8fb2fb8e7aba90a4384d74981
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-06-21 11:43:44 +0100

    log IDs

--- scripts/score_recalc_all.rb
+++ scripts/score_recalc_all.rb
@@ -1,9 +1,11 @@
 Question.all.each do |q|
+  puts q.id
   q.score = q.votes.sum(:vote_type)
   q.save!
 end
 
 Answer.all.each do |a|
+  puts a.question.id
   a.score = a.votes.sum(:vote_type)
   a.save!
 end
