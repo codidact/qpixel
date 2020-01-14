9fc627c0869d2c9fe7e5825ddeb2df651b03c8f5
ArtOfCode- <hello@artofcode.co.uk>
2020-01-14 22:27:29 +0000

    Change sender email address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'notifications@stage.artofcode.co.uk'
+  config.mailer_sender = 'writing@codidact.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
