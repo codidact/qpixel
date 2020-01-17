169e2eb9d257c670cac47c03567d23382bd2b6f4
ArtOfCode- <hello@artofcode.co.uk>
2020-01-17 00:18:00 +0000

    Skip caching values if we're in test

--- config/initializers/zz_cache_setup.rb
+++ config/initializers/zz_cache_setup.rb
@@ -2,6 +2,8 @@ Rails.cache.persistent 'current_commit', clear: true do
   "#{`git rev-parse HEAD`.strip[0..7]} (#{`git log -1 --date=iso --pretty=format:%cd`.strip})"
 end
 
-SiteSetting.all.each do |setting|
-  Rails.cache.write "SiteSettings/#{setting.name}", SiteSetting[setting.name]
+unless Rails.env.test?
+  SiteSetting.all.each do |setting|
+    Rails.cache.write "SiteSettings/#{setting.name}", SiteSetting[setting.name]
+  end
 end
