76716e54cab9d929d6ede3feb911ff7e34c90f87
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2020-01-25 16:21:17 +0000

    Remove site settings caching
    Cache write on first query is easy enough, and this was causing errors otherwise: #55

--- config/initializers/zz_cache_setup.rb
+++ config/initializers/zz_cache_setup.rb
@@ -1,9 +1,3 @@
 Rails.cache.persistent 'current_commit', clear: true do
   "#{`git rev-parse HEAD`.strip[0..7]} (#{`git log -1 --date=iso --pretty=format:%cd`.strip})"
 end
-
-unless Rails.env.test?
-  SiteSetting.all.each do |setting|
-    Rails.cache.write "SiteSettings/#{setting.name}", SiteSetting[setting.name]
-  end
-end
