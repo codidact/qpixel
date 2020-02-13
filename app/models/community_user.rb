6af6bc284230a07276d6f76877b4a288cf276df1
ArtOfCode- <hello@artofcode.co.uk>
2020-02-13 21:21:02 +0000

    Only one CommunityUser per user/community combination

--- app/models/community_user.rb
+++ app/models/community_user.rb
@@ -2,5 +2,7 @@ class CommunityUser < ApplicationRecord
   belongs_to :community
   belongs_to :user
 
+  validates :user_id, uniqueness: { scope: [:community_id] }
+
   scope :for_context, ->{ where(community_id: RequestContext.community_id) }
 end
