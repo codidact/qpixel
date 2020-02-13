8cc7de9d4ddd5a5d35d1433da4151a3c817d72a0
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2020-02-13 21:33:55 +0000

    Don't worry about non-thread-safe implementations

--- test/models/request_context_test.rb
+++ test/models/request_context_test.rb
@@ -26,34 +26,25 @@ class RequestContextTest < ActiveSupport::TestCase
     assert_equal RequestContext.user_id, @user.id
   end
 
-  class NonThreadSafe
-    cattr_accessor :community
-  end
-
   test "thread safety" do
     @community1 = Community.new(id: 17)
     @community2 = Community.new(id: 18)
     @community3 = Community.new(id: 19)
     RequestContext.community = @community1
-    NonThreadSafe.community = @community1   # as a baseline, to demonstrate non-thread safe behavior
 
     worker1 = Thread.new do
       RequestContext.community = @community2
-      NonThreadSafe.community = @community2
       sleep 0.5
 
       # this check runs second
       assert_equal RequestContext.community, @community2
-      assert_not_equal NonThreadSafe.community, @community2
     end
 
     worker2 = Thread.new do
       RequestContext.community = @community3
-      NonThreadSafe.community = @community3
 
       # this check runs first
       assert_equal RequestContext.community, @community3
-      assert_equal NonThreadSafe.community, @community3
     end
     worker1.join(0.1) # run worker1 until sleep
     worker2.join(0.5) # run worker2 to completion
@@ -61,6 +52,5 @@ class RequestContextTest < ActiveSupport::TestCase
 
     # this check runs last
     assert_equal RequestContext.community, @community1
-    assert_not_equal NonThreadSafe.community, @community1
   end
 end
