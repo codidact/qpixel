47f40ae4bad26d3b8a9f53739106429e81b352d4
ArtOfCode- <hello@artofcode.co.uk>
2020-04-27 21:11:48 +0100

    Activate category if @category is correct

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,5 +1,6 @@
 module CategoriesHelper
   def active?(category)
-    current_page?(category_path(category)) || (category.is_homepage && current_page?(root_path))
+    current_page?(category_path(category)) || (category.is_homepage && current_page?(root_path)) ||
+      (defined?(@category) && @category&.id == category.id)
   end
 end
