76a6ab244693cd23a4ab783be06a1a8099baada6
luap42 <pstrobach15+luap42@gmail.com>
2020-05-25 09:45:51 +0200

    Make tagging work

--- app/models/suggested_edit.rb
+++ app/models/suggested_edit.rb
@@ -3,6 +3,8 @@ class SuggestedEdit < ApplicationRecord
 
     belongs_to :user
 
+    serialize :tags_cache, Array
+
     belongs_to :decided_by, class_name: "User", optional: true
     has_and_belongs_to_many :tags
 
@@ -16,5 +18,29 @@ class SuggestedEdit < ApplicationRecord
 
     def rejected?
         !active && !accepted
+    end
+
+    def on_question?
+        post.question?
+    end
+
+    
+    before_validation :update_tag_associations, if: :on_question?
+
+    def update_tag_associations
+        return if tags_cache.nil? # Don't update if this doesn't affect tags
+
+        tags_cache.each do |tag_name|
+            tag = Tag.find_or_create_by name: tag_name, tag_set: post.category.tag_set
+            unless tags.include? tag
+                tags << tag
+            end
+        end
+
+        tags.each do |tag|
+            unless tags_cache.include? tag.name
+                tags.delete tag
+            end
+        end
     end
 end
