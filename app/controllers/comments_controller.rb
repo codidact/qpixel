6e42959211ac1a51f8ce0d7cb3c6f882806781fd
ArtOfCode- <hello@artofcode.co.uk>
2020-06-09 02:51:17 +0100

    Move CommentScrubber

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -95,16 +95,3 @@ class CommentsController < ApplicationController
     end
   end
 end
-
-# Provides a custom HTML sanitization interface to use for cleaning up the HTML in questions.
-class CommentScrubber < Rails::Html::PermitScrubber
-  def initialize
-    super
-    self.tags = %w[a b i em strong strike del code]
-    self.attributes = %w[href title]
-  end
-
-  def skip_node?(node)
-    node.text?
-  end
-end
