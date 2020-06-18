5d5dc354a056c028c16920f54b27e990c689339b
ArtOfCode- <hello@artofcode.co.uk>
2020-06-18 20:13:45 +0100

    Required tags clickable

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -15,4 +15,19 @@ $(() => {
       }
     });
   });
+
+  $('.js-add-required-tag').on('click', ev => {
+    const $tgt = $(ev.target);
+    const tagId = $tgt.attr('data-tag-id');
+    const tagName = $tgt.attr('data-tag-name');
+    const $select = $tgt.parents('.form-group').find('select');
+    const existing = $select.find(`option[value=${tagId}]`);
+    if (existing.length > 0) {
+      $select.val([tagId, ...$select.val()]).trigger('change');
+    }
+    else {
+      const option = new Option(tagName, tagId, false, true);
+      $tgt.parents('.form-group').find('select').append(option).trigger('change');
+    }
+  });
 });
