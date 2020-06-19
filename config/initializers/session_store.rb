f12beb687722e8f8c9671b29a389ab33f4da8a00
ArtOfCode- <hello@artofcode.co.uk>
2020-06-19 21:09:50 +0100

    I'm dumb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_codidact_acct'
+  Rails.application.config.session_store :cookie_store, key: 'codidact_acct', domain: :all
 else
-  Rails.application.config.session_store :cookie_store, key: '_codidact_acct'
+  Rails.application.config.session_store :cookie_store, key: 'codidact_acct'
 end
