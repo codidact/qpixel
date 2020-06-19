6d9fee47055f477604ea399d69e3ed15aca658af
ArtOfCode- <hello@artofcode.co.uk>
2020-06-19 09:56:52 +0100

    Scope user report to site

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,11 @@ class ReportsController < ApplicationController
   before_action :authenticate_user!
   before_action :verify_moderator
 
-  def users; end
+  def users
+    @users = User.joins(:community_users).where(community_users: { community_id: RequestContext.community_id })
+                 .where("users.email NOT LIKE '%localhost'")
+                 .where('users.created_at >= ?', 1.year.ago).group_by_week(:created_at).count
+  end
 
   def subscriptions; end
 
