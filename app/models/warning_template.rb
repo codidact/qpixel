31390b73dbf0cc629c28c8ea75be0dcbdbe70f0d
luap42 <pstrobach15+luap42@gmail.com>
2020-06-26 23:08:39 +0200

    This should do it.

--- app/models/warning_template.rb
+++ app/models/warning_template.rb
@@ -6,12 +6,12 @@ class WarningTemplate < ApplicationRecord
   def body_as_b64
     body_with_site_replacements = body.gsub '$SiteName', SiteSetting['SiteName']
 
-    body_with_site_replacements = if SiteSetting['ChatLink'].nil?
-      body_with_site_replacements.gsub '$ChatLink', 'chat'
-    else
-      chat_link = '[chat](' + SiteSetting['ChatLink'] + ')'
-      body_with_site_replacements.gsub '$ChatLink', chat_link
-    end
+    chat_link = if SiteSetting['ChatLink'].nil?
+                  'chat'
+                else
+                  '[chat](' + SiteSetting['ChatLink'] + ')'
+                end
+    body_with_site_replacements = body_with_site_replacements.gsub '$ChatLink', chat_link
 
     Base64.encode64(body_with_site_replacements)
   end
