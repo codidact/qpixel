0f04d08d130e09d521faf67d94588f97e4606e3c
ArtOfCode- <hello@artofcode.co.uk>
2020-06-30 15:43:14 +0100

    Display tag descriptions in selector

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,4 +1,26 @@
 $(() => {
+  const sum = (ary) => ary.reduce((a, b) => a + b, 0);
+
+  const splitWordsMaxLength = (text, max) => {
+    const words = text.split(' ');
+    const splat = [[]];
+    words.forEach(word => {
+      if (sum(splat[splat.length - 1].map(w => w.length + 1)) > max - word.length) {
+        splat.push([]);
+      }
+      splat[splat.length - 1].push(word);
+    });
+    return splat.map(s => s.join(' '));
+  };
+
+  const template = (tag) => {
+    const tagSpan = `<span>${tag.text}</span>`;
+    const descSpan = !!tag.desc ?
+      `<br/><span class="has-color-tertiary-900 has-font-size-caption">${splitWordsMaxLength(tag.desc, 120)[0]}...</span>` :
+      '';
+    return $(tagSpan + descSpan);
+  }
+
   $('.js-tag-select').each((i, el) => {
     const $tgt = $(el);
     const useIds = $tgt.attr('data-use-ids') === 'true';
@@ -11,8 +33,11 @@ $(() => {
         },
         headers: { 'Accept': 'application/json' },
         delay: 100,
-        processResults: data => ({results: data.map(t => ({id: useIds ? t.id : t.name, text: t.name}))}),
-      }
+        processResults: data => {
+          return {results: data.map(t => ({id: useIds ? t.id : t.name, text: t.name, desc: t.excerpt}))};
+        },
+      },
+      templateResult: template
     });
   });
 
