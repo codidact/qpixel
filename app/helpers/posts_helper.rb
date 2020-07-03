91b539af4fe186724a875556462b42868b6027e0
ArtOfCode- <hello@artofcode.co.uk>
2020-07-03 17:41:52 +0100

    Alt attribute

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,7 +4,7 @@ module PostsHelper
       super
       self.tags = %w[a p b i em strong hr h1 h2 h3 h4 h5 h6 blockquote img strike del code pre br ul ol li sup sub
                      section]
-      self.attributes = %w[id class href title src height width]
+      self.attributes = %w[id class href title src height width alt]
     end
 
     def skip_node?(node)
