ceff5c09d7606ee86f965a24338e840380575a42
ArtOfCode- <hello@artofcode.co.uk>
2020-07-11 01:05:45 +0100

    No respect for the effort that went into that, rubocop. None at all.

--- app/helpers/advertisement_helper.rb
+++ app/helpers/advertisement_helper.rb
@@ -7,48 +7,32 @@ module AdvertisementHelper
     final = ''
     current = []
     current_mode = rtl?(chars[0]) ? :rtl : :ltr
-    chars.each.with_index do |c, i|
+    chars.each do |c|
       new_mode = rtl?(c) ? :rtl : :ltr
-      # new_mode = if c.match(/(?:\s|[[:punct:]])/)
-      #              if i == chars.size - 1
-      #                :ltr
-      #              elsif current.size >= 1
-      #                puts "[#{current_mode}] pchar #{c}    previous #{rtl?(current[-1])}    => #{rtl?(current[-1]) ? :rtl : :ltr}"
-      #                rtl?(current[-1]) ? :rtl : :ltr
-      #              else
-      #                puts "[#{current_mode}] pchar #{c}    no previous    => ltr"
-      #                :ltr
-      #              end
-      #            else
-      #              puts "[#{current_mode}] char #{c}    => #{rtl?(c) ? :rtl : :ltr}"
-      #              rtl?(c) ? :rtl : :ltr
-      #            end
       if new_mode != current_mode
-        if current_mode == :rtl
-          final += current.join('').reverse
-          puts "dump current -> reverse -> final, final = #{final}"
-        else
-          final += current.join('')
-          puts "dump current -> final, final = #{final}"
-        end
+        final += if current_mode == :rtl
+                   current.join('').reverse
+                 else
+                   current.join('')
+                 end
         current = []
       end
       current_mode = new_mode
       current << c
     end
 
-    if current_mode == :rtl
-      final += current.join('').reverse
-    else
-      final += current.join('')
-    end
+    final += if current_mode == :rtl
+               current.join('').reverse
+             else
+               current.join('')
+             end
 
     final
   end
 
-  def rtl?(c)
-    return false if c.nil?
+  def rtl?(char)
+    return false if char.nil?
 
-    c.ord >= RTL_BLOCK_START && c.ord <= RTL_BLOCK_END
+    char.ord >= RTL_BLOCK_START && char.ord <= RTL_BLOCK_END
   end
 end
