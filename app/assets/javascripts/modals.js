d1330651ff422747cfc3842b92ff790e11f68356
ArtOfCode- <hello@artofcode.co.uk>
2020-07-14 12:23:47 +0100

    Remove obsolete modal JS

--- app/assets/javascripts/modals.js
+++ app/assets/javascripts/modals.js
@@ -1,11 +1,4 @@
 $(() => {
-  $(document).on('click', '[data-modal]', ev => {
-    const $tgt = $(ev.target);
-    const $trigger = $tgt.is('[data-modal]') ? $tgt : $tgt.parents('[data-modal]');
-    const $modal = $($trigger.attr('data-modal'));
-    $modal.toggleClass('is-active');
-  });
-
   $(document).on('keydown', ev => {
     if (ev.keyCode === 27) { // Esc
       $('.modal').removeClass('is-active');
