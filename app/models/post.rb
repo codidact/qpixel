ed9543cd1d8ee1e1aff3bceb93c3f59457253391
luap42 <21335202+luap42@users.noreply.github.com>
2020-07-17 10:16:11 +0200

    Prevent accidental suspension notice (#113)
    * Prevent accidental suspension notice
    
    * Equal use
    
    * Aargh.
    
    * 1. Aaargh
    
    * 2. Aaargh
    
    * Fixing the worst of all crimes you can commit in the eyes of almighty rubocop.

--- app/models/post.rb
+++ app/models/post.rb
@@ -170,7 +170,7 @@ class Post < ApplicationRecord
   def modify_author_reputation
     sc = saved_changes
     if sc.include?('deleted') && sc['deleted'][0] != sc['deleted'][1] && created_at >= 60.days.ago
-      deleted = !!saved_changes['deleted']&.last # rubocop:disable Style/DoubleNegation
+      deleted = !!saved_changes['deleted']&.last
       if deleted
         user.update(reputation: user.reputation - Vote.total_rep_change(votes))
       else
