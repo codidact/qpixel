56e9ec2ca7bc2dd2d6f3cf7dcd1902a3845ec7b0
luap42 <pstrobach15+luap42@gmail.com>
2020-07-18 17:22:50 +0200

    Add blocks

--- app/models/blocked_item.rb
+++ app/models/blocked_item.rb
@@ -1,9 +1,12 @@
 class BlockedItem < ApplicationRecord
 
+  scope :active, -> { where("expires IS NULL OR expires > NOW()") }
+
   # Allow for easy checking of type
   ['ip', 'email', 'email_host'].each do |bt|
-    define_method "#{pt.underscore}?" do
+    define_method "#{bt.underscore}?" do
       type == bt
     end
+    scope (bt + 's').to_sym, -> { active.where(type: bt) }
   end
 end
