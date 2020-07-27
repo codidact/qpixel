7aefd2e924fa36ec661833a546385b00d57a464b
luap42 <pstrobach15+luap42@gmail.com>
2020-07-27 20:34:48 +0200

    pocobuR

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -1,26 +1,26 @@
 class TourController < ApplicationController
-    def index
-        render layout: 'without_sidebar'
-    end
+  def index
+    render layout: 'without_sidebar'
+  end
 
-    def question1
-        render layout: 'without_sidebar'
-    end
+  def question1
+    render layout: 'without_sidebar'
+  end
 
-    def question2
-        render layout: 'without_sidebar'
-    end
+  def question2
+    render layout: 'without_sidebar'
+  end
 
-    def question3
-        render layout: 'without_sidebar'
-    end
+  def question3
+    render layout: 'without_sidebar'
+  end
 
-    def more
-        @communities = Community.all
-        render layout: 'without_sidebar'
-    end
+  def more
+    @communities = Community.all
+    render layout: 'without_sidebar'
+  end
 
-    def end
-        render layout: 'without_sidebar'
-    end
+  def end
+    render layout: 'without_sidebar'
+  end
 end
