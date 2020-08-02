5a089b28d438c555f3c74d5a050979cd086c9c11
ArtOfCode- <hello@artofcode.co.uk>
2020-08-03 00:54:16 +0100

    Comment timestamps

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -13,17 +13,31 @@ $(() => {
   
       const unread = data.filter(n => !n.is_read);
       const read = data.filter(n => n.is_read);
-
-      console.log(read);
   
       unread.forEach(notification => {
-        const item = $(`<div class="widget is-teal h-m-0 h-m-b-2"><div class="widget--body h-p-2"><div class="h-c-tertiary-600 h-fs-caption">${notification.community_name} &middot; <strong>unread</strong></div><a href="${notification.link}" data-id="${notification.id}" class="h-fw-bold is-not-underlined">${notification.content}</a></div></div>`);
+        const item = $(`<div class="widget is-teal h-m-0 h-m-b-2">
+            <div class="widget--body h-p-2">
+                <div class="h-c-tertiary-600 h-fs-caption">
+                    ${notification.community_name} &middot; <strong>unread</strong> &middot;
+                    <span data-livestamp="${notification.created_at}">${notification.created_at}</span>
+                </div>
+                <a href="${notification.link}" data-id="${notification.id}" class="h-fw-bold is-not-underlined">${notification.content}</a>
+            </div>
+        </div>`);
         $inboxContainer.append(item);
       });
   
       $inboxContainer.append(`<div role="separator" class="header-slide--separator"></div>`);
       read.forEach(notification => {
-        const item = $(`<div class="widget h-m-0 h-m-b-2"><div class="widget--body h-p-2"><div class="h-c-tertiary-600 h-fs-caption">${notification.community_name} &middot; read</div><a href="${notification.link}" data-id="${notification.id}" class="h-fw-bold is-not-underlined">${notification.content}</a></div></div>"`);
+        const item = $(`<div class="widget h-m-0 h-m-b-2">
+            <div class="widget--body h-p-2">
+                <div class="h-c-tertiary-600 h-fs-caption">
+                    ${notification.community_name} &middot; read &middot;
+                    <span data-livestamp="${notification.created_at}">${notification.created_at}</span>
+                </div>
+                <a href="${notification.link}" data-id="${notification.id}" class="h-fw-bold is-not-underlined">${notification.content}</a>
+            </div>
+        </div>"`);
         $inboxContainer.append(item);
       });
   
