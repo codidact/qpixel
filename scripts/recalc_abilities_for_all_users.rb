cb17c07247ab226f000033b882bb5171cf695edc
luap42 <pstrobach15+luap42@gmail.com>
2020-08-13 21:50:04 +0200

    Add enforcement for moderator ability to recalc script

--- scripts/recalc_abilities_for_all_users.rb
+++ scripts/recalc_abilities_for_all_users.rb
@@ -7,6 +7,10 @@ User.unscoped.all.map do |u|
     puts "  Attempt CommunityUser.Id=#{cu.id}"
     RequestContext.community = cu.community
     cu.recalc_privileges
+
+    if (cu.is_moderator || cu.is_admin || u.is_global_moderator || u.is_global_admin) && !cu.privilege?('mod')
+      cu.grant_privilege('mod')
+    end
   rescue
     puts "    !!! Error recalcing for CommunityUser.Id=#{cu.id}"
   end
