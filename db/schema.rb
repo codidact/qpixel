cd19643fdfab921d00e56d28fabea380a834b364
luap42 <pstrobach15+luap42@gmail.com>
2020-08-13 16:19:39 +0200

    Add summary description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_132829) do
+ActiveRecord::Schema.define(version: 2020_08_13_141412) do
 
   create_table "abilities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "community_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_132829) do
     t.decimal "flag_score_threshold", precision: 10, scale: 8
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "summary"
     t.index ["community_id"], name: "index_abilities_on_community_id"
   end
 
