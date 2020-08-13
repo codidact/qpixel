c3f8a00c61d10645641d61faf22fda16e8bd80f4
luap42 <pstrobach15+luap42@gmail.com>
2020-08-13 21:45:25 +0200

    Allow 3rd-party view of /abilities

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -1,4 +1,6 @@
 class AbilitiesController < ApplicationController
+  before_action :set_user
+
   def index
     @abilities = Ability.all
   end
@@ -7,11 +9,20 @@ class AbilitiesController < ApplicationController
     @ability = Ability.where(internal_id: params[:id]).first
     return not_found if @ability.nil?
 
-    @your_ability = current_user.community_user.privilege @ability.internal_id
+    @your_ability = @user.community_user.privilege @ability.internal_id
   end
 
   def recalc
-    current_user.community_user.recalc_privileges
-    redirect_to abilities_url
+    @user.community_user.recalc_privileges
+    redirect_to abilities_url(for: @user.id)
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+    if params[:for]
+      @user = User.where(id: params[:for]).first || @user
+    end
   end
 end
