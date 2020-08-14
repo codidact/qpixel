7b9fb1566c6f8b684baa0b5a6c6b21d2ba65445a
ArtOfCode- <hello@artofcode.co.uk>
2020-08-14 22:10:17 +0100

    Rubocop, sigh

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -57,7 +57,7 @@ class AdminController < ApplicationController
             end.user_sort({ term: params[:sort], default: :created_at },
                           age: :created_at, type: :log_type, event: :event_type,
                           related: Arel.sql('related_type DESC, related_id DESC'), user: :user_id)
-               .paginate(page: params[:page], per_page: 100)
+            .paginate(page: params[:page], per_page: 100)
     render layout: 'without_sidebar'
   end
 end
