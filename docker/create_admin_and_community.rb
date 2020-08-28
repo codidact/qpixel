46e379e6cb167005483d4b41a84815df8aa7cce0
ArtOfCode- <hello@artofcode.co.uk>
2020-08-28 20:03:46 +0100

    -_-

--- docker/create_admin_and_community.rb
+++ docker/create_admin_and_community.rb
@@ -7,6 +7,7 @@ username = ENV['COMMUNITY_ADMIN_USERNAME'] || 'admin'
 password = ENV['COMMUNITY_ADMIN_PASSWORD'] || 'password'
 email = ENV['COMMUNITY_ADMIN_EMAIL'] || 'codadict@noreply.com'
 
-User.create(username: username, password: password, email: email, is_global_admin: true, is_global_moderator: true, staff: true)
+User.create(username: username, password: password, email: email, is_global_admin: true, is_global_moderator: true,
+            staff: true)
 # You'll need to manually set confirmation for this user
 # $ docker exec qpixel_uwsgi_1 rails runner "User.second.update(confirmed_at: DateTime.now)"
