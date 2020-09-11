0149dc74929d6abb55746cc867f328471920a714
galegosimpatico <galegosimpatico@outlook.com>
2020-09-11 20:58:35 +0200

    Better visualization for tag merge (with itself).
    Flash 400 instead of 500 for the invalid request.
    
    Attempts to tackle issue #175.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -94,6 +94,26 @@ class TagsController < ApplicationController
 
   def merge
     @primary = @tag
+
+    # No merge to self.
+    if params[:merge_with_id].eql? @primary.id.to_s
+      flash[:danger] = 'Cannot merge a tag with itself.'
+
+      # redirect_to tag_path(id: @category.id, tag_id: @primary.id) && return
+
+      # render :edit, status: 400
+      # return
+
+      # render json: { success: false, status: 400, tag: @tag }
+      # return
+
+      # render 'tag', category: @category, tag: @tag
+      # return
+
+      redirect_back fallback_location: categories_path
+      return
+    end
+
     @subordinate = Tag.find params[:merge_with_id]
 
     # Take the tag off posts
