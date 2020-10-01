1c596f2db41a7cc24fec1b1dee3f57b93ec1428d
luap42 <pstrobach15+luap42@gmail.com>
2020-10-01 12:14:34 +0200

    Allow lifting warnings

--- config/routes.rb
+++ config/routes.rb
@@ -212,6 +212,7 @@ Rails.application.routes.draw do
   get   'warning/log/:user_id',            to: 'mod_warning#log', as: :mod_warning_log
   get   'warning/new/:user_id',            to: 'mod_warning#new', as: :new_mod_warning
   post  'warning/new/:user_id',            to: 'mod_warning#create', as: :create_mod_warning
+  post  'warning/lift/:user_id',            to: 'mod_warning#lift', as: :lift_mod_warning
 
   get   'uploads/:key',                    to: 'application#upload', as: :uploaded
 
