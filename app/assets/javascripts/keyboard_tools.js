86798bf8ecc1fc3951f1a14bae7d4525006ad179
luap42 <pstrobach15+luap42@gmail.com>
2020-10-04 20:12:03 +0200

    Fix review comments

--- app/assets/javascripts/keyboard_tools.js
+++ app/assets/javascripts/keyboard_tools.js
@@ -1,13 +1,14 @@
 $(() => {
     const userLink = $('.header--item.is-complex.is-visible-on-mobile[href^="/users/"]').attr('href');
-    const keyboardToolsAreEnabled = !!window.localStorage.getItem('keyboard__enable');
+    const keyboardToolsAreEnabled = !!window.localStorage.getItem("keyboard__enable");
 
-    $(".js-keyboard_tools-status").text(keyboardToolsAreEnabled ? 'activated' : 'inactive');
+    $(".js-keyboard_tools-status").text(keyboardToolsAreEnabled ? "activated" : "inactive");
     $(".js-keyboard_tools-toggle").click(() => {
-        if (window.localStorage.getItem('keyboard__enable')) {
-            window.localStorage.removeItem('keyboard__enable');
-        } else {
-            window.localStorage.setItem('keyboard__enable', true);
+        if (window.localStorage.getItem("keyboard__enable")) {
+            window.localStorage.removeItem("keyboard__enable");
+        }
+        else {
+            window.localStorage.setItem("keyboard__enable", true);
         }
         window.location.reload();
     })
