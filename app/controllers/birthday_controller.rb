5338a4e78acec2065a84d32c5873d13c09be607f
luap42 <pstrobach15+luap42@gmail.com>
2020-10-16 16:00:42 +0200

    Even on its birthday...

--- app/controllers/birthday_controller.rb
+++ app/controllers/birthday_controller.rb
@@ -1,12 +1,14 @@
 class BirthdayController < ApplicationController
-    def index
-        render layout: 'without_sidebar'
-    end
+  def index
+    render layout: 'without_sidebar'
+  end
 
-    def ranking
-        @ranking = Rails.cache.fetch("birthday-ranking", expires_in: 1.hour) do
-            User.unscoped.joins(:posts).where('posts.score > 0.6 AND posts.created_at >= \'2020-10-16\' AND posts.created_at <= \'2020-10-23\'').group('posts.user_id').select('users.*', 'count(posts.id) as post_count').order('post_count DESC').all
-        end
-        render layout: 'without_sidebar'
+  def ranking
+    @ranking = Rails.cache.fetch('birthday-ranking', expires_in: 1.hour) do
+      User.unscoped.joins(:posts).where('posts.score > 0.6') \
+          .where('AND posts.created_at >= \'2020-10-16\' AND posts.created_at <= \'2020-10-23\'') \
+          .group('posts.user_id').select('users.*', 'count(posts.id) as post_count').order('post_count DESC').all
     end
+    render layout: 'without_sidebar'
+  end
 end
