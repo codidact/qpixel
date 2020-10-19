849878e6f83d631ea078243799dd97dc7217090b
luap42 <pstrobach15+luap42@gmail.com>
2020-10-19 13:56:18 +0200

    Fix scoping now

--- app/controllers/birthday_controller.rb
+++ app/controllers/birthday_controller.rb
@@ -5,7 +5,7 @@ class BirthdayController < ApplicationController
 
   def ranking
     @ranking = Rails.cache.fetch('birthday-ranking', expires_in: 1.hour) do
-      User.unscoped.joins(:posts).where('posts.score > 0.6') \
+      User.unscoped.joins('INNER JOIN posts ON posts.user_id = users.id').where('posts.score > 0.1') \
           .where('posts.created_at >= \'2020-10-16\' AND posts.created_at <= \'2020-10-23\'') \
           .group('posts.user_id').select('users.*', 'count(posts.id) as post_count').order('post_count DESC').all
     end
