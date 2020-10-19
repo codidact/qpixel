75f3301d57fc4313f4a8e362e27ecf52ecb6b589
luap42 <pstrobach15+luap42@gmail.com>
2020-10-19 14:45:41 +0200

    Development is fun......

--- app/controllers/birthday_controller.rb
+++ app/controllers/birthday_controller.rb
@@ -5,9 +5,10 @@ class BirthdayController < ApplicationController
 
   def ranking
     @ranking = Rails.cache.fetch('birthday-ranking', expires_in: 1.hour) do
-      User.unscoped.joins('INNER JOIN posts ON posts.user_id = users.id').where('posts.score > 0.1') \
+      User.unscoped.joins('INNER JOIN posts ON posts.user_id = users.id').where('posts.score > 0.6') \
           .where('posts.created_at >= \'2020-10-16\' AND posts.created_at <= \'2020-10-23\'') \
-          .group('posts.user_id').select('users.*', 'count(posts.id) as post_count').order('post_count DESC').all
+          .where(staff: false).group('posts.user_id').select('users.*', 'count(posts.id) as post_count') \
+          .order('post_count DESC, users.created_at DESC').all
     end
     render layout: 'without_sidebar'
   end
