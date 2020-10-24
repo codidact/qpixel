cc69589d455b980166dcaa66d3609ae910758429
sau226 <32805612+sau226@users.noreply.github.com>
2020-10-25 07:55:59 +0800

    Add check for cookie before element

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -135,8 +135,9 @@ $(document).on('ready', function() {
     });
   });
 
-
-  document.getElementById("fvn-dismiss").onclick = function() {
-    document.cookie = 'dismiss_fvn=true; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+  if (document.cookie.indexOf ('dismiss_fvn') == -1 ) {
+    document.getElementById("fvn-dismiss").onclick = function() {
+      document.cookie = 'dismiss_fvn=true; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+    }
   };
 });
