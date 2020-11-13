cd9a9c3f845375d3bf17e5b5e41bf432926ca5cd
ArtOfCode- <hello@artofcode.co.uk>
2020-11-13 01:17:07 +0000

    Omit last activity in history (among other things); close #279

--- app/controllers/post_history_controller.rb
+++ app/controllers/post_history_controller.rb
@@ -1,7 +1,7 @@
 class PostHistoryController < ApplicationController
   def post
     @post = Post.find(params[:id])
-    @history = PostHistory.where(post_id: params[:id]).includes(:post_history_type, :user)
+    @history = PostHistory.where(post_id: params[:id]).includes(:post_history_type, :user, post_history_tags: [:tag])
                           .order(created_at: :desc).paginate(per_page: 20, page: params[:page])
     render layout: 'without_sidebar'
   end
