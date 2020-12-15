8d62ba7e4605f39e5d58d451793d0f64ab3d1e3e
ArtOfCode- <hello@artofcode.co.uk>
2020-12-15 14:28:39 +0000

    Test non-public types

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -289,6 +289,28 @@ class PostsControllerTest < ActionController::TestCase
     assert_response 401
   end
 
+  test 'cannot edit non-public post without permissions' do
+    sign_in users(:standard_user)
+    get :edit, params: { id: posts(:blog_post).id }
+    assert_response 302
+    assert_redirected_to root_path
+    assert_not_nil flash[:danger]
+  end
+
+  test 'author can edit non-public post' do
+    sign_in users(:closer)
+    get :edit, params: { id: posts(:blog_post).id }
+    assert_response 200
+    assert_not_nil assigns(:post)
+  end
+
+  test 'moderator can edit non-public post' do
+    sign_in users(:moderator)
+    get :edit, params: { id: posts(:blog_post).id }
+    assert_response 200
+    assert_not_nil assigns(:post)
+  end
+
   # Update
 
   test 'can update post' do
