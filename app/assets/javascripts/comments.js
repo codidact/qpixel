8100a9e6babb7d392702911e9b33ffffe2fbce88
luap42 / Paul <pstrobach15+luap42@gmail.com>
2021-01-01 15:21:16 +0100

    Refactor

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,16 +1,4 @@
 $(() => {
-  $('.js-comment-form').hide();
-
-  $('.js-add-comment').on('click', async evt => {
-    // User clicked `Add a comment`.
-
-    evt.preventDefault();
-
-    const $form = $(evt.target).parent().find('.js-comment-form');
-    $form.show();
-    $form.find('.js-comment-content').focus();
-  });
-
   $('.js-more-comments').on('click', async evt => {
     evt.preventDefault();
     const $tgt = $(evt.target);
@@ -23,38 +11,6 @@ $(() => {
     const data = await resp.text();
     $tgt.parents('.post--comments').find('.post--comments-container').html(data).trigger('ajax:success');
     $anchor.remove();
-  });
-
-  $('.comment-form').on('ajax:success', async (evt, data) => {
-    // Comment posting is succeeded! ^_^
-
-    const $tgt = $(evt.target);
-    if (data.status === 'success') {
-      $tgt.parents('.post--comments').find('.post--comments-container').append(data.comment);
-      $tgt.find('.js-comment-content').val('');
-
-      // On success, the `Post` button, which has been re-labeled as `Posted`, is not yet re-labeled `Post` when
-      // reaching this line. The line below re-labels it back to `Post`.
-      $tgt.find('input[type="submit"]').attr('value', 'Post')
-    }
-    else {
-      QPixel.createNotification('danger', data.message);
-    }
-  }).on('ajax:error', async (evt, xhr) => {
-    // Comment posting is errored, e.g. it might be too short to be posted.
-
-    const data = xhr.responseJSON;
-    QPixel.createNotification('danger', data.message);
-
-    // On error, the `Post` button, which has been re-labeled as `Posted`, will be re-labeled `Post` back again. There's
-    // no need to do anything else at this point in this block, until proven otherwise.
-  });
-
-  $('.comment-form').find('input[value="Discard"]').on('click', function(data) {
-      const $tgt = $(data.target);
-      const $form = $tgt.parents('form');
-      $form[0].reset();  // Clear the comment field before hiding the form for a fresh start.
-      $form.hide();
   });
 
   $(document).on('click', '.js-comment-edit', async evt => {
@@ -72,18 +28,12 @@ $(() => {
     const content = data.content;
 
     const formTemplate = `<form action="/comments/${commentId}/edit" method="POST" class="comment-edit-form" data-remote="true">
-      <div class="form-group-horizontal">
-        <div class="form-group">
-          <label for="comment-content">Your comment</label>
-          <textarea rows="3" class="form-element is-small" name="comment[content]">${content}</textarea>
-        </div>
-        <div class="actions">
-          <input type="submit" class="button is-outlined" value="Post" />
-        </div>
-      </div>
+      <label for="comment-content" class="form-element">Comment body:</label>
+      <textarea id="comment-content" rows="3" class="form-element is-small" name="comment[content]">${content}</textarea>
+      <input type="submit" class="button is-muted is-filled" value="Update comment" />
     </form>`;
 
-    $comment.html(formTemplate);
+    $comment.find(".comment--body").html(formTemplate);
   });
 
   $(document).on('ajax:success', '.comment-edit-form', async (evt, data) => {
