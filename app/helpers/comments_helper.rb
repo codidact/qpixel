0611261078fb672936661a053082c23bf69416f9
luap42 / Paul <pstrobach15+luap42@gmail.com>
2021-01-01 15:29:20 +0100

    Add comment permalinks

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,10 +1,17 @@
 module CommentsHelper
+  def comment_link(comment)
+    if comment.deleted
+      comment_thread_path(comment.comment_thread_id) + "?show_deleted_comments=1#comment-#{comment.id}"
+    else
+      comment_thread_path(comment.comment_thread_id) + "#comment-#{comment.id}"
+    end
+  end
 end
 
 class CommentScrubber < Rails::Html::PermitScrubber
   def initialize
     super
-    self.tags = %w[a b i em strong strike del code]
+    self.tags = %w[a b i em strong strike del code p]
     self.attributes = %w[href title]
   end
 
