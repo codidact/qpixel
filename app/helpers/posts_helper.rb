344cdd52c89108175284389eb87f46bd193c64ad
ArtOfCode- <ArtOfCode-@users.noreply.github.com>
2021-01-02 15:36:10 +0000

    Allow s tag; close #312

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,7 +7,7 @@ module PostsHelper
     def initialize
       super
       self.tags = %w[a p span b i em strong hr h1 h2 h3 h4 h5 h6 blockquote img strike del code pre br ul ol li sup sub
-                     section details summary ins table thead tbody tr th td]
+                     section details summary ins table thead tbody tr th td s]
       self.attributes = %w[id class href title src height width alt rowspan colspan lang]
     end
 
