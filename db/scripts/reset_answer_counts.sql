3950e4048074db40c1775e38877afed437208d2a
ArtOfCode- <ArtOfCode-@users.noreply.github.com>
2021-02-22 21:20:55 +0000

    Add no-excerpt view; close #438

--- db/scripts/reset_answer_counts.sql
+++ db/scripts/reset_answer_counts.sql
@@ -2,10 +2,8 @@ update posts tl
 inner join (
     select px.id, count(pr.id) as answer_count
     from posts px
-    left join posts pr on px.id = pr.parent_id
+    left join posts pr on px.id = pr.parent_id and pr.deleted = false
     where px.post_type_id = 1
-      and pr.parent_id is not null
-      and pr.deleted = false
     group by px.id
 ) q on q.id = tl.id
 set tl.answer_count = q.answer_count;
