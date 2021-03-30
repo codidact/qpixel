75f2bf70d1d2aa5dc7a5beaceabdd8de8a1c96c0
ArtOfCode- <ArtOfCode-@users.noreply.github.com>
2021-03-30 01:29:29 +0100

    Support dir; close #459

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -20,7 +20,7 @@ module PostsHelper
       super
       self.tags = %w[a p span b i em strong hr h1 h2 h3 h4 h5 h6 blockquote img strike del code pre br ul ol li sup sub
                      section details summary ins table thead tbody tr th td s]
-      self.attributes = %w[id class href title src height width alt rowspan colspan lang start]
+      self.attributes = %w[id class href title src height width alt rowspan colspan lang start dir]
     end
 
     def skip_node?(node)
