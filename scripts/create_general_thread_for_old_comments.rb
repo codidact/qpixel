6cf6f10eb09cd65b01429339f7eb8b4c859203ed
ArtOfCode- <ArtOfCode-@users.noreply.github.com>
2021-06-09 18:45:45 +0100

    Apply some suggestions

--- scripts/create_general_thread_for_old_comments.rb
+++ scripts/create_general_thread_for_old_comments.rb
@@ -1,4 +1,5 @@
-Post.unscoped.each do |post|
+Post.unscoped.where(id: Comment.unscoped.where(comment_thread: nil)
+                               .select(Arel.sql('distinct post_id'))).each do |post|
   comments = Comment.unscoped.where(post: post, comment_thread: nil)
   next unless comments.any?
 
@@ -8,9 +9,7 @@ Post.unscoped.each do |post|
                                  archived: false, deleted: false, community: post.community)
   new_thread.save
 
-  comments.each do |c|
-    c.update comment_thread: new_thread
-  end
+  comments.update_all(comment_thread_id: new_thread.id)
 
   puts "#{comments.size} comments updated for post Id=#{post.id}"
 end
