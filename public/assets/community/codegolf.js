ef14bd83a55bc5aa2057eda18e4a0f612798ba3b
ArtOfCode- <ArtOfCode-@users.noreply.github.com>
2021-06-26 23:42:34 +0100

    Clean up CG script

--- public/assets/community/codegolf.js
+++ public/assets/community/codegolf.js
@@ -1,234 +1,266 @@
-// ==UserScript==
-// @name         Codidact - Codegolf Leaderboards - ToC Edition
-// @namespace    http://tampermonkey.net/
-// @version      2.10
-// @description  Addes a leaderboard to code golf challenges
-// @author       Moshi
-// @match        https://codegolf.codidact.com/posts/*
-// @icon         https://www.google.com/s2/favicons?domain=codidact.com
-// @grant        none
-// @run-at       document-start
-// ==/UserScript==
-
-let CHALLENGE_ID = location.pathname.match(/(?<=posts\/)\d+/)[0];
-let leaderboard;
-
-/**
- * Wrapper around localStorage
+/*! Code Golf Leaderboard script
+ *  Author: Moshi <https://github.com/MoshiKoi>
+ *  License: AGPLv3
  */
-let settings = {
-	_defaults: {
-		groupByLanguage: true,
-		mergeVariants: false,
-		showPlacements: true
-	},
-	currentSettings: {}, // Used as a fallback if localStorage is unavailable
-	get groupByLanguage() { return this._get('groupByLanguage'); },
-	set groupByLanguage(value) { return this._set('groupByLanguage', value); },
 
-	get mergeVariants() { return this._get('mergeVariants'); },
-	set mergeVariants(value) { return this._set('mergeVariants', value); },
+(() => {
+  let match = location.pathname.match(/(?<=posts\/)\d+/);
 
-	get showPlacements() { return this._get('showPlacements'); },
-	set showPlacements(value) { return this._set('showPlacements', value); },
+  // Don't run on non-post pages.
+  if (!match) {
+    return;
+  }
 
-	_get(name) {
-		try {
-			return this.currentSettings[name] ?? JSON.parse(localStorage.getItem(name)) ?? this._defaults[name];
-		} catch(err) {
-			// console.warn(`Failed to retrieve ${name} from localStorage`);
-			return this._defaults[name];
-		}
-	},
-	_set(name, value) {
-		try {
-			this.currentSettings[name] = value
-			localStorage.setItem(name, JSON.stringify(value));
-		} catch(err) {
-			// console.warn(`Failed to store ${name} into localStorage`);
-		}
-	}
-};
+  let CHALLENGE_ID = match[0];
+  let leaderboard;
 
-async function getLeaderboard(id) {
-	let response = await fetch(`https://codegolf.codidact.com/posts/${id}`);
-	let text = await response.text();
+  console.log(`CG Leaderboard active, challenge ID ${CHALLENGE_ID}`);
 
-	let doc = new DOMParser().parseFromString(text.toString(), 'text/html');
+  /**
+   * Wrapper around localStorage
+   */
+  let settings = {
+    _defaults: {
+      groupByLanguage: true,
+      mergeVariants: false,
+      showPlacements: true
+    },
+    currentSettings: {}, // Used as a fallback if localStorage is unavailable
+    get groupByLanguage() {
+      return this._get('groupByLanguage');
+    },
+    set groupByLanguage(value) {
+      return this._set('groupByLanguage', value);
+    },
 
-	const pagination = doc.querySelector('.pagination');
-	const num_pages = pagination ? parseInt(pagination.querySelector('.next').previousElementSibling.innerText) : 1;
+    get mergeVariants() {
+      return this._get('mergeVariants');
+    },
+    set mergeVariants(value) {
+      return this._set('mergeVariants', value);
+    },
 
-	const pagePromises = [];
-	for (let i = 1; i <= num_pages; i++) {
-		pagePromises.push(fetch(`https://codegolf.codidact.com/posts/${id}?sort=age&page=${i}`).then(response => response.text()));
-	}
+    get showPlacements() {
+      return this._get('showPlacements');
+    },
+    set showPlacements(value) {
+      return this._set('showPlacements', value);
+    },
 
-	const leaderboard = [];
+    _get(name) {
+      try {
+        return this.currentSettings[name] ?? JSON.parse(localStorage.getItem(name)) ?? this._defaults[name];
+      } catch (err) {
+        // console.warn(`Failed to retrieve ${name} from localStorage`);
+        return this._defaults[name];
+      }
+    },
+    _set(name, value) {
+      try {
+        this.currentSettings[name] = value
+        localStorage.setItem(name, JSON.stringify(value));
+      } catch (err) {
+        // console.warn(`Failed to store ${name} into localStorage`);
+      }
+    }
+  };
 
-	for (let i = 0; i < pagePromises.length; i++) {
-		let text = await pagePromises[i];
-		let doc = new DOMParser().parseFromString(text.toString(), 'text/html');
-		let [question, ...page_answers] = doc.querySelectorAll('.post');
+  async function getLeaderboard(id) {
+    let response = await fetch(`https://codegolf.codidact.com/posts/${id}`);
+    let text = await response.text();
 
-		for (let answerPost of page_answers) {
+    let doc = new DOMParser().parseFromString(text.toString(), 'text/html');
 
-			let header = answerPost.querySelector('h1, h2, h3');
-			let code = header.parentElement.querySelector(':scope > pre > code, :scope > p > code');
-			let full_language = header ? header.innerText.split(',')[0].trim() : undefined
-			let { language, variant } = (full_language?.match(/(?<language>.+) \((?<variant>.+)\)/) ?? full_language?.match(/(?<language>.+)/))?.groups
+    const pagination = doc.querySelector('.pagination');
+    const num_pages = pagination ? parseInt(pagination.querySelector('.next').previousElementSibling.innerText) : 1;
 
-			let entry = {
-				answerID: answerPost.id,
-				page: i + 1, // +1 because pages are 1-indexed while arrays are 0-indexed
-				username: answerPost.querySelector('.user-card--link').firstChild.data.trim(),
-				userid: answerPost.querySelector('.user-card--link').href.match(/\d+/)[0],
-				language: language,
-				variant: variant,
-				code: code?.innerText,
-				score: header ? header.innerText.match(/\d+(?= [bB]ytes?)/)?.[0] : undefined
-			};
+    const pagePromises = [];
+    for (let i = 1; i <= num_pages; i++) {
+      pagePromises.push(fetch(`https://codegolf.codidact.com/posts/${id}?sort=age&page=${i}`).then(response => response.text()));
+    }
 
-			leaderboard.push(entry);
-		}
-	}
+    const leaderboard = [];
 
-	return leaderboard;
-}
+    for (let i = 0; i < pagePromises.length; i++) {
+      let text = await pagePromises[i];
+      let doc = new DOMParser().parseFromString(text.toString(), 'text/html');
+      let [question, ...page_answers] = doc.querySelectorAll('.post');
 
-function augmentLeaderboardWithPlacements(leaderboard, comparator) {
-	leaderboard.sort(comparator);
+      for (let answerPost of page_answers) {
 
-	let placement = 1;
-	let slack = 0;
+        let header = answerPost.querySelector('h1, h2, h3');
+        let code = header.parentElement.querySelector(':scope > pre > code, :scope > p > code');
+        let full_language = header ? header.innerText.split(',')[0].trim() : undefined
+        let {
+          language,
+          variant
+        } = (full_language?.match(/(?<language>.+) \((?<variant>.+)\)/) ?? full_language?.match(/(?<language>.+)/))?.groups
 
-	leaderboard[0].placement = 1;
+        let entry = {
+          answerID: answerPost.id,
+          page: i + 1, // +1 because pages are 1-indexed while arrays are 0-indexed
+          username: answerPost.querySelector('.user-card--link').firstChild.data.trim(),
+          userid: answerPost.querySelector('.user-card--link').href.match(/\d+/)[0],
+          language: language,
+          variant: variant,
+          code: code?.innerText,
+          score: header ? header.innerText.match(/\d+(?= [bB]ytes?)/)?.[0] : undefined
+        };
 
-	for (let i = 1; i < leaderboard.length; i++) {
-		slack++;
+        leaderboard.push(entry);
+      }
+    }
 
-		// If they compare equal (returns 0), we don't increase the placement
-		if (comparator(leaderboard[i], leaderboard[i-1])) {
-			placement += slack;
-			slack = 0;
-		}
+    return leaderboard;
+  }
 
-		leaderboard[i].placement = placement;
-	}
-}
+  function augmentLeaderboardWithPlacements(leaderboard, comparator) {
+    leaderboard.sort(comparator);
 
-let embed = document.createElement('div');
-embed.innerHTML = `
-<div class="toc">
-	<div style="display:flex">
-		<div class="toc--header">Leaderboards by language</div>
-		<div class="has-padding-2" style="margin-left:auto">
-			<label>Group by language
-				<input id="group-by-lang" type=checkbox${settings.groupByLanguage ? ' checked' : ''}>
-			</label>
-		</div>
-		<div class="has-padding-2" style="margin-left:auto">
-			<label title="Shows variants of a language as the same language (E.g. Python (Cython) and Python (PyPy) will both be put under Python)">Merge variants
-				<input id="merge-variants" type=checkbox${settings.mergeVariants ? ' checked' : ''}>
-			</label>
-		</div>
-		<div class="has-padding-2">
-			<label>Show placements
-				<input id="show-placement" type=checkbox${settings.showPlacements ? ' checked' : ''}>
-			</label
-		</div>
-	</div>
+    let placement = 1;
+    let slack = 0;
+
+    leaderboard[0].placement = 1;
+
+    for (let i = 1; i < leaderboard.length; i++) {
+      slack++;
+
+      // If they compare equal (returns 0), we don't increase the placement
+      if (comparator(leaderboard[i], leaderboard[i - 1])) {
+        placement += slack;
+        slack = 0;
+      }
+
+      leaderboard[i].placement = placement;
+    }
+  }
+
+  let embed = document.createElement('div');
+  embed.innerHTML = `
+<div class="toc cg-leaderboard">
+  <div class="cgl-container">
+    <div class="toc--header has-margin-2">Leaderboards by language</div>
+    <div class="has-padding-2 cgl-option">
+      <label>
+        Group by language
+        <input id="group-by-lang" type="checkbox" ${settings.groupByLanguage ? 'checked' : ''}>
+      </label>
+    </div>
+    <div class="has-padding-2 cgl-option">
+      <label title="Shows variants of a language as the same language (e.g. Python (Cython) and Python (PyPy) will both be put under Python)">
+        Merge variants
+        <input id="merge-variants" type="checkbox" ${settings.mergeVariants ? 'checked' : ''}>
+      </label>
+    </div>
+    <div class="has-padding-2">
+      <label>
+        Show placements
+        <input id="show-placement" type="checkbox" ${settings.showPlacements ? 'checked' : ''}>
+      </label
+    </div>
+  </div>
 </div>`;
 
-const leaderboardsTable = embed.querySelector('.toc');
-const groupByLanguageInput = embed.querySelector('#group-by-lang');
-const mergeVariantsInput = embed.querySelector('#merge-variants');
-const showPlacementsInput = embed.querySelector('#show-placement');
+  const leaderboardsTable = embed.querySelector('.toc');
+  const groupByLanguageInput = embed.querySelector('#group-by-lang');
+  const mergeVariantsInput = embed.querySelector('#merge-variants');
+  const showPlacementsInput = embed.querySelector('#show-placement');
 
-groupByLanguageInput.addEventListener('click', _ => { settings.groupByLanguage = groupByLanguageInput.checked; refreshBoard(); });
-mergeVariantsInput.addEventListener('click', _ => { settings.mergeVariants = mergeVariantsInput.checked; refreshBoard(); });
-showPlacementsInput.addEventListener('click', _ => { settings.showPlacements = showPlacementsInput.checked; refreshBoard(); });
+  groupByLanguageInput.addEventListener('click', _ => {
+    settings.groupByLanguage = groupByLanguageInput.checked;
+    refreshBoard();
+  });
+  mergeVariantsInput.addEventListener('click', _ => {
+    settings.mergeVariants = mergeVariantsInput.checked;
+    refreshBoard();
+  });
+  showPlacementsInput.addEventListener('click', _ => {
+    settings.showPlacements = showPlacementsInput.checked;
+    refreshBoard();
+  });
 
-function refreshBoard() {
-	// Clear table
-	leaderboardsTable.querySelectorAll('a').forEach(el => el.remove());
+  function refreshBoard() {
+    // Clear table
+    leaderboardsTable.querySelectorAll('a').forEach(el => el.remove());
 
-	if (settings.groupByLanguage) {
-		renderLeaderboardsByLanguage();
-	} else {
-		renderLeaderboardsByByteCount();
-	}
-}
+    if (settings.groupByLanguage) {
+      renderLeaderboardsByLanguage();
+    } else {
+      renderLeaderboardsByByteCount();
+    }
+  }
 
-/**
- * Helper function
- * Turns arrays into associative arrays
- */
-function createGroups(array, categorizer) {
-	let groups = {};
+  /**
+   * Helper function
+   * Turns arrays into associative arrays
+   */
+  function createGroups(array, categorizer) {
+    let groups = {};
 
-	for (let item of array) {
-		let category = categorizer(item);
-		if (groups[category]) {
-			groups[category].push(item);
-		} else {
-			groups[category] = [item];
-		}
-	}
+    for (let item of array) {
+      let category = categorizer(item);
+      if (groups[category]) {
+        groups[category].push(item);
+      } else {
+        groups[category] = [item];
+      }
+    }
 
-	return groups;
-}
+    return groups;
+  }
 
-function createRow(answer) {
-	let row = document.createElement('a');
-	row.classList.add('toc--entry');
-	row.href = `https://codegolf.codidact.com/posts/${CHALLENGE_ID}?sort=age&page=${answer.page}#${answer.answerID}`;
+  function createRow(answer) {
+    let row = document.createElement('a');
+    row.classList.add('toc--entry');
+    row.href = `https://codegolf.codidact.com/posts/${CHALLENGE_ID}?sort=age&page=${answer.page}#${answer.answerID}`;
 
-	row.innerHTML = `
-		<div class="toc--badge"><span class="badge is-tag is-green">${answer.score}</span></div>
-		<div class="toc--full"><p style="overflow: hidden;text-overflow: ellipsis;white-space: nowrap;"><span class='username'></span> <code></code></p></div>
-		${answer.placement === 1 ? '<div class="toc--badge"><span class="badge is-tag is-yellow"><i class="fas fa-trophy"></i></span></div>'
-		: (settings.showPlacements ? `<div class="toc--badge"><span class="badge is-tag">#${answer.placement}</span></div>` : '')}
-		<div class="toc--badge"><span class="language-badge badge is-tag is-blue"></span></div>`;
+    row.innerHTML = `
+    <div class="toc--badge"><span class="badge is-tag is-green">${answer.score}</span></div>
+    <div class="toc--full"><p class="row-summary"><span class='username'></span> <code></code></p></div>
+    ${answer.placement === 1 ? '<div class="toc--badge"><span class="badge is-tag is-yellow"><i class="fas fa-trophy"></i></span></div>'
+      : (settings.showPlacements ? `<div class="toc--badge"><span class="badge is-tag">#${answer.placement}</span></div>` : '')}
+    <div class="toc--badge"><span class="language-badge badge is-tag is-blue"></span></div>`;
 
-	row.querySelector('.username').innerText = answer.username
-	row.querySelector('.language-badge').innerText = !settings.mergeVariants && answer.variant ? `${answer.language} (${answer.variant})`: answer.language;
-	row.querySelector('code').innerText = answer.code ? answer.code.split('\n')[0].substring(0, 200) : undefined
+    row.querySelector('.username').innerText = answer.username
+    row.querySelector('.language-badge').innerText = !settings.mergeVariants && answer.variant ? `${answer.language} (${answer.variant})` : answer.language;
+    row.querySelector('code').innerText = answer.code ? answer.code.split('\n')[0].substring(0, 200) : undefined
 
-	return row;
-}
+    return row;
+  }
 
-async function renderLeaderboardsByLanguage() {
-	leaderboard = leaderboard || await getLeaderboard(CHALLENGE_ID);
-	let languageLeaderboards = createGroups(leaderboard, entry => settings.mergeVariants ? entry.language : entry.language + entry.variant);
+  async function renderLeaderboardsByLanguage() {
+    leaderboard = leaderboard || await getLeaderboard(CHALLENGE_ID);
+    let languageLeaderboards = createGroups(leaderboard, entry => settings.mergeVariants ? entry.language : entry.language + entry.variant);
 
-	for (let language in languageLeaderboards) {
-		augmentLeaderboardWithPlacements(languageLeaderboards[language], (x,y) => x.score - y.score);
+    for (let language in languageLeaderboards) {
+      augmentLeaderboardWithPlacements(languageLeaderboards[language], (x, y) => x.score - y.score);
 
-		for (let answer of languageLeaderboards[language]) {
-			let row = createRow(answer);
-			leaderboardsTable.appendChild(row);
-		}
-	}
-}
+      for (let answer of languageLeaderboards[language]) {
+        let row = createRow(answer);
+        leaderboardsTable.appendChild(row);
+      }
+    }
+  }
 
-async function renderLeaderboardsByByteCount() {
-	leaderboard = leaderboard || await getLeaderboard(CHALLENGE_ID);
-	augmentLeaderboardWithPlacements(leaderboard, (x,y) => x.score - y.score);
+  async function renderLeaderboardsByByteCount() {
+    leaderboard = leaderboard || await getLeaderboard(CHALLENGE_ID);
+    augmentLeaderboardWithPlacements(leaderboard, (x, y) => x.score - y.score);
 
-	for (let answer of leaderboard) {
-		let row = createRow(answer);
-		leaderboardsTable.appendChild(row);
-	}
-}
+    for (let answer of leaderboard) {
+      let row = createRow(answer);
+      leaderboardsTable.appendChild(row);
+    }
+  }
 
-(function() {
-	'use strict';
-	window.addEventListener('DOMContentLoaded', _ => {
-		if (document.querySelector('.category-header--name').innerText === 'Challenges') {
-			document.querySelector('.post:first-child').nextElementSibling.insertAdjacentElement('afterend', embed);
+  (function () {
+    'use strict';
+    window.addEventListener('DOMContentLoaded', _ => {
+      if (document.querySelector('.category-header--name').innerText === 'Challenges') {
+        document.querySelector('.post:first-child').nextElementSibling.insertAdjacentElement('afterend', embed);
 
-			refreshBoard();
-		}
-	});
+        refreshBoard();
+      }
+    });
+  })();
+
 })();
