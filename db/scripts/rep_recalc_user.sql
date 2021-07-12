a29405392a603836c1aada500debf5c13b2e87b1
ArtOfCode- <ArtOfCode-@users.noreply.github.com>
2021-07-12 18:53:59 +0100

    Update recalc scripts

--- db/scripts/rep_recalc_user.sql
+++ db/scripts/rep_recalc_user.sql
@@ -7,13 +7,13 @@ INNER JOIN ( SELECT * FROM (
     LEFT JOIN (
         SELECT v.id, v.community_id, v.recv_user_id,
                CASE
-                 WHEN v.vote_type = -1 THEN pt.downvote_rep
-                 WHEN v.vote_type = 1 THEN pt.upvote_rep
+                 WHEN v.vote_type = -1 THEN cpt.downvote_rep
+                 WHEN v.vote_type = 1 THEN cpt.upvote_rep
                  ELSE 0
                END AS rep_change
         FROM votes v
         INNER JOIN posts p ON v.post_id = p.id
-        INNER JOIN post_types pt ON p.post_type_id = pt.id
+        INNER JOIN categories_post_types cpt ON cpt.post_type_id = p.post_type_id AND cpt.category_id = p.category_id
         WHERE p.deleted = 0
     ) vq ON cu.user_id = vq.recv_user_id
     WHERE cu.user_id = @uid
