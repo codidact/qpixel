28ab0065d5e8164926ee0cae4c33283dea2dfa76
ArtOfCode- <ArtOfCode-@users.noreply.github.com>
2021-08-26 23:13:45 +0100

    Correctly grant user abilities; close #649

--- scripts/recalc_abilities.rb
+++ scripts/recalc_abilities.rb
@@ -1,26 +1,58 @@
-puts "Recalculating the Abilities (Scheduled)"
-puts
+options = OpenStruct.new
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: rails r scripts/recalc_abilities.rb [options]"
+
+  opts.on('-v', '--verbose', 'Run with additional logging') do
+    options.verbose = true
+  end
+
+  opts.on('-q', '--quiet', 'Run with minimal logging') do
+    options.quiet = true
+  end
+end.parse!
 
 resolved = []
+destroy = []
+all = AbilityQueue.pending.to_a
 
-AbilityQueue.pending.map do |q|
-  cu = q.community_user
-  puts "Scope: CommunityUser.Id=#{cu.id}"
-  RequestContext.community = cu.community
-  cu.recalc_privileges
+all.each do |q|
+  begin
+    cu = q.community_user
+    u = cu&.user
 
-  # Grant mod ability if mod status is given
-  if (cu.is_moderator || cu.is_admin || u.is_global_moderator || u.is_global_admin) && !cu.privilege?('mod')
-    cu.grant_privilege('mod')
+    if cu.nil? || u.nil?
+      destroy << q.id
+      next
+    end
+
+    RequestContext.community = cu.community
+
+    if options.verbose && !options.quiet
+      puts "Scope: Community     : #{cu.community.name} (#{cu.community.host})"
+      puts "       User          : #{u.username} (#{cu.user_id})"
+      puts "       CommunityUser : #{cu.id}"
+    elsif !options.verbose && !options.quiet
+      puts "Scope: CommunityUser : #{cu.id}"
+    end
+
+    cu.recalc_privileges
+
+    # Grant mod ability if mod status is given
+    if (cu.is_moderator || cu.is_admin || u.is_global_moderator || u.is_global_admin) && !cu.privilege?('mod')
+      cu.grant_privilege('mod')
+    end
+
+    resolved << q.id
+  rescue => e
+    $stderr.puts "  Failed: #{e.class.name}: #{e.message}"
+    $stderr.puts e.backtrace
   end
-  
-  resolved << q.id
-  rescue
-    puts "  Failed."
 end
 
-# Mark resolved queue tasks as completed
 AbilityQueue.where(id: resolved).update(completed: true)
+AbilityQueue.where(id: destroy).delete_all
 
-puts "---"
-puts "Recalculating completed"
+unless options.quiet
+  puts "Completed, #{resolved.size}/#{all.size} tasks successful, #{destroy.size} tasks invalid"
+end
