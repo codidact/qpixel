c47f9bc73cb781d39af6db299456d64a02466550
luap42 / Paul <pstrobach15+luap42@gmail.com>
2021-10-03 18:20:35 +0200

    Test ReactionsController (administrative part)

--- test/controllers/reactions_controller_test.rb
+++ test/controllers/reactions_controller_test.rb
@@ -136,4 +136,143 @@ class ReactionsControllerTest < ActionController::TestCase
     assert_response 403
     assert_equal 'failed', JSON.parse(response.body)['status']
   end
+
+  test 'index should fail for signed-out' do
+    get :index
+    assert_response 302
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'index should fail for standard users' do
+    sign_in users(:standard_user)
+    get :index
+    assert_response 404
+  end
+
+  test 'index should fail for privileged standard users' do
+    sign_in users(:closer)
+    get :index
+    assert_response 404
+  end
+
+  test 'index should show for moderators' do
+    sign_in users(:moderator)
+    get :index
+    assert_response 200
+  end
+
+  test 'edit should fail for signed-out' do
+    get :edit, params: { id: reaction_types(:wfm).id }
+    assert_response 302
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'edit should fail for standard users' do
+    sign_in users(:standard_user)
+    get :edit, params: { id: reaction_types(:wfm).id }
+    assert_response 404
+  end
+
+  test 'edit should fail for privileged standard users' do
+    sign_in users(:closer)
+    get :edit, params: { id: reaction_types(:wfm).id }
+    assert_response 404
+  end
+
+  test 'edit should show for moderators' do
+    sign_in users(:moderator)
+    get :edit, params: { id: reaction_types(:wfm).id }
+    assert_response 200
+    assert_not_nil assigns(:reaction_type)
+  end
+
+  test 'update should fail for signed-out' do
+    patch :update, params: { id: reaction_types(:wfm).id, reaction_type: { name: 'WORKZ', active: false } }
+    assert_response 302
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'update should fail for standard users' do
+    sign_in users(:standard_user)
+    patch :update, params: { id: reaction_types(:wfm).id, reaction_type: { name: 'WORKZ', active: false } }
+    assert_response 404
+  end
+
+  test 'update should fail for privileged standard users' do
+    sign_in users(:closer)
+    patch :update, params: { id: reaction_types(:wfm).id, reaction_type: { name: 'WORKZ', active: false } }
+    assert_response 404
+  end
+
+  test 'update should pass for moderators' do
+    sign_in users(:moderator)
+    patch :update, params: { id: reaction_types(:wfm).id, reaction_type: { name: 'WORKZ', active: false } }
+    assert_response 302
+    assert_redirected_to reactions_path
+    assert_not_nil assigns(:reaction_type)
+    assert_equal false, assigns(:reaction_type).active
+    assert_equal 'WORKZ', assigns(:reaction_type).name
+  end
+
+  test 'new should fail for signed-out' do
+    get :new
+    assert_response 302
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'new should fail for standard users' do
+    sign_in users(:standard_user)
+    get :new
+    assert_response 404
+  end
+
+  test 'new should fail for privileged standard users' do
+    sign_in users(:closer)
+    get :new
+    assert_response 404
+  end
+
+  test 'new should show for moderators' do
+    sign_in users(:moderator)
+    get :new
+    assert_response 200
+    assert_not_nil assigns(:reaction_type)
+  end
+
+  test 'create should fail for signed-out' do
+    data = { name: 'Catastrophic', description: 'Absolutely Terrible', on_post_label: 'Aaaah for',
+      icon: 'aaaaaah-icon', color: 'is-deeppurple is-orangegreen', requires_comment: false,
+      position: 42 }
+    post :create, params: { id: reaction_types(:wfm).id, reaction_type: data }
+    assert_response 302
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'create should fail for standard users' do
+    sign_in users(:standard_user)
+    data = { name: 'Catastrophic', description: 'Absolutely Terrible', on_post_label: 'Aaaah for',
+      icon: 'aaaaaah-icon', color: 'is-deeppurple is-orangegreen', requires_comment: false,
+      position: 42 }
+    post :create, params: { id: reaction_types(:wfm).id, reaction_type: data }
+    assert_response 404
+  end
+
+  test 'create should fail for privileged standard users' do
+    sign_in users(:closer)
+    data = { name: 'Catastrophic', description: 'Absolutely Terrible', on_post_label: 'Aaaah for',
+      icon: 'aaaaaah-icon', color: 'is-deeppurple is-orangegreen', requires_comment: false,
+      position: 42 }
+    post :create, params: { id: reaction_types(:wfm).id, reaction_type: data }
+    assert_response 404
+  end
+
+  test 'create should pass for moderators' do
+    sign_in users(:moderator)
+    data = { name: 'Catastrophic', description: 'Absolutely Terrible', on_post_label: 'Aaaah for',
+      icon: 'aaaaaah-icon', color: 'is-deeppurple is-orangegreen', requires_comment: false,
+      position: 42 }
+    post :create, params: { id: reaction_types(:wfm).id, reaction_type: data }
+    assert_response 302
+    assert_redirected_to reactions_path
+  end
 end
