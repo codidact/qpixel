732c6457d393a23e7f65663893834353349879e7
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2021-11-10 22:45:43 +0000

    Rubocop

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -3,6 +3,6 @@ class Reaction < ApplicationRecord
   belongs_to :user
   belongs_to :post
   belongs_to :comment
-  
+
   default_scope { joins(:reaction_type).where(reaction_types: { community_id: RequestContext.community_id }) }
 end
