0f9998d92f531294b533dede899b724533b2be41
ArtOfCode- <ArtOfCode-@users.noreply.github.com>
2021-11-20 00:06:46 +0000

    Fix email errors

--- scripts/mail_uncaptured_donations.rb
+++ scripts/mail_uncaptured_donations.rb
@@ -6,13 +6,13 @@ intents.auto_paging_each do |pi|
     next unless pi.metadata['user_id'].present?
     next if pi.metadata['emailed'].present?
     user = User.find(pi.metadata['user_id'])
-    symbol = { 'GBP' => '£', 'USD' => '$', 'EUR' => '€' }[pi.currency]
+    symbol = { 'GBP' => '£', 'USD' => '$', 'EUR' => '€' }[pi.currency.upcase]
     amount = pi.amount / 100
     DonationMailer.with(currency: symbol, amount: amount, email: user.email, name: user.username, intent: pi)
                   .donation_uncaptured.deliver_now
     Stripe::PaymentIntent.update(pi.id, { metadata: { emailed: true } })
     puts "Mailed ##{user.id} for PaymentIntent #{pi.id}"
   rescue => ex
-    Stripe::PaymentIntent.update(pi.id, { metadata: { email_error: "#{ex.message}\n#{ex.backtrace}" } })
+    Stripe::PaymentIntent.update(pi.id, { metadata: { email_error: "#{ex.message}" } })
   end
 end
