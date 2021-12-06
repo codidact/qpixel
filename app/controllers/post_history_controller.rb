4ff45461fb9404e98107730e6eb9ad2d7f0e7582
Istiak <58106197+b-Istiak-s@users.noreply.github.com>
2021-12-06 12:28:10 +0600

    Exclude post history with nil user

--- app/controllers/post_history_controller.rb
+++ app/controllers/post_history_controller.rb
@@ -2,6 +2,7 @@ class PostHistoryController < ApplicationController
   def post
     @post = Post.find(params[:id])
     @history = PostHistory.where(post_id: params[:id]).includes(:post_history_type, :user, post_history_tags: [:tag])
+                          .where.not(user_id: nil)
                           .order(created_at: :desc).paginate(per_page: 20, page: params[:page])
     render layout: 'without_sidebar'
   end
