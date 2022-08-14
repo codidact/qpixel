fd568a9fafaea57c9ac9411cb7724efab968db7b
Taico Aerts <t.v.aerts@tudelft.nl>
2022-08-14 22:23:16 +0200

    Add initializer settings

---
+++ config/initializers/elasticsearch.rb
@@ -0,0 +1,11 @@
+# When not using default elasticsearch settings, add them here.
+#
+# Elasticsearch::Model.client = Elasticsearch::Client.new hosts: [
+#   {
+#     host: 'localhost',
+#     port: '9200',
+#     user: 'elastic',
+#     password: 'password',
+#     scheme: 'https'
+#   }
+# ]
