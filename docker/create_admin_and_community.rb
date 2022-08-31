a6da7925fe9f5c6705be217fc532ef2da2f1e112
Taico Aerts <t.v.aerts@tudelft.nl>
2022-08-31 11:19:25 +0200

    Improve the docker-compose setup
    * Confirm account in creation script to remove the need for a manual
      step
    * Fix instruction steps to something that actually works
    * Add information on how to access a rails console

--- docker/create_admin_and_community.rb
+++ docker/create_admin_and_community.rb
@@ -9,6 +9,4 @@ password = ENV['COMMUNITY_ADMIN_PASSWORD'] || 'password'
 email = ENV['COMMUNITY_ADMIN_EMAIL'] || 'codadict@noreply.com'
 
 User.create(username: username, password: password, email: email, is_global_admin: true, is_global_moderator: true,
-            staff: true)
-# You'll need to manually set confirmation for this user
-# $ docker exec qpixel_uwsgi_1 rails runner "User.second.update(confirmed_at: DateTime.now)"
+            staff: true, confirmed_at: DateTime.now)
