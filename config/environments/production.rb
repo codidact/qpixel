7591c7c38276fd052ab0f1b4ab9d29ef1dcfacd2
Taico Aerts <t.v.aerts@tudelft.nl>
2022-09-04 15:46:07 +0200

    Switch to terser for js compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = Terser.new
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
