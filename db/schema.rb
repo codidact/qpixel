e9f5ae97c104a587acdc1682d09185d06e2f8ac4
Taico Aerts <t.v.aerts@tudelft.nl>
2022-09-04 15:46:23 +0200

    Add schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_03_174045) do
+ActiveRecord::Schema[6.1].define(version: 2022_09_03_174045) do
 
   create_table "abilities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "community_id"
