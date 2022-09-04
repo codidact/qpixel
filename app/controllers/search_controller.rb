47facd5b52598360f5ff5d523130d97c4211edcf
Taico Aerts <t.v.aerts@tudelft.nl>
2022-09-04 22:32:29 +0200

    Paginate after search to fix elasticsearch issue

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,18 +3,20 @@ class SearchController < ApplicationController
     @posts = if params[:search].present?
                search_data = helpers.parse_search(params[:search])
                posts = (current_user&.is_moderator || current_user&.is_admin ? Post : Post.undeleted)
-                       .qa_only.list_includes
+                       .qa_only
                posts = helpers.qualifiers_to_sql(search_data[:qualifiers], posts)
-               posts = posts.paginate(page: params[:page], per_page: 25)
 
                if search_data[:search].present?
                  search_score_key = SiteSetting['ElasticsearchEnabled'] ? :es_search_score : :search_score
-                 posts.search(search_data[:search]).user_sort({ term: params[:sort], default: search_score_key },
-                                                              relevance: search_score_key, score: :score, age: :created_at)
+                 posts = posts.search(search_data[:search])
+                              .user_sort({ term: params[:sort], default: search_score_key },
+                                         relevance: search_score_key, score: :score, age: :created_at)
                else
-                 posts.user_sort({ term: params[:sort], default: :score },
-                                 score: :score, age: :created_at)
+                 posts = posts.user_sort({ term: params[:sort], default: :score },
+                                         score: :score, age: :created_at)
                end
+
+               posts.list_includes.paginate(page: params[:page], per_page: 25)
              end
     @count = begin
       @posts&.count
