00235ef76574c0d1ebb7d7d9374b9670b9eacb64
Taico Aerts <t.v.aerts@tudelft.nl>
2022-09-09 12:20:52 +0200

    Replace deprecated method with correct one

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -403,8 +403,8 @@ class PostsController < ApplicationController
       render json: { error: "Images must be one of #{extensions.join(', ')}" }, status: :bad_request
       return
     end
-    @blob = ActiveStorage::Blob.create_after_upload!(io: params[:file], filename: params[:file].original_filename,
-                                                     content_type: params[:file].content_type)
+    @blob = ActiveStorage::Blob.create_and_upload!(io: params[:file], filename: params[:file].original_filename,
+                                                   content_type: params[:file].content_type)
     render json: { link: uploaded_url(@blob.key) }
   end
 
