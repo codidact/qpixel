7d6575e2f11aa0fdcb901e3e782654aee73ec497
Taico Aerts <t.v.aerts@tudelft.nl>
2022-09-12 23:19:45 +0200

    Add ability to create tags for mods
    Closes #180

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,8 @@
 class TagsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :rename, :merge, :select_merge]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :rename, :merge, :select_merge]
   before_action :set_category, except: [:index]
   before_action :set_tag, only: [:show, :edit, :update, :children, :rename, :merge, :select_merge, :nuke, :nuke_warning]
-  before_action :verify_moderator, only: [:rename, :merge, :select_merge]
+  before_action :verify_moderator, only: [:new, :create, :rename, :merge, :select_merge]
   before_action :verify_admin, only: [:nuke, :nuke_warning]
 
   def index
@@ -56,6 +56,21 @@ class TagsController < ApplicationController
     respond_to do |format|
       format.html
       format.rss
+    end
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params.merge(tag_set_id: @category.tag_set.id))
+    if @tag.save
+      flash[:danger] = nil
+      redirect_to tag_path(id: @category.id, tag_id: @tag.id)
+    else
+      flash[:danger] = @tag.errors.full_messages.join(', ')
+      render :new, status: :bad_request
     end
   end
 
@@ -177,7 +192,7 @@ class TagsController < ApplicationController
   end
 
   def tag_params
-    params.require(:tag).permit(:excerpt, :wiki_markdown, :parent_id)
+    params.require(:tag).permit(:excerpt, :wiki_markdown, :parent_id, :name)
   end
 
   def exec(sql_array)
