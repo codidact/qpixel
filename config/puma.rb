b20ef235637b9d442862999050af1aad9e553ed7
Taico Aerts <t.v.aerts@tudelft.nl>
2022-09-12 21:09:07 +0200

    Start with migration to rails 6.0

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,6 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port ENV.fetch("PORT") { 3000 }
-
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
