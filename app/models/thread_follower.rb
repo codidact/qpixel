5ef877bbf7e6bf662571c2ac44ae181ae3eb67ae
ArtOfCode- <hello@artofcode.co.uk>
2022-09-13 21:17:56 +0100

    0.8.0

--- app/models/thread_follower.rb
+++ app/models/thread_follower.rb
@@ -1,4 +1,15 @@
 class ThreadFollower < ApplicationRecord
-  belongs_to :comment_thread
+  belongs_to :comment_thread, optional: true
+  belongs_to :post, optional: true
   belongs_to :user
+
+  validate :thread_or_post
+
+  private
+
+  def thread_or_post
+    if comment_thread.nil? && post.nil?
+      errors.add(:base, 'Must refer to either a comment thread or a post.')
+    end
+  end
 end
