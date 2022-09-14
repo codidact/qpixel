a740af51f31a6fb3087753485a0bcc8cab605f57
MoshiKoi <54333972+MoshiKoi@users.noreply.github.com>
2022-09-14 13:27:38 -0700

    Format both result and options of select

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,5 +1,19 @@
 $(() => {
   QPixel.filters().then(filters => {
+    function template(option) {
+      if (option.id == '') { return 'None'; }
+
+      const filter = filters[option.id];
+      const name = `<span>${option.text}</span>`;
+      const systemIndicator = filter?.system
+        ? ' <span has-font-size-caption">(System)</span>'
+        : '';
+      const newIndicator = !filter
+        ? ' <span has-font-size-caption">(New)</span>'
+        : '';
+      return $(name + systemIndicator + newIndicator);
+    }  
+
     $('.js-filter-select').each((i, el) => {
       const $tgt = $(el);
       const $form = $tgt.closest('form');
@@ -8,14 +22,8 @@ $(() => {
         data: Object.keys(filters),
         tags: true,
         
-        templateResult: option => {
-          const filter = filters[option.id];
-          const name = `<span>${option.text}</span>`;
-          const systemIndicator = filter?.system
-            ? ' <span class=has-color-tertiary-900 has-font-size-caption">(System)</span>'
-            : ''
-          return $(name + systemIndicator);
-        }
+        templateResult: template,
+        templateSelection: template
       }).on('select2:select', evt => {
         const filterName = evt.params.data.id;
         const preset = filters[filterName];
