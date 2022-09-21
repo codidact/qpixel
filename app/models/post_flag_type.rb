141a6d8f750deb71d79e86965493221a341d19e4
Taico Aerts <t.v.aerts@tudelft.nl>
2022-09-21 23:42:12 +0200

    Add missing relation

--- app/models/post_flag_type.rb
+++ app/models/post_flag_type.rb
@@ -1,6 +1,8 @@
 class PostFlagType < ApplicationRecord
   include CommunityRelated
 
+  belongs_to :post_type, optional: true
+
   validates :name, uniqueness: { scope: [:community_id], case_sensitive: false }
 
   scope :not_confidential, -> { where(confidential: false) }
