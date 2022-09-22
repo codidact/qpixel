619ef7c5f4422419d3c4012961f63d7a7cb28ff5
Taico Aerts <t.v.aerts@tudelft.nl>
2022-09-22 11:17:32 +0200

    Revert redis to version 4 until issue is fixed and update settings
    Settings are now properly loaded from the database.yml config file,
    rather than only taking some of those settings.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,8 +24,7 @@ Rails.application.configure do
   redis_config = YAML.safe_load(processed, permitted_classes: [], permitted_symbols: [], aliases: true)["redis_#{Rails.env}"]
   config.cache_store = QPixel::NamespacedEnvCache.new(
     ActiveSupport::Cache::RedisCacheStore.new(
-      url: "redis://#{redis_config['host']}:#{redis_config['port']}",
-      inherit_socket: true,
+      **redis_config.deep_symbolize_keys.merge(reconnect_attempts: 3),
       error_handler: -> (method:, returning:, exception:) {
         Rails.logger.error("Cache error: method=#{method} returning=#{returning} exception=#{exception.message}")
       }
