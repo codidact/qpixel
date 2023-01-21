ce204454af223643aa698ab75591ea552352b668
Taico Aerts <t.v.aerts@tudelft.nl>
2023-01-21 18:30:19 +0100

    Fix migration mistake

--- db/migrate/20220916075849_create_filters.rb
+++ db/migrate/20220916075849_create_filters.rb
@@ -1,4 +1,4 @@
-class AddTagsToFilters < ActiveRecord::Migration[7.0]
+class CreateFilters < ActiveRecord::Migration[7.0]
   def change
     create_table :filters do |t|
       t.references :user, null: false, foreign_key: true
