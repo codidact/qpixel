b770fe11b1b44ac3490b013234d712c9b7d2cd4b
Taico Aerts <t.v.aerts@tudelft.nl>
2023-01-21 21:39:01 +0100

    Add missing dependent destroy

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,5 +1,6 @@
 class Filter < ApplicationRecord
   belongs_to :user
+  has_many :category_filter_defaults, dependent: :destroy
   validates :name, uniqueness: { scope: :user }
   serialize :include_tags, Array
   serialize :exclude_tags, Array
