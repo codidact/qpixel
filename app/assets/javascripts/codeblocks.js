12519bdad40ce61c0527f60fc9804c5765e4b472
MoshiKoi <54333972+MoshiKoi@users.noreply.github.com>
2023-01-22 14:05:38 -0800

    Add some styling

--- app/assets/javascripts/codeblocks.js
+++ app/assets/javascripts/codeblocks.js
@@ -1,11 +1,16 @@
 $(() => {
   $(".post--content pre").each((_, block) => {
     const copyButton = document.createElement('button');
+    copyButton.classList.add('button', 'is-muted', 'is-outlined', 'has-float-right');
     copyButton.innerText = 'Copy';
     copyButton.addEventListener('click', _ => {
       navigator.clipboard.writeText(block.innerText);
+      copyButton.innerText = 'Copied!';
+      setTimeout(() => {
+        copyButton.innerText = 'Copy';
+      }, 2000);
     });
 
-    block.insertAdjacentElement('afterend', copyButton);
+    block.insertAdjacentElement('afterbegin', copyButton);
   });
 });
