7b84fdd98cad844f82e0b23f1103f79ac4a1cb25
MoshiKoi <54333972+MoshiKoi@users.noreply.github.com>
2023-01-22 18:55:38 -0800

    Fix positioning

--- app/assets/javascripts/codeblocks.js
+++ app/assets/javascripts/codeblocks.js
@@ -1,8 +1,14 @@
 $(() => {
   $(".post--content pre > code").each((_, block) => {
     const wrapper = block.parentElement;
+
+    const wrapperDiv = document.createElement('div');
+    wrapperDiv.style.position = 'relative';
+
     const copyButton = document.createElement('button');
-    copyButton.classList.add('button', 'is-muted', 'is-outlined', 'has-float-right');
+    copyButton.style.position = 'absolute';
+    copyButton.style.right = 0;
+    copyButton.classList.add('copy-button', 'button', 'is-muted', 'is-outlined', 'has-margin-2');
     copyButton.innerText = 'Copy';
     copyButton.addEventListener('click', _ => {
       navigator.clipboard.writeText(block.innerText);
@@ -12,6 +18,7 @@ $(() => {
       }, 2000);
     });
 
-    wrapper.insertAdjacentElement('afterbegin', copyButton);
+    wrapper.insertAdjacentElement('beforebegin', wrapperDiv);
+    wrapperDiv.append(copyButton, wrapper);
   });
 });
