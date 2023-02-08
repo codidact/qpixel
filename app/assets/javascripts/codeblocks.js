4fa0c206c61671d62558e95e3c407a76f98b219e
MoshiKoi <54333972+MoshiKoi@users.noreply.github.com>
2023-02-07 16:00:07 -0800

    Use jQuery

--- app/assets/javascripts/codeblocks.js
+++ app/assets/javascripts/codeblocks.js
@@ -1,24 +1,16 @@
 $(() => {
-  $(".post--content pre > code").each((_, block) => {
-    const wrapper = block.parentElement;
-
-    const wrapperDiv = document.createElement('div');
-    wrapperDiv.style.position = 'relative';
-
-    const copyButton = document.createElement('button');
-    copyButton.style.position = 'absolute';
-    copyButton.style.right = 0;
-    copyButton.classList.add('copy-button', 'button', 'is-muted', 'is-outlined', 'has-margin-2');
-    copyButton.innerText = 'Copy';
-    copyButton.addEventListener('click', _ => {
-      navigator.clipboard.writeText(block.innerText);
-      copyButton.innerText = 'Copied!';
-      setTimeout(() => {
-        copyButton.innerText = 'Copy';
-      }, 2000);
-    });
-
-    wrapper.insertAdjacentElement('beforebegin', wrapperDiv);
-    wrapperDiv.append(copyButton, wrapper);
+  $(".post--content pre > code")
+    .parent()
+    .each(function() {
+      const content = $(this).text()
+      $(this)
+        .wrap('<div style="position:relative;"></div>')
+        .parent()
+        .prepend($('<button class="copy-button button is-muted is-outlined has-margin-2">Copy</button>')
+          .click(function () {
+            navigator.clipboard.writeText(content);
+            $(this).text('Copied!');
+            setTimeout(() => { $(this).text('Copy'); }, 2000);
+          }))
   });
 });
