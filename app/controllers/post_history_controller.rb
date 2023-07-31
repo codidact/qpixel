957d199d1661fbe99bcf72407759c1da9372a590
Taico Aerts <t.v.aerts@tudelft.nl>
2023-07-31 21:26:52 +0200

    Add restoration to specific event logic

--- app/controllers/post_history_controller.rb
+++ app/controllers/post_history_controller.rb
@@ -67,11 +67,126 @@ class PostHistoryController < ApplicationController
   def revert_to
     @post = Post.find(params[:post_id])
     @history = PostHistory.find(params[:id])
-    after_histories = @post.post_histories
-                           .where(created_at: @history.created_at..)
-                           .where.not(id: @history.id)
-                           .includes(:post_history_type)
-                           .order(created_at: :desc, id: :desc)
+
+    msg = disallowed_to_restore(@post, @history, current_user)
+    if msg.present?
+      flash[:danger] = "You are not allowed to revert to this history element: #{msg}"
+      redirect_to post_history_url(@post)
+      return
+    end
+
+    revert_to_state(@post, @history)
+  end
+
+  protected
+
+  # Reverts the given post to the state of the given history item by using the aggregated changes from to_change
+  # @param post [Post]
+  # @param history [PostHistory]
+  def revert_to_state(post, history)
+    to_change = determine_changes_to_restore(post, history)
+    revert_comment = "Reverting to [##{index}: #{history.post_history_type.name.humanize}]" \
+                     "(#{post_history_url(post, anchor: index)})"
+
+    # Perform edit
+    if to_change[:title].present? || to_change[:body].present? || to_change[:tags].present?
+      opts = {
+        before: post.body_markdown, before_title: post.title, before_tags: post.tags.to_a,
+        comment: revert_comment
+      }
+
+      post.title = to_change[:title] if to_change[:title].present?
+      if to_change[:body].present?
+        post.body_markdown = to_change[:body]
+        post.body = ApplicationController.helpers.render_markdown(to_change[:body])
+      end
+      post.tags_cache = to_change[:tags].map(&:name) if to_change[:tags].present?
+      post.last_activity = DateTime.now
+      post.last_activity_by = current_user
+      post.save
+
+      opts = opts.merge(after: post.body_markdown, after_title: post.title, after_tags: post.tags.to_a)
+
+      PostHistory.post_edited(post, current_user, **opts)
+    end
+
+    # Perform close/reopen
+    unless to_change[:closed].nil?
+      if to_change[:closed]
+        post.update(closed: true, closed_by: current_user, closed_at: DateTime.now,
+                    close_reason_id: to_change[:close_reason_id],
+                    duplicate_post_id: to_change[:duplicate_post_id],
+                    last_activity: DateTime.now, last_activity_by: current_user)
+        PostHistory.question_closed(post, current_user, comment: revert_comment,
+                                    close_reason_id: to_change[:close_reason_id],
+                                    duplicate_post_id: to_change[:duplicate_post_id])
+      else
+        post.update(closed: false, closed_by: nil, closed_at: nil, close_reason: nil, duplicate_post: nil,
+                    last_activity: DateTime.now, last_activity_by: current_user)
+        PostHistory.question_reopened(post, current_user, comment: revert_comment)
+      end
+    end
+
+    # Perform deletion / undeletion
+    unless to_change[:deleted].nil?
+      if to_change[:deleted]
+        post.update(deleted: true, deleted_at: DateTime.now, deleted_by: current_user,
+                    last_activity: DateTime.now, last_activity_by: current_user)
+        PostHistory.post_deleted(post, current_user, comment: revert_comment)
+      else
+        post.update(deleted: false, deleted_at: nil, deleted_by: nil,
+                    last_activity: DateTime.now, last_activity_by: current_user)
+        PostHistory.post_undeleted(post, current_user, comment: revert_comment)
+      end
+    end
+  end
+
+  # This check is based on checking whether the user would be allowed to undo each of the history items that came after
+  # the given one.
+  #
+  # @param post [Post]
+  # @param history [PostHistory]
+  # @param user [User]
+  # @return [String, Nil] if the user is not allowed to restore, the message why not. Nil if the user is allowed to.
+  def disallowed_to_restore(post, history, user)
+    after_histories = post.post_histories
+                          .where(created_at: history.created_at..)
+                          .where.not(id: history.id)
+                          .includes(:post_history_type)
+                          .order(created_at: :desc, id: :desc)
+
+    after_histories.each do |ph|
+      msg = helpers.disallow_rollback_history(ph, user)
+      return msg if msg
+
+      if ['imported_from_external_source', 'initial_revision'].include?(ph.post_history_type.name)
+        return "Events of the type #{ph.post_history_type.name.humanize} cannot be rolled back"
+      end
+    end
+
+    nil
+  end
+
+  # Determines the set of changes to apply to the post to revert back to the state it had at the given history item.
+  #
+  # The returned hash can contain:
+  #   deleted [Boolean] - whether the post should now be deleted
+  #   closed [Boolean] - whether the post should be closed
+  #   title [String] - the title the post should be updated to have
+  #   body [String] - the body the post should be updated to have
+  #   tags [Array<Tag>] - the tags the post should be updated to have
+  #   close_reason_id [Integer] - the close reason that should be set
+  #   duplicate_post_id [Integer] - the id of the post of which this post is a duplicate that should be set
+  #
+  # @param post [Post]
+  # @param history [PostHistory]
+  # @return [Hash]
+  def determine_changes_to_restore(post, history)
+    after_histories = post.post_histories
+                          .where(created_at: history.created_at..)
+                          .where.not(id: history.id)
+                          .includes(:post_history_type)
+                          .order(created_at: :desc, id: :desc)
 
     to_change = {}
     after_histories.each do |ph|
@@ -88,73 +203,18 @@ class PostHistoryController < ApplicationController
         to_change[:title] = ph.before_title if ph.before_title.present?
         to_change[:body] = ph.before_state if ph.before_state.present?
         to_change[:tags] = ph.before_tags if ph.before_tags.present?
+      when 'imported_from_external_source', 'initial_revision'
+        raise ArgumentError('Unexpected event in history revert request!')
       end
     end
 
     # Recover post close reason from predecessor of last reopened
     if to_change[:closed]
       ph = find_predecessor('question_closed', after_histories.last)
-      to_change[:close_reason_id] ||= ph.extra&.fetch('close_reason_id', nil)
-      to_change[:duplicate_post_id] ||= ph.extra&.fetch('duplicate_post_id', nil)
+      to_change[:close_reason_id] = ph&.close_reason_id
+      to_change[:duplicate_post_id] = ph&.duplicate_post_id
     end
-
-    revert_comment = "Reverting to [##{index}: #{@history.post_history_type.name.humanize}]" \
-                     "(#{post_history_url(@post, anchor: index)})"
-
-    # Perform edit
-    if to_change[:title].present? || to_change[:body].present? || to_change[:tags].present?
-      opts = {
-        before: @post.body_markdown, before_title: @post.title, before_tags: @post.tags.to_a,
-        comment: revert_comment
-      }
-
-      @post.title = to_change[:title] if to_change[:title].present?
-      if to_change[:body].present?
-        @post.body_markdown = to_change[:body]
-        @post.body = ApplicationController.helpers.render_markdown(to_change[:body])
-      end
-      @post.tags_cache = to_change[:tags].map(&:name) if to_change[:tags].present?
-      @post.last_activity = DateTime.now
-      @post.last_activity_by = current_user
-      @post.save
-
-      opts = opts.merge(after: @post.body_markdown, after_title: @post.title, after_tags: @post.tags.to_a)
-
-      PostHistory.post_edited(@post, current_user, **opts)
-    end
-
-    # Perform reopen
-    unless to_change[:closed].nil?
-      if to_change[:closed]
-        # Close
-        @post.update(closed: true, closed_by: current_user, closed_at: DateTime.now,
-                     close_reason_id: to_change[:close_reason_id],
-                     duplicate_post_id: to_change[:duplicate_post_id],
-                     last_activity: DateTime.now, last_activity_by: current_user)
-        PostHistory.question_closed(@post, current_user, comment: revert_comment,
-                                    extra: {
-                                      close_reason_id: to_change[:close_reason_id],
-                                      duplicate_post_id: to_change[:duplicate_post_id]
-                                    })
-      else
-        # Reopen
-        @post.update(closed: false, closed_by: nil, closed_at: nil, close_reason: nil, duplicate_post: nil,
-                     last_activity: DateTime.now, last_activity_by: current_user)
-        PostHistory.question_reopened(@post, current_user, comment: revert_comment)
-      end
-    end
-
-    unless to_change[:deleted].nil?
-      if to_change[:deleted]
-        @post.update(deleted: true, deleted_at: DateTime.now, deleted_by: current_user,
-                     last_activity: DateTime.now, last_activity_by: current_user)
-        PostHistory.post_deleted(@post, current_user, comment: revert_comment)
-      else
-        @post.update(deleted: false, deleted_at: nil, deleted_by: nil,
-                     last_activity: DateTime.now, last_activity_by: current_user)
-        PostHistory.post_undeleted(@post, current_user, comment: revert_comment)
-      end
-    end
+    to_change
   end
 
   private
