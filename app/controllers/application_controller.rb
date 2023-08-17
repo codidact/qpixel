cee7cb4a7807c51306fe2aede2cc14be955c5ec6
Taico Aerts <t.v.aerts@tudelft.nl>
2023-08-17 22:45:36 +0200

    Remove method that is no longer necessary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -365,21 +365,6 @@ class ApplicationController < ActionController::Base
     stored_location_for(resource) || root_path
   end
 
-  # Checks if the requested location should be stored.
-  #
-  # Its important that the location is NOT stored if:
-  # - The request method is not GET (non idempotent)
-  # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
-  #    infinite redirect loop.
-  # - The request is an Ajax request as this can lead to very unexpected behaviour.
-  def storable_location?
-    request.get? &&
-      is_navigational_format? &&
-      !devise_controller? &&
-      !request.xhr? &&
-      !request.path.start_with?('/users/')
-  end
-
   def block_write_request(**add)
     respond_to do |format|
       format.html do
