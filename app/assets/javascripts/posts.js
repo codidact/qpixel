f4f519218c90a4b1eed1ac2c046c58443dd123e3
Oleg Valter <o.a.philippov@gmail.com>
2023-10-22 14:13:43 +0300

    remove no longer needed js-permalink JS handling

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -260,19 +260,6 @@ $(() => {
     </div>`);
   });
 
-  $('.js-permalink > .js-text').text('Copy Link');
-  $('.js-permalink').on('click', ev => {
-    ev.preventDefault();
-
-    const $tgt = $(ev.target).is('a') ? $(ev.target) : $(ev.target).parents('a');
-    const link = $tgt.attr('href');
-    navigator.clipboard.writeText(link);
-    $tgt.find('.js-text').text('Copied!');
-    setTimeout(() => {
-      $tgt.find('.js-text').text('Copy Link');
-    }, 1000);
-  });
-
   const setCopyButtonState = ($button, state) => {
     const isSuccess = state === "success";
     const buttonClass = isSuccess ? "is-green" : "is-danger";
