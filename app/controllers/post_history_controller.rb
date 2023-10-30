4767da33aabe5a95c1759c2a42a36cc8c9ca0df9
Oleg Valter <o.a.philippov@gmail.com>
2023-10-30 12:44:43 +0300

    expose number of pages in PostHistory controller

--- app/controllers/post_history_controller.rb
+++ app/controllers/post_history_controller.rb
@@ -10,11 +10,15 @@ class PostHistoryController < ApplicationController
                             .includes(:post_history_type, :user, post_history_tags: [:tag])
                             .order(created_at: :desc, id: :desc)
 
-    @history = base_query.paginate(per_page: 20, page: params[:page])
+    per_page = 20
+
+    @history = base_query.paginate(per_page: per_page, page: params[:page])
 
     @count = base_query.count
 
     @page = params[:page].nil? ? 1 : params[:page].to_i
+
+    @pages = (@count.to_f / per_page).ceil
 
     render layout: 'without_sidebar'
   end
