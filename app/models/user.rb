0990efb6a5fa7559f44d9d796c5d16316880cfde
Monica Cellio <cellio@pobox.com>
2023-12-31 00:03:27 -0500

    make user checks null-safe

--- app/models/user.rb
+++ app/models/user.rb
@@ -123,45 +123,12 @@ class User < ApplicationRecord
     is_global_admin || community_user&.is_admin || false
   end
 
-
   def has_ability_on(community_id, ability_internal_id)
-    if is_admin || is_global_moderator
-      return true
-    end
-
-    ability = Ability.where(community_id: community_id).find_by internal_id: ability_internal_id
-
-    if ability.nil?
-      return false
-    end
-
-    cu = community_users.find { |cuf| cuf.community_id == community_id && cuf.id == id }
-
-    if cu.nil?
-      return false
-    end
-
-    if cu.is_moderator || cu.privilege('mod')
-      return true
-    end
-
     cu = community_users.joins(:community).where(communities: { id: community_id }).first
-    if is_admin || is_global_moderator || cu.is_moderator 
+    if is_admin || is_global_moderator || cu&.is_moderator
       true
     else
-      UserAbility.joins(:ability).where(community_user_id: cu.id, is_suspended: false, ability: { internal_id: ability_internal_id }).exists?
-    end
-
-    UserAbility.where(community_user_id: cu.id, ability_id: ability.id, is_suspended: false).exists?
-  end
-
-  
-  def has_ability_on_art(community_id, ability_internal_id)
-    cu = community_users.joins(:community).where(communities: { id: community_id }).first
-    if is_admin || is_global_moderator || cu.is_moderator
-      true
-    else
-      UserAbility.joins(:ability).where(community_user_id: cu.id, is_suspended: false,
+      UserAbility.joins(:ability).where(community_user_id: cu&.id, is_suspended: false,
                                         ability: { internal_id: ability_internal_id }).exists?
     end
   end
