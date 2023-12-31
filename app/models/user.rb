45e5dfc2e1c5d45f0025607d7e91c2605d795db4
Monica Cellio <cellio@pobox.com>
2023-12-30 23:21:18 -0500

    checkpoint: restored original version of function, which doesn't emit errors on a multi-community local setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Represents a user. Most of the User's logic is controlled by Devise and its overrides. A user, as far as the
 # application code (i.e. excluding Devise) is concerned, has many questions, answers, and votes.
 class User < ApplicationRecord
@@ -122,7 +123,40 @@ class User < ApplicationRecord
     is_global_admin || community_user&.is_admin || false
   end
 
+
   def has_ability_on(community_id, ability_internal_id)
+    if is_admin || is_global_moderator
+      return true
+    end
+
+    ability = Ability.where(community_id: community_id).find_by internal_id: ability_internal_id
+
+    if ability.nil?
+      return false
+    end
+
+    cu = community_users.find { |cuf| cuf.community_id == community_id && cuf.id == id }
+
+    if cu.nil?
+      return false
+    end
+
+    if cu.is_moderator || cu.privilege('mod')
+      return true
+    end
+
+    cu = community_users.joins(:community).where(communities: { id: community_id }).first
+    if is_admin || is_global_moderator || cu.is_moderator 
+      true
+    else
+      UserAbility.joins(:ability).where(community_user_id: cu.id, is_suspended: false, ability: { internal_id: ability_internal_id }).exists?
+    end
+
+    UserAbility.where(community_user_id: cu.id, ability_id: ability.id, is_suspended: false).exists?
+  end
+
+  
+  def has_ability_on_art(community_id, ability_internal_id)
     cu = community_users.joins(:community).where(communities: { id: community_id }).first
     if is_admin || is_global_moderator || cu.is_moderator
       true
