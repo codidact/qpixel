070dcbf1cedd55e618c744587a6eadbbc61c8759
Oleg Valter <o.a.philippov@gmail.com>
2024-01-03 22:34:51 +0300

    internationalize post history validation errors

--- app/models/concerns/post_history_validations.rb
+++ app/models/concerns/post_history_validations.rb
@@ -8,8 +8,10 @@ module PostHistoryValidations
 
   def max_edit_comment_length
     max_edit_comment_length = SiteSetting['MaxEditCommentLength']
-    if comment.length > [(max_edit_comment_length || 255), 255].min
-      errors.add(:comment, "can't be more than #{max_edit_comment_length} characters")
+    max_length = [(max_edit_comment_length || 255), 255].min
+    if comment.length > max_length
+      msg = I18n.t('post_histories.max_edit_comment_length').gsub(':length', max_length.to_s)
+      errors.add(:base, msg)
     end
   end
 end
