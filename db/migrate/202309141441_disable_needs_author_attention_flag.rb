92a3ade1c3920523286d17156d93b88fba29933f
Tomas Peterka <tomas@peterka.me>
2024-04-08 23:03:55 +0200

    make it directly usable in Docker

--- db/migrate/202309141441_disable_needs_author_attention_flag.rb
+++
@@ -1,9 +0,0 @@
-class DisableNeedsAuthorAttentionFlag < ActiveRecord::Migration[7.0]
-  def up
-    PostFlagType.unscoped.where(name: "needs author's attention").update_all(active: false)
-  end
-
-  def down
-    PostFlagType.unscoped.where(name: "needs author's attention").update_all(active: true)
-  end
-end
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+set -x
 
 # If not created yet
-if [ ! -f "/db-created" ]; then
+if [ ! -f "/code/tmp/db-created" ]; then
     rails db:create
     rails db:schema:load
-    rails r db/scripts/create_tags_path_view.rb
     rails db:migrate
-    rails db:migrate RAILS_ENV=development
+    rails r db/scripts/create_tags_path_view.rb
     rails r docker/create_admin_and_community.rb
     UPDATE_POSTS=true rails db:seed
-    touch /db-created
+    touch /code/tmp/db-created
 fi
 
 # If this isn't done again, there is a 500 error on the first page about posts
