769f6c86eaecff72f251726e85460c1b86abf3be
trichoplax <xalpohcirt@gmail.com>
2024-06-07 18:54:49 +0100

    Use 'failban' rather than 'hellban' throughout code for consistency
    (cherry picked from commit 3b225c25fde5c85143df192d96e0834266a9bea0)

--- config/routes.rb
+++ config/routes.rb
@@ -226,7 +226,7 @@ Rails.application.routes.draw do
     get    '/:id/mod/annotations',      to: 'users#annotations', as: :user_annotations
     post   '/:id/mod/annotations',      to: 'users#annotate', as: :annotate_user
     get    '/:id/mod/activity-log',     to: 'users#full_log', as: :full_user_log
-    post   '/:id/hellban',              to: 'admin#hellban', as: :hellban_user
+    post   '/:id/failban',              to: 'admin#failban', as: :failban_user
     get    '/:id/mod/delete',           to: 'users#mod_delete', as: :mod_delete
     get    '/:id/mod/delete-network-account', to: 'users#mod_delete_network_account', as: :mod_delete_network_account
     get    '/:id/mod/failban',          to: 'users#mod_failban', as: :mod_failban
