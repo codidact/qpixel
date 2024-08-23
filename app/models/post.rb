3cee4149b1232b527ae72a6cf8079a6a5a321ee6
Monica Cellio <cellio@pobox.com>
2024-08-23 10:53:31 -0400

    don't try to update rep for the nil user

--- app/models/post.rb
+++ app/models/post.rb
@@ -260,10 +260,12 @@ class Post < ApplicationRecord
     sc = saved_changes
     if sc.include?('deleted') && sc['deleted'][0] != sc['deleted'][1] && created_at >= 60.days.ago
       deleted = !!saved_changes['deleted']&.last
-      if deleted
-        user.update(reputation: user.reputation - Vote.total_rep_change(votes))
-      else
-        user.update(reputation: user.reputation + Vote.total_rep_change(votes))
+      if user
+        if deleted
+          user.update(reputation: user.reputation - Vote.total_rep_change(votes))
+        else
+          user.update(reputation: user.reputation + Vote.total_rep_change(votes))
+        end
       end
     end
   end
