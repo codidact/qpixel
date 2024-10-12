8fa4f36311ea1620262f0b34c24bbde54ddcefcc
ArtOfCode- <hello@artofcode.co.uk>
2024-10-12 14:15:25 +0100

    Add a QPixel DOM convenience API

--- app/assets/javascripts/micro_auth/apps.js
+++ app/assets/javascripts/micro_auth/apps.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.js-copy-key').forEach(el => {
-    el.addEventListener('click', ev => {
-      const label = ev.target.closest('label');
-      const field = document.querySelector(`#${label.getAttribute('for')}`);
-      navigator.clipboard.writeText(field.value);
-      field.focus();
-      field.setSelectionRange(0, field.value.length);
-    });
+  QPixel.DOM.addSelectorListener('click', '.js-copy-key', ev => {
+    const label = ev.target.closest('label');
+    const field = document.querySelector(`#${label.getAttribute('for')}`);
+    navigator.clipboard.writeText(field.value);
+    field.focus();
+    field.setSelectionRange(0, field.value.length);
   });
 });
---
+++ app/assets/javascripts/qpixel_dom.js
@@ -0,0 +1,45 @@
+window.QPixel = window.QPixel || {};
+
+/**
+ * @callback eventCallback
+ * @param {Event} event The browser event.
+ */
+
+QPixel.DOM = {
+  _delegatedListeners: [],
+  _eventListeners: {},
+
+  /**
+   * Add a delegated event listener. Use when an event listener is required that will fire for elements added to the
+   * DOM dynamically after the delegated listener is added.
+   * @param {string} event An event name to listen for.
+   * @param {string} selector A CSS selector representing elements on which to apply the listener.
+   * @param {eventCallback} callback A callback function to pass to the event listener.
+   */
+  addDelegatedListener: (event, selector, callback) => {
+    if (!QPixel.DOM._eventListeners[event]) {
+      const listener = ev => {
+        QPixel.DOM._delegatedListeners.filter(x => x.event === event).forEach(listener => {
+          if (ev.target.matches(listener.selector)) {
+            listener.callback(ev);
+          }
+        });
+      };
+      document.addEventListener(event, listener);
+      QPixel.DOM._eventListeners[event] = listener;
+    }
+    QPixel.DOM._delegatedListeners.push({ event, selector, callback });
+  },
+
+  /**
+   * Convenience method. Add an event listener to _all_ elements that currently match a selector.
+   * @param {string} event An event name to listen for.
+   * @param {string} selector A CSS selector representing elements on which to apply the listener.
+   * @param {eventCallback} callback A callback function to pass to the event listener.
+   */
+  addSelectorListener: (event, selector, callback) => {
+    document.querySelectorAll(selector).forEach(el => {
+      el.addEventListener(event, callback);
+    });
+  }
+};
