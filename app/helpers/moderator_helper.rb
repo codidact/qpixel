b2dccce633e832708d6c48acd283b7b620d2bf99
Oleg Valter <o.a.philippov@gmail.com>
2024-10-27 02:03:03 +0300

    I swear, officer, I wasn't making my lines too long

--- app/helpers/moderator_helper.rb
+++ app/helpers/moderator_helper.rb
@@ -4,8 +4,8 @@ module ModeratorHelper
   # Display text on a specified background color.
   # @param cls [String] The background color class.
   # @param content [String] The text to display. For uses beyond simple text, pass a block instead.
-  # @option opts :class [String] Additional classes to add to the element. For instance, if the background color is dark,
-  #   consider passing a class for a light text color.
+  # @option opts :class [String] Additional classes to add to the element.
+  # For instance, if the background color is dark, consider passing a class for a light text color.
   # @yieldparam context [ActionView::Helpers::TagHelper::TagBuilder]
   # @yieldreturn [ActiveSupport::SafeBuffer, String]
   # @return [ActiveSupport::SafeBuffer]
