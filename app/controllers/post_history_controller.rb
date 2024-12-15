a73ada31524a0098485e4a2fbf79cd417960991f
Oleg Valter <o.a.philippov@gmail.com>
2024-12-15 17:48:43 +0300

    added post history routes for help & policy posts

--- app/controllers/post_history_controller.rb
+++ app/controllers/post_history_controller.rb
@@ -8,7 +8,23 @@ class PostHistoryController < ApplicationController
 
     @history = PostHistory.where(post_id: params[:id])
                           .includes(:post_history_type, :user, post_history_tags: [:tag])
-                          .order(created_at: :desc, id: :desc).paginate(per_page: 20, page: params[:page])
+                          .order(created_at: :desc, id: :desc)
+                          .paginate(per_page: 20, page: params[:page])
     render layout: 'without_sidebar'
+  end
+
+  def slug_post
+    @post = Post.by_slug(params[:slug], current_user)
+
+    if @post.nil?
+      return not_found
+    end
+
+    @history = PostHistory.where(post_id: @post.id)
+                          .includes(:post_history_type, :user)
+                          .order(created_at: :desc, id: :desc)
+                          .paginate(per_page: 20, page: params[:page])
+
+    render 'post_history/post', layout: 'without_sidebar', locals: { show_content: false }
   end
 end
