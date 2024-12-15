8608aad278f9ca107cbae452d4dfd9d1c8f85e25
Oleg Valter <o.a.philippov@gmail.com>
2024-12-15 15:09:44 +0300

    added test for TagSet's model with_paths method

--- test/models/tag_set_test.rb
+++ test/models/tag_set_test.rb
@@ -6,4 +6,13 @@ class TagSetTest < ActiveSupport::TestCase
   test 'is community related' do
     assert_community_related(TagSet)
   end
+
+  test 'with_paths method should respect no_excerpt' do
+    main = TagSet.main
+
+    all = main.with_paths.size
+    excerptless = main.with_paths(true).size
+
+    assert_not_equal(all, excerptless)
+  end
 end
