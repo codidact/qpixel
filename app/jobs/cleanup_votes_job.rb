c1cbe8ff446c029274e960d76006de4e95247d10
Oleg Valter <o.a.philippov@gmail.com>
2024-12-18 16:37:47 +0300

    added audit log for when the cleanup_votes job successfully deletes orphaned votes

--- app/jobs/cleanup_votes_job.rb
+++ app/jobs/cleanup_votes_job.rb
@@ -5,12 +5,27 @@ class CleanupVotesJob < ApplicationJob
     Community.all.each do |c|
       RequestContext.community = c
       orphan_votes = Vote.all.reject { |v| v.post.present? }
-      puts "[#{c.name}] destroying #{orphan_votes.length} #{'orphan vote'.pluralize(orphan_votes.length)}"
-      failed = orphan_votes.reject(&:destroy)
 
-      failed.each do |v|
-        puts "[#{c.name}] failed to destroy vote \"#{v.id}\""
-        v.errors.each { |e| puts e.full_message }
+      puts "[#{c.name}] destroying #{orphan_votes.length} #{'orphan vote'.pluralize(orphan_votes.length)}"
+
+      system_user = User.find(-1)
+
+      orphan_votes.each do |v|
+        result = v.destroy
+
+        if result
+          AuditLog.admin_audit(
+            comment: "Deleted orphaned vote for user ##{v.recv_user_id} " \
+                     "on post ##{v.post_id} " \
+                     "in community ##{c.id} (#{c.name})",
+            event_type: 'vote_delete',
+            related: v,
+            user: system_user
+          )
+        else
+          puts "[#{c.name}] failed to destroy vote \"#{v.id}\""
+          v.errors.each { |e| puts e.full_message }
+        end
       end
     end
   end
