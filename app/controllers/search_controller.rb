6b2e047db2d57f45dff5a894f528f1b23d7f3bcb
Oleg Valter <o.a.philippov@gmail.com>
2025-01-08 11:32:50 +0300

    trying to search for user:me when signed out should flash an error in the widget

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,11 @@
 class SearchController < ApplicationController
   def search
     @posts, @qualifiers = helpers.search_posts
+
+    @signed_out_me = @qualifiers.any? { |q| q[:param] == :user && q[:user_id].zero? }
+
     @active_filter = helpers.active_filter
+
     @count = begin
       @posts&.count
     rescue
