704feaaed74837047e26b3c3cb668827cb494355
Oleg Valter <o.a.philippov@gmail.com>
2025-02-02 00:18:45 +0300

    user_id is implicit when accessing user.user_websites

--- app/tasks/maintenance/initialize_user_websites_task.rb
+++ app/tasks/maintenance/initialize_user_websites_task.rb
@@ -7,14 +7,15 @@ module Maintenance
     end
 
     def process(user)
-      unless user.user_websites.where(user_id: user.id, position: 1).size.positive?
+      unless user.user_websites.where(position: 1).size.positive?
         if user.website.present?
           UserWebsite.create!(user_id: user.id, position: 1, label: 'website', url: user.website)
         else
           UserWebsite.create!(user_id: user.id, position: 1)
         end
       end
-      unless user.user_websites.where(user_id: user.id, position: 2).size.positive?
+
+      unless user.user_websites.where(position: 2).size.positive?
         if user.twitter.present?
           UserWebsite.create!(user_id: user.id, position: 2, label: 'Twitter',
                               url: "https://twitter.com/#{user.twitter}")
@@ -22,8 +23,9 @@ module Maintenance
           UserWebsite.create!(user_id: user.id, position: 2)
         end
       end
+
       # This check *should* be superfluous, but just in case...
-      unless user.user_websites.where(user_id: user.id, position: 3).size.positive?
+      unless user.user_websites.where(position: 3).size.positive?
         UserWebsite.create!(user_id: user.id, position: 3)
       end
     end
