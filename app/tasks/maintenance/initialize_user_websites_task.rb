23c1a021a0c79aecf3d00cfc57dd08a363064d87
Oleg Valter <o.a.philippov@gmail.com>
2025-02-02 00:10:15 +0300

    removed unnecessary comments from user websites maintenance tasks & empty 'count'

--- app/tasks/maintenance/initialize_user_websites_task.rb
+++ app/tasks/maintenance/initialize_user_websites_task.rb
@@ -3,15 +3,10 @@
 module Maintenance
   class InitializeUserWebsitesTask < MaintenanceTasks::Task
     def collection
-      # Collection to be iterated over
-      # Must be Active Record Relation or Array
       User.all
     end
 
     def process(user)
-      # The work to be done in a single iteration of the task.
-      # This should be idempotent, as the same element may be processed more
-      # than once if the task is interrupted and resumed.
       unless user.user_websites.where(user_id: user.id, position: 1).size.positive?
         if user.website.present?
           UserWebsite.create!(user_id: user.id, position: 1, label: 'website', url: user.website)
@@ -31,11 +26,6 @@ module Maintenance
       unless user.user_websites.where(user_id: user.id, position: 3).size.positive?
         UserWebsite.create!(user_id: user.id, position: 3)
       end
-    end
-
-    def count
-      # Optionally, define the number of rows that will be iterated over
-      # This is used to track the task's progress
     end
   end
 end
