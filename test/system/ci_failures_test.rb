54243ab5626419bfb1d67636ce4635eda46e6f22
trichoplax <xalpohcirt@gmail.com>
2025-03-24 17:16:58 +0000

    Add tests to see what users see

--- test/system/ci_failures_test.rb
+++ test/system/ci_failures_test.rb
@@ -4,4 +4,32 @@ class CIFailuresTest < ApplicationSystemTestCase
   test 'empty test to start with' do
     assert true
   end
+
+  test 'temporary test to analyse CI failures what does basic user see in main' do
+    category = categories(:main)
+    log_in :basic_user
+    visit category_path(category)
+    assert_text 'Nonexistent to force screenshot'
+  end
+
+  test 'temporary test to analyse CI failures what does standard user see in main' do
+    category = categories(:main)
+    log_in :standard_user
+    visit category_path(category)
+    assert_text 'Nonexistent to force screenshot'
+  end
+
+  test 'temporary test to analyse CI failures what does standard user see after adding post to meta' do
+    category = categories(:meta)
+    log_in :standard_user
+    visit category_path(category)
+    post_title = 'Test title text for testing threads'
+    create_post('Test body text for testing threads.', post_title)
+    assert_text 'Unfollow new'  # Ensure post has finished saving to avoid timing problems
+
+    log_out
+    log_in :standard_user
+    visit category_path(category)
+    assert_text 'Nonexistent to force screenshot'
+  end
 end
