4427dc38f048b88ac6e726434f4f5a661a75d38e
Oleg Valter <o.a.philippov@gmail.com>
2025-03-25 03:56:05 +0300

    removed unused SeedsHelper#update_from_seeds method

--- app/helpers/seeds_helper.rb
+++ app/helpers/seeds_helper.rb
@@ -1,12 +1,4 @@
 module SeedsHelper
-  def update_from_seeds(type, unique_key, value_attribute)
-    data = YAML.load_file(Rails.root.join("db/seeds/#{type}.yml"))
-    cls = type.to_s.singularize.classify.constantize
-    data.each do |seed|
-      cls.unscoped.where(unique_key => seed[unique_key.to_s]).update(value_attribute => seed[value_attribute.to_s])
-    end
-  end
-
   # Gets the list of seed file paths
   # @param seed_name [String, nil] optional specific seed name to run
   # @return [Array<String>]
