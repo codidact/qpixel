2d5c55a0eadec334648ba79d2e7d4cf80fc464d9
Oleg Valter <o.a.philippov@gmail.com>
2025-05-08 22:58:00 +0300

    fixed ability editing input not being appended

--- app/assets/javascripts/privileges.js
+++ app/assets/javascripts/privileges.js
@@ -14,11 +14,14 @@ $(() => {
     const resp = await fetch(`/admin/privileges/${name}`, {
       credentials: 'include'
     });
-    const data = await resp.json();
-    const value = data[`${type}_score_threshold`];
 
+    const data = await resp.json();
+
+    
+    const value = data[`${type}_score_threshold`];
+    
     const form = editField.clone().val(value ? value.toString() : '').attr('data-name', name).attr('data-type', type);
-    $tgt.addClass('editing').html(form.html()).append(`<button class="button is-filled js-privilege-submit">Update</button>`);
+    $tgt.addClass('editing').html(form[0]).append(`<button class="button is-filled js-privilege-submit">Update</button>`);
   });
 
   $(document).on('click', '.js-privilege-submit', async (evt) => {
