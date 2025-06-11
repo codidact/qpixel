5c78d33e3d52a31a7e7a502482aee2d0b7fa236e
Oleg Valter <o.a.philippov@gmail.com>
2025-06-11 09:16:45 +0300

    switched UsersController#full_log to the 'deleted' scope where applicable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -270,7 +270,7 @@ class UsersController < ApplicationController
 
     @all_edits = @suggested_edits + @edits
 
-    @interesting_comments = Comment.by(@user).where(deleted: true).count
+    @interesting_comments = Comment.by(@user).deleted.count
     @interesting_flags = Flag.where(user: @user, status: 'declined').count
     @interesting_edits = SuggestedEdit.where(user: @user, active: false, accepted: false).count
     @interesting_posts = Post.where(user: @user).where('score < 0.25 OR deleted=1').count
@@ -290,7 +290,7 @@ class UsersController < ApplicationController
               when 'warnings'
                 ModWarning.where(community_user: @user.community_user).all
               when 'interesting'
-                Comment.by(@user).where(deleted: true).all + Flag.where(user: @user, status: 'declined').all + \
+                Comment.by(@user).deleted.all + Flag.where(user: @user, status: 'declined').all + \
                   SuggestedEdit.where(user: @user, active: false, accepted: false).all + \
                   Post.where(user: @user).where('score < 0.25 OR deleted=1').all
               else
