cc08e3b96dd8fcd1b00c072a2d35d1c72491111b
trichoplax <xalpohcirt@gmail.com>
2025-06-17 23:48:38 +0100

    Rename a clashing system test class to allow rails test:all

--- test/system/post_system_test.rb
+++ test/system/post_system_test.rb
@@ -1,6 +1,6 @@
 require 'application_system_test_case'
 
-class PostTest < ApplicationSystemTestCase
+class PostSystemTest < ApplicationSystemTestCase
   # -------------------------------------------------------
   # Create
   # -------------------------------------------------------
