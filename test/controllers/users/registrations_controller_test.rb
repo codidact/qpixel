18aaf8ba5f18a0a0ec91418545ccbd8b0b3f38b4
ArtOfCode- <hello@artofcode.co.uk>
2025-07-01 14:47:09 +0100

    Add tests

---
+++ test/controllers/users/registrations_controller_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class Users::RegistrationsControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+  include ApplicationHelper
+
+  test 'should register user' do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    try_register_user('test', 'test@example.com', 'testtest')
+    assert_response(:found)
+    assert_not_nil assigns(:user).id
+    assert_redirected_to root_path
+  end
+
+  test 'should prevent rapid registrations from same IP' do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    User.create(username: 'test', email: 'test2@example.com', password: 'testtest', current_sign_in_ip: '0.0.0.0')
+    try_register_user('test', 'test@example.com', 'testtest')
+    assert_response(:found)
+    assert_redirected_to root_path
+    assert_nil assigns(:user).id
+    assert_not_nil flash[:danger]
+  end
+
+  private
+
+  def try_register_user(username, email, password)
+    post :create, params: { user: { username: username, email: email, password: password,
+                                    password_confirmation: password } }
+  end
+end
