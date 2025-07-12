02b634dd7dcbe7f75c73d3891a81a83d1482a709
Oleg Valter <o.a.philippov@gmail.com>
2025-07-13 02:49:46 +0300

    testing system tests & screenshots

--- test/system/post_system_test.rb
+++ test/system/post_system_test.rb
@@ -7,6 +7,11 @@ class PostSystemTest < ApplicationSystemTestCase
   # Create
   # -------------------------------------------------------
 
+  test 'dummy test for screenshot uploading' do
+    visit root_url
+    take_screenshot
+  end
+
   test 'Not-signed in user cannot create a post' do
     visit root_url
     click_on 'Create Post'
