c193280fd9da9deefe885ed7d7eeffe367720f1e
ArtOfCode- <hello@artofcode.co.uk>
2025-07-14 16:19:47 +0100

    Update & test flag mailer

--- test/mailers/flag_mailer_test.rb
+++ test/mailers/flag_mailer_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class FlagMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'flag_escalated' do
+    assert_emails 1 do
+      FlagMailer.with(flag: flags(:escalated)).flag_escalated.deliver_now
+    end
+  end
 end
