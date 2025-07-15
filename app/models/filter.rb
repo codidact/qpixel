1aa0fc079cb7b332ba0ea01d5cff065dd1b214e4
ArtOfCode- <hello@artofcode.co.uk>
2025-07-15 17:18:07 +0100

    That seemed to work; update the rest of them

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -2,6 +2,6 @@ class Filter < ApplicationRecord
   belongs_to :user
   has_many :category_filter_defaults, dependent: :destroy
   validates :name, uniqueness: { scope: :user }
-  serialize :include_tags, Array
-  serialize :exclude_tags, Array
+  serialize :include_tags, coder: JSON, type: Array
+  serialize :exclude_tags, coder: JSON, type: Array
 end
--- app/models/suggested_edit.rb
+++ app/models/suggested_edit.rb
@@ -6,8 +6,8 @@ class SuggestedEdit < ApplicationRecord
 
   belongs_to :user
 
-  serialize :tags_cache, Array
-  serialize :before_tags_cache, Array
+  serialize :tags_cache, coder: JSON, type: Array
+  serialize :before_tags_cache, coder: JSON, type: Array
 
   belongs_to :decided_by, class_name: 'User', optional: true
   has_and_belongs_to_many :tags
