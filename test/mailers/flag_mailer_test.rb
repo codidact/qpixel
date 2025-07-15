5cb0045c4f739887050077c5ff4125f1f61a69a6
Oleg Valter <o.a.philippov@gmail.com>
2025-07-15 23:57:16 +0300

    ensured flag mailer test reproduces the 500 error

--- test/mailers/flag_mailer_test.rb
+++ test/mailers/flag_mailer_test.rb
@@ -3,6 +3,11 @@ require 'test_helper'
 class FlagMailerTest < ActionMailer::TestCase
   test 'flag_escalated' do
     assert_emails 1 do
+      flag = flags(:escalated)
+
+      # forces 'post modified after flag' to be rendered
+      flag.post.update(updated_at: DateTime.now)
+
       FlagMailer.with(flag: flags(:escalated)).flag_escalated.deliver_now
     end
   end
