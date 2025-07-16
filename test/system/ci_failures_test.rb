816b15e75578ff2ee92227a3506d3dc65a23d88e
trichoplax <xalpohcirt@gmail.com>
2025-07-16 01:19:04 +0100

    Extra tests to try to see why posts don't show in category page

--- test/system/ci_failures_test.rb
+++ test/system/ci_failures_test.rb
@@ -5,6 +5,23 @@ class CIFailuresTest < ApplicationSystemTestCase
     assert true
   end
 
+
+  test 'Anyone can view question' do
+    post = posts(:question_one)
+    visit post_url(post)
+    # category=categories(:main)
+    # visit category_path(category)
+
+    assert_text 'Nonexistent to force screenshot'
+  end
+
+  test 'temporary test to analyse CI failures what does main look like before logging in' do
+    clear_cache
+    category = categories(:main)
+    visit category_path(category)
+    assert_text 'Nonexistent to force screenshot'
+  end
+
   test 'temporary test to analyse CI failures what does basic user see in main' do
     category = categories(:main)
     log_in :basic_user
@@ -40,5 +57,24 @@ class CIFailuresTest < ApplicationSystemTestCase
     log_in :standard_user
     visit category_path(category)
     assert_text 'Nonexistent to force screenshot'
+  end
+
+  test 'temporary test to analyse CI failures what does basic user see after adding post to main' do
+    category = categories(:main)
+    log_in :standard_user
+    visit category_path(category)
+    button_label = 'Create Post'
+    post_body = 'Test body text for testing threads.'
+    post_title = 'Test title text for testing threads'
+    tags = ['bug']
+    create_post(button_label, post_body, post_title, tags)
+    assert_text 'Unfollow new'  # Ensure post has finished saving to avoid timing problems
+
+    log_out
+    assert_text 'Sign in'
+
+    log_in :basic_user
+    visit category_path(category)
+    assert_text '0 posts'
   end
 end
