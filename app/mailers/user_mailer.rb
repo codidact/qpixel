088e88d57476e3f8059ef41e5cbcbc64620ec365
ArtOfCode- <hello@artofcode.co.uk>
2025-07-18 21:45:02 +0100

    Not sure why that isn't working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,10 @@
 class UserMailer < ApplicationMailer
   helper :application, :users
 
-  default from: lambda {
-    "#{SiteSetting['NoReplySenderName']} <#{SiteSetting['NoReplySenderEmail']}>"
-  }
-
   def deletion_confirmation
     @user = params[:user]
     @host = params[:host]
-    mail to: @user.email, subject: 'Your Codidact account has been deleted as you requested'
+    mail to: @user.email, subject: 'Your Codidact account has been deleted as you requested',
+         from: "#{SiteSetting['NoReplySenderName']} <#{SiteSetting['NoReplySenderEmail']}>"
   end
 end
