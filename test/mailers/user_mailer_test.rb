18a80fd23772734f4a1853be75d685e3a3969b2d
ArtOfCode- <hello@artofcode.co.uk>
2025-07-18 22:13:13 +0100

    Maybe a mailer test?

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'deletion_confirmation' do
+    email = UserMailer.with(user: users(:standard_user), host: communities(:sample).host)
+                      .deletion_confirmation
+    assert_emails 1 do
+      email.deliver_now
+    end
+    assert_operator email.from[0].length, :>, 3, 'Sender for deletion confirmation appears empty or default value'
+  end
 end
