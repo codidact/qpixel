0085d502eda2c6012cb9ddd8463c506fa09481e6
Oleg Valter <o.a.philippov@gmail.com>
2025-07-19 03:26:19 +0300

    let's not spam hundreds of thousands of logs when testing, info is more than enough

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   config.cache_classes = false
   config.action_view.cache_template_loading = true
 
+  config.log_level = :info
+
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
