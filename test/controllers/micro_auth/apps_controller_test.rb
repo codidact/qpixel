70d0daffea4e19e1f35501aa3541cd9a5f211c6a
Oleg Valter <o.a.philippov@gmail.com>
2025-07-21 03:44:16 +0300

    added first microauth tests (coverage)

--- test/controllers/micro_auth/apps_controller_test.rb
+++ test/controllers/micro_auth/apps_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class MicroAuth::AppsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test 'should require authentication to create apps' do
+    post create_oauth_app_path
+
+    assert_response(:found)
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'should correctly create apps' do
+    sign_in users(:standard_user)
+
+    post create_oauth_app_path, params: {
+      micro_auth_app: {
+        auth_domain: 'localhost',
+        name: 'MyApp',
+        description: 'testing app'
+      }
+    }
+
+    assert_response(:found)
+
+    @app = assigns(:app)
+    assert_not_nil @app
+    assert_redirected_to oauth_app_path(@app.app_id)
+  end
 end
