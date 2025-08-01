6f87a013f346726e6e2fc749160f9a03d81a2e81
Oleg Valter <o.a.philippov@gmail.com>
2025-08-01 20:24:41 +0300

    made posts title FTS migraiton more robust

--- db/migrate/20250801124236_add_post_titles_to_posts_fts.rb
+++ db/migrate/20250801124236_add_post_titles_to_posts_fts.rb
@@ -1,7 +1,7 @@
 # initial post FTS did not include titles
 class AddPostTitlesToPostsFts < ActiveRecord::Migration[7.2]
   def change
-    remove_index :posts, :body_markdown
-    add_index :posts, [:body_markdown, :title], type: :fulltext
+    remove_index :posts, :body_markdown, if_exists: true, type: :fulltext
+    add_index :posts, [:body_markdown, :title], if_not_exists: true, type: :fulltext
   end
 end
