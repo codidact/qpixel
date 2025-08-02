d1abae9972811d9ddd062aef1dd1f834e86b3b55
Oleg Valter <o.a.philippov@gmail.com>
2025-08-02 20:18:06 +0300

    ensured PinnedLink validates that either post or URL is set

--- app/models/pinned_link.rb
+++ app/models/pinned_link.rb
@@ -2,9 +2,17 @@ class PinnedLink < ApplicationRecord
   include MaybeCommunityRelated
   belongs_to :post, optional: true
 
+  validate :check_post_or_url
+
   # Is the link time-constrained?
   # @return [Boolean] check result
   def timed?
     shown_before.present? || shown_after.present?
+  end
+
+  def check_post_or_url
+    unless post_id.present? || link.present?
+      errors.add(:base, 'either a post or a URL must be set')
+    end
   end
 end
