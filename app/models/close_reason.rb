7cc80877ceac6d742c9382aa7bcb9fa8372158a0
Oleg Valter <o.a.philippov@gmail.com>
2025-08-04 16:58:47 +0300

    added global? method to CloseReason for checking if it's community-specific or not

--- app/models/close_reason.rb
+++ app/models/close_reason.rb
@@ -6,4 +6,8 @@ class CloseReason < ApplicationRecord
   validates :name, length: { maximum: 255 },
                    presence: true,
                    uniqueness: { scope: [:community_id], case_sensitive: false }
+
+  def global?
+    community.nil?
+  end
 end
