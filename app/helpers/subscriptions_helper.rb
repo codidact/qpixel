8b934a7666602dcc059eea3592a045ed557adc90
Oleg Valter <o.a.philippov@gmail.com>
2025-08-05 22:48:52 +0300

    added type_choice_for subscription helper for getting available type subscription type select options

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,6 +1,6 @@
 module SubscriptionsHelper
-  # Generate <select> options for available subscription frequences
-  # @return [Array(String, String)>]
+  # Generates <select> options for available subscription frequences
+  # @return [Array(String, String)]
   def frequency_choice
     [
       ['Every day', 1],
@@ -8,5 +8,13 @@ module SubscriptionsHelper
       ['Every month', 30],
       ['Every quarter', 90]
     ]
+  end
+
+  # Generates <select> options for available subscription types for a given user
+  # @param user [User] user to perform access control checks for
+  # @return [Array(String, String)]
+  def type_choice_for(user)
+    Subscription.types_accessible_to(user)
+                .map { |type| [phrase_for(type).capitalize, type] }
   end
 end
