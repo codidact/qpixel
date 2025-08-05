6526a7dbe381a153d69d9d890e8a73bcf4700222
Oleg Valter <o.a.philippov@gmail.com>
2025-08-05 22:45:06 +0300

    added frequency_choice subscription helper for getting available frequency select options

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,2 +1,12 @@
 module SubscriptionsHelper
+  # Generate <select> options for available subscription frequences
+  # @return [Array(String, String)>]
+  def frequency_choice
+    [
+      ['Every day', 1],
+      ['Every week', 7],
+      ['Every month', 30],
+      ['Every quarter', 90]
+    ]
+  end
 end
