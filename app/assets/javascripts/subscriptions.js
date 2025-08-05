89d5114b4f6be5bd71d863e0691bbd050246adcd
Oleg Valter <o.a.philippov@gmail.com>
2025-08-06 00:19:47 +0300

    clear qualifier on subscription type change

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const clearQualifier = () => {
+    const qualifierField = document.querySelector('.js-subscription-qualifier-field');
+
+    if (qualifierField instanceof HTMLInputElement) {
+      qualifierField.value = '';
+    }
+  };
+
   document.querySelectorAll('.js-subscription-type-select, .js-subscription-frequency-select').forEach((el) => {
-    $(el).select2();
+    $(el).select2().on('change', ($event) => {
+      if ($event.target.matches('.js-subscription-type-select')) {
+        clearQualifier();
+      }
+    });
   });
 
   $('.js-enable-subscription').on('change', async (evt) => {
