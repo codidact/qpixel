ee48e0f93cfeaccd1c83f8e0a6940332fb0a9e49
Oleg Valter <o.a.philippov@gmail.com>
2025-08-10 17:12:45 +0300

    moved SettingConverter class to /lib

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -126,37 +126,3 @@ class SiteSetting < ApplicationRecord
     end
   end
 end
-
-class SettingConverter
-  def initialize(value)
-    @value = value
-  end
-
-  def as_array
-    @value&.split
-  end
-
-  def as_string
-    @value&.to_s
-  end
-
-  def as_text
-    @value&.to_s
-  end
-
-  def as_integer
-    @value&.to_i
-  end
-
-  def as_float
-    @value&.to_f
-  end
-
-  def as_boolean
-    ActiveModel::Type::Boolean.new.cast(@value)
-  end
-
-  def as_json
-    JSON.parse(@value)
-  end
-end
