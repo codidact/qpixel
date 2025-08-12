6de8a8571f640ffc4d02ff7253450f39547ec26a
Oleg Valter <o.a.philippov@gmail.com>
2025-08-12 19:23:37 +0300

    added controller test for trying to rename comment threads to invalid titles

--- test/controllers/comments/rename_test.rb
+++ test/controllers/comments/rename_test.rb
@@ -23,13 +23,26 @@ class CommentsControllerTest < ActionController::TestCase
       before_title = comment_threads(name).title
 
       try_rename_thread(comment_threads(name))
-
       @thread = assigns(:comment_thread)
 
       assert_response(:found)
       assert_not_nil @thread
       assert_redirected_to comment_thread_path(@thread)
       assert_equal before_title, @thread.title
+    end
+  end
+
+  test 'should correctly handle invalid thread titles' do
+    sign_in users(:admin)
+
+    ['', 'a' * 512].each do |title|
+      try_rename_thread(comment_threads(:normal), title: title)
+      @thread = assigns(:comment_thread)
+
+      assert_response(:found)
+      assert_not_nil @thread
+      assert_not @thread.valid?
+      assert_not_nil flash[:danger]
     end
   end
 end
