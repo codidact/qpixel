a88cc5cf288dc29df947803ca5760d40e8dd9aed
Oleg Valter <o.a.philippov@gmail.com>
2025-08-28 19:55:17 +0300

    category tags should be eager-loaded with the list_includes scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -52,6 +52,7 @@ class Post < ApplicationRecord
   scope :qa_only, -> { where(post_type_id: [Question.post_type_id, Answer.post_type_id, Article.post_type_id]) }
   scope :list_includes, lambda {
                           includes(:user, :tags, :post_type, :category, :community, :last_activity_by,
+                                   category: [:moderator_tags, :required_tags, :topic_tags],
                                    user: :avatar_attachment)
                         }
   scope :has_duplicates, -> { joins(:inbound_duplicates) } # uses INNER JOIN by default so no where required
