a362159440fe1b21458ea7213329de9610dbe208
Oleg Valter <o.a.philippov@gmail.com>
2025-09-04 04:56:40 +0300

    switched tour step-4 to hint_widget & allowed the wait to be skipped

--- app/assets/javascripts/tour.js
+++ app/assets/javascripts/tour.js
@@ -1,4 +1,6 @@
 const tour = {
+    badAnswerTimeout: null,
+
     firstAnswer: function () {
         $(".js-good-answer").removeClass("hide");
         $(".js-answer-counter").text("1 answer");
@@ -11,7 +13,8 @@ const tour = {
         $(".step-4").removeClass("hide");
         $(".js-good-answer .js-upvote-count").text("+1");
         $(".js-good-answer .js-upvote-count").text("+1");
-        window.setTimeout(() => {
+
+        tour.badAnswerTimeout = window.setTimeout(() => {
             tour.secondAnswer();
         }, 4000);
     },
@@ -31,6 +34,7 @@ const tour = {
             $(".js-answer-counter").text("1 answer");
             $(".js-bad-answer").addClass("deleted-content");
         }, 4000);
+
         window.setTimeout(() => {
             $(".js-bad-answer").addClass("hide");
         }, 6000);
@@ -50,6 +54,16 @@ $(() => {
 
         if (selector) {
           document.querySelector(selector)?.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+
+    $(document).on('click', '.js-tour-skip-wait', (ev) => {
+        const timeout = $(ev.target).data('timeout');
+
+        switch (timeout) {
+            case 'bad-answer':
+                clearTimeout(tour.badAnswerTimeout);
+                tour.secondAnswer();
         }
     });
 
