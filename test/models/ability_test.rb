5de8faaf60aee0ef0c381a21ab9a475fe97646b9
Oleg Valter <o.a.philippov@gmail.com>
2025-09-06 05:07:59 +0300

    added auto success path tests for score_percent_for methods of Ability model

--- test/models/ability_test.rb
+++ test/models/ability_test.rb
@@ -20,4 +20,16 @@ class AbilityTest < ActiveSupport::TestCase
       end
     end
   end
+
+  test 'score_percent_for methods should return 100 for abilities 0 thresholds' do
+    std = users(:standard_user)
+
+    [:edit, :flag, :post].each do |type|
+      abilities.each do |ability|
+        next unless ability.send("#{type}_score_threshold")&.zero?
+
+        assert_equal 100, ability.send("#{type}_score_percent_for", std)
+      end
+    end
+  end
 end
