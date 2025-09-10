e337269707442c4b22b8a143a3e47afd8582e6ab
Oleg Valter <o.a.philippov@gmail.com>
2025-09-11 02:10:20 +0300

    added CommentThread#last_activity_at for caching purposes

--- test/models/comment_thread_test.rb
+++ test/models/comment_thread_test.rb
@@ -15,4 +15,17 @@ class CommentThreadTest < ActiveSupport::TestCase
       end
     end
   end
+
+  test 'last_activity_at should correctly get last activity' do
+    normal = comment_threads(:normal)
+    normal_two = comment_threads(:normal_two)
+    locked = comment_threads(:locked)
+
+    assert_not_equal locked.last_activity_at, locked.created_at
+    assert_not_equal normal_two.last_activity_at, normal_two.created_at
+
+    assert_equal locked.last_activity_at, locked.locked_at
+    assert_equal normal.last_activity_at, normal.created_at
+    assert_equal normal_two.last_activity_at, normal_two.updated_at
+  end
 end
