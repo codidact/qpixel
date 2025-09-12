c2e717946c8b0cc7b41a987f150abfe0e4b924c5
Oleg Valter <o.a.philippov@gmail.com>
2025-09-12 06:29:31 +0300

    reduced reliance of filters JS asset on jQuery

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,4 +1,4 @@
-$(() => {
+document.addEventListener('DOMContentLoaded', () => {
   $('.js-filter-select').toArray().forEach(async (el) => {
     const $select = $(el);
     const $form = $select.closest('form');
