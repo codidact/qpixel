00947d9d07b1704adf7b0371bd808db0a4924002
Oleg Valter <o.a.philippov@gmail.com>
2025-10-01 13:29:53 +0300

    rendered_description should account for nil values & added YARD to it

--- app/helpers/site_settings_helper.rb
+++ app/helpers/site_settings_helper.rb
@@ -1,5 +1,9 @@
 module SiteSettingsHelper
+  # Renders description for a given site setting
+  # @param setting [SiteSetting] setting to render the description for
+  # @return [ActiveSupport::SafeBuffer] rendered description
   def rendered_description(setting)
-    sanitize(render_markdown(setting.description))
+    raw_description = setting.description || ''
+    sanitize(render_markdown(raw_description))
   end
 end
