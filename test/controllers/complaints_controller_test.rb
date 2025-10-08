940551c89709849b8f1cc7a5bcdb311639ce027d
ArtOfCode- <hello@artofcode.co.uk>
2025-10-08 20:43:40 +0100

    Rubocop

--- test/controllers/complaints_controller_test.rb
+++ test/controllers/complaints_controller_test.rb
@@ -3,35 +3,35 @@ require 'test_helper'
 class ComplaintsControllerTest < ActionDispatch::IntegrationTest
   test 'should get safety center' do
     get safety_center_path
-    assert_response :success
+    assert_response(:success)
   end
 
   test 'safety center should work signed in' do
     sign_in users(:basic_user)
     get safety_center_path
-    assert_response :success
+    assert_response(:success)
   end
 
   test 'safety center should work for staff' do
     sign_in users(:staff)
     get safety_center_path
-    assert_response :success
+    assert_response(:success)
   end
 
   test 'new report should work signed out' do
     get new_complaint_path
-    assert_response :success
+    assert_response(:success)
   end
 
   test 'new report should work signed in' do
     sign_in users(:basic_user)
     get new_complaint_path
-    assert_response :success
+    assert_response(:success)
   end
 
   test 'new report should work for staff' do
     sign_in users(:staff)
     get new_complaint_path
-    assert_response :success
+    assert_response(:success)
   end
 end
