d96a415e2b48e11d6b3c842eb8d57d6a4ffc9c9d
ArtOfCode- <hello@artofcode.co.uk>
2025-10-08 20:48:01 +0100

    tsc

--- app/assets/javascripts/complaints.js
+++ app/assets/javascripts/complaints.js
@@ -1,13 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
   QPixel.DOM.addSelectorListener('change', 'input[name="report_type"]', ev => {
-    const value = document.querySelector('input[name="report_type"]:checked').value;
+    const value = /** @type HTMLInputElement */(document.querySelector('input[name="report_type"]:checked')).value;
     document.querySelectorAll(`[data-report-type="${value}"]`).forEach(el => {
       el.classList.remove('hidden');
       el.removeAttribute('disabled');
     });
     document.querySelectorAll(`[data-report-type]:not([data-report-type="${value}"])`).forEach(el => {
       el.classList.add('hidden');
-      el.disabled = true;
+      el.setAttribute('disabled', 'disabled');
     });
   });
 });
