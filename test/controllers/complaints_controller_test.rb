91482a7074ff5f21afac0426c86ea2687b6edb8e
ArtOfCode- <hello@artofcode.co.uk>
2025-10-09 22:52:49 +0100

    Rubocop

--- test/controllers/complaints_controller_test.rb
+++ test/controllers/complaints_controller_test.rb
@@ -41,24 +41,24 @@ class ComplaintsControllerTest < ActionDispatch::IntegrationTest
     sign_in users(:basic_user)
     try_create_report report_type: 'illegal', reported_url: 'https://example.com', content_type: 'fraud',
                       content: 'test', email: 'something@else.com', user_wants_updates: true
-    assert_response :found
+    assert_response(:found)
     assert_not_nil assigns(:complaint)
-    #assert_redirected_to complaint_path(@complaint.access_token)
+    # assert_redirected_to complaint_path(@complaint.access_token)
     assert_equal users(:basic_user).email, assigns(:complaint).email
   end
 
   test 'should create report for anonymous user and use email provided' do
     try_create_report report_type: 'illegal', reported_url: 'https://example.com', content_type: 'fraud',
                       content: 'test', email: 'something@else.com', user_wants_updates: true
-    assert_response :found
+    assert_response(:found)
     assert_not_nil assigns(:complaint)
-    #assert_redirected_to complaint_path(@complaint.access_token)
+    # assert_redirected_to complaint_path(@complaint.access_token)
     assert_equal 'something@else.com', assigns(:complaint).email
   end
 
   test 'should correctly validate fields for report' do
     try_create_report user_wants_updates: true
-    assert_response :bad_request
+    assert_response(:bad_request)
     assert_not_nil assigns(:complaint)
     assert_equal 3, assigns(:errors).size
   end
