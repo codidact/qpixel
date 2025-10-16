2f3da57c1a32cbff3c498f931ff860728983a780
Oleg Valter <o.a.philippov@gmail.com>
2025-10-16 17:40:32 +0300

    ensured safety_center AppConfig accessors are nil-safe

--- app/helpers/complaints_helper.rb
+++ app/helpers/complaints_helper.rb
@@ -1,25 +1,55 @@
 module ComplaintsHelper
-  def statuses_select_options
-    AppConfig.safety_center['statuses'].map { |k, v| [v['name'], k] }
+  def content_types
+    value_for('content_types') || []
   end
 
-  def status(key)
-    AppConfig.safety_center['statuses'][key]
+  def content_type(key)
+    value_for('content_types', key)
   end
 
   def outcomes_select_options
-    AppConfig.safety_center['outcomes'].map { |k, v| [v['name'], k] }
+    select_options_for('outcomes')
   end
 
   def outcome(key)
-    AppConfig.safety_center['outcomes'][key]
+    value_for('outcomes', key)
+  end
+
+  def report_types
+    value_for('report_types') || []
   end
 
   def report_types_select_options
-    AppConfig.safety_center['report_types'].map { |k, v| [v['name'], k] }
+    select_options_for('report_types')
   end
 
   def report_type(key)
-    AppConfig.safety_center['report_types'][key]
+    value_for('report_types', key)
+  end
+
+  def enabled_report_types
+    report_types.select { |_k, t| t['enabled'] }
+  end
+
+  def statuses
+    value_for('statuses') || []
+  end
+
+  def statuses_select_options
+    select_options_for('statuses')
+  end
+
+  def status(key)
+    value_for('statuses', key)
+  end
+
+  private
+
+  def value_for(*path)
+    AppConfig.safety_center&.dig(*path)
+  end
+
+  def select_options_for(key)
+    value_for(key)&.map { |k, v| [v['name'], k] } || []
   end
 end
