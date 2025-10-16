36881ec0d5a1de40d9fe1c91ddab117c5536bd2b
ArtOfCode- <hello@artofcode.co.uk>
2025-10-16 23:08:55 +0100

    Rubocop

--- app/jobs/auto_close_complaints_job.rb
+++ app/jobs/auto_close_complaints_job.rb
@@ -1,7 +1,7 @@
 class AutoCloseComplaintsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(*_args)
     complaints = Complaint.where(status: 'reviewed')
                           .where(Arel.sql('status_updated_at <= ?', 14.days.ago))
     complaints.each do |complaint|
