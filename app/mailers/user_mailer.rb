f0ec2e9e868fba296f4e04d40b48fe414ac057ba
Oleg Valter <o.a.philippov@gmail.com>
2025-10-26 21:18:12 +0300

    switched a bunch more views to the NetworkName site setting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     @host = params[:host]
     @community = params[:community]
-    mail to: @user.email, subject: 'Your Codidact account has been deleted as you requested',
+    mail to: @user.email, subject: "Your #{SiteSetting['NetworkName']} account has been deleted as you requested",
          from: "#{SiteSetting['NoReplySenderName', community: @community]} " \
                "<#{SiteSetting['NoReplySenderEmail', community: @community]}>"
   end
