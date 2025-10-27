16c012203d31cadcaa03558c5afb17c63e6ed85f
Oleg Valter <o.a.philippov@gmail.com>
2025-10-27 04:55:41 +0300

    added tests for TwoFactorMailer

--- test/mailers/two_factor_mailer_test.rb
+++ test/mailers/two_factor_mailer_test.rb
@@ -1,7 +1,66 @@
 require 'test_helper'
 
 class TwoFactorMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'disable_email should correctly send emails' do
+    com_usr = communities(:sample)
+    std_usr = users(:standard_user)
+
+    email = TwoFactorMailer.with(user: std_usr, host: com_usr.host)
+                           .disable_email
+
+    assert_emails 1 do
+      email.deliver_later
+    end
+
+    token_uri = Rails.application
+                     .routes
+                     .url_helpers
+                     .two_factor_disable_link_url(token: '', host: com_usr.host)
+
+    assert_select_email do
+      assert_select "a[href^='#{token_uri}']"
+    end
+  end
+
+  test 'login_email should correctly send emails' do
+    com_usr = communities(:sample)
+    std_usr = users(:standard_user)
+
+    email = TwoFactorMailer.with(user: std_usr, host: com_usr.host)
+                           .login_email
+
+    assert_emails 1 do
+      email.deliver_later
+    end
+
+    qr_uri = Rails.application
+                  .routes
+                  .url_helpers
+                  .qr_login_url(token: '', host: com_usr.host)
+
+    assert_select_email do
+      assert_select "a[href^='#{qr_uri}']"
+    end
+  end
+
+  test 'backup_code should correctly send emails' do
+    com_usr = communities(:sample)
+    std_usr = users(:standard_user)
+
+    email = TwoFactorMailer.with(user: std_usr, host: com_usr.host)
+                           .backup_code
+
+    assert_emails 1 do
+      email.deliver_later
+    end
+
+    backup_uri = Rails.application
+                      .routes
+                      .url_helpers
+                      .two_factor_status_url(host: com_usr.host)
+
+    assert_select_email do
+      assert_select "a[href^='#{backup_uri}']"
+    end
+  end
 end
