2bbe33c8be5988d4b9dcacf13588292d1f3843d5
Oleg Valter <o.a.philippov@gmail.com>
2025-10-27 06:06:33 +0300

    fixed Filter model test since we don't have a system user fixture anymore

--- test/models/filter_test.rb
+++ test/models/filter_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class FilterTest < ActiveSupport::TestCase
   test 'system? should correctly determine if a filter is a system filter' do
-    sys_usr = users(:system)
+    sys_usr = User.system
 
     filters.each do |filter|
       assert_equal filter.system?, filter.user_id == sys_usr
