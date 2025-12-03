88827852eab7a6a6fa70315ba7b01aa6d64a594f
trichoplax <xalpohcirt@gmail.com>
2025-12-03 22:09:22 +0000

    Use single quotes for rubocop

--- test/models/new_thread_follower_test.rb
+++ test/models/new_thread_follower_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class NewThreadFollowerTest < ActiveSupport::TestCase
-  # test "the truth" do
+  # test 'the truth' do
   #   assert true
   # end
 end
