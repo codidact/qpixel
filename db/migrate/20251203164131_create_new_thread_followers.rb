9a3e5c08f96cb817649bcf49cad8e4035012ac7a
trichoplax <xalpohcirt@gmail.com>
2025-12-04 00:22:48 +0000

    Include data migration and column removal in migration

--- db/migrate/20251203164131_create_new_thread_followers.rb
+++ db/migrate/20251203164131_create_new_thread_followers.rb
@@ -1,5 +1,11 @@
 class CreateNewThreadFollowers < ActiveRecord::Migration[7.2]
   def change
+    create_table_new_thread_followers
+    move_rows_with_non_nil_post_id
+    remove_post_id_column_from_thread_followers
+  end
+
+  def create_table_new_thread_followers
     create_table :new_thread_followers do |t|
       t.bigint :user_id
       t.bigint :post_id
@@ -8,5 +14,17 @@ class CreateNewThreadFollowers < ActiveRecord::Migration[7.2]
     end
     add_index :new_thread_followers, :user_id
     add_index :new_thread_followers, :post_id
+  end
+
+  def move_rows_with_non_nil_post_id
+    NewThreadFollower.insert_all(
+      ThreadFollower.select(:user_id, :post_id, :created_at, :updated_at).where.not(post_id:nil)
+    )
+  end
+
+  def remove_post_id_column_from_thread_followers
+    change_table :thread_followers do |t|
+      t.remove :post_id
+    end
   end
 end
