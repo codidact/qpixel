ac4f738c6caaae1f78265e280077a23831125154
trichoplax <xalpohcirt@gmail.com>
2025-12-06 01:52:10 +0000

    Tidying thanks to rubocop

--- test/models/new_thread_follower_test.rb
+++ test/models/new_thread_follower_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
 class NewThreadFollowerTest < ActiveSupport::TestCase
-  test "save succeeds with user and post" do
-    new_thread_follower=NewThreadFollower.new
-    new_thread_follower.user=users(:basic_user)
-    new_thread_follower.post=posts(:question_one)
+  test 'save succeeds with user and post' do
+    new_thread_follower = NewThreadFollower.new
+    new_thread_follower.user = users(:basic_user)
+    new_thread_follower.post = posts(:question_one)
     assert new_thread_follower.save
   end
 
-  test "save fails without user" do
-    new_thread_follower=NewThreadFollower.new
-    new_thread_follower.post=posts(:question_one)
+  test 'save fails without user' do
+    new_thread_follower = NewThreadFollower.new
+    new_thread_follower.post = posts(:question_one)
     assert_not new_thread_follower.save
   end
 
-  test "save fails without post" do
-    new_thread_follower=NewThreadFollower.new
-    new_thread_follower.user=users(:basic_user)
+  test 'save fails without post' do
+    new_thread_follower = NewThreadFollower.new
+    new_thread_follower.user = users(:basic_user)
     assert_not new_thread_follower.save
   end
 end
--- test/models/thread_follower_test.rb
+++ test/models/thread_follower_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
 class ThreadFollowerTest < ActiveSupport::TestCase
-  test "save succeeds with user and thread" do
-    new_thread_follower=ThreadFollower.new
-    new_thread_follower.user=users(:basic_user)
-    new_thread_follower.comment_thread=comment_threads(:normal)
+  test 'save succeeds with user and thread' do
+    new_thread_follower = ThreadFollower.new
+    new_thread_follower.user = users(:basic_user)
+    new_thread_follower.comment_thread = comment_threads(:normal)
     assert new_thread_follower.save
   end
 
-  test "save fails without user" do
-    new_thread_follower=ThreadFollower.new
-    new_thread_follower.comment_thread=comment_threads(:normal)
+  test 'save fails without user' do
+    new_thread_follower = ThreadFollower.new
+    new_thread_follower.comment_thread = comment_threads(:normal)
     assert_not new_thread_follower.save
   end
 
-  test "save fails without thread" do
-    new_thread_follower=ThreadFollower.new
-    new_thread_follower.user=users(:basic_user)
+  test 'save fails without thread' do
+    new_thread_follower = ThreadFollower.new
+    new_thread_follower.user = users(:basic_user)
     assert_not new_thread_follower.save
   end
 end
