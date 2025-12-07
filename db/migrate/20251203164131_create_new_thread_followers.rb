5eb10d14c4c335c58c91e73d5170e50289139e63
trichoplax <xalpohcirt@gmail.com>
2025-12-07 19:52:32 +0000

    Add up and down to make NewThreadFollower migration reversible

--- db/migrate/20251203164131_create_new_thread_followers.rb
+++ db/migrate/20251203164131_create_new_thread_followers.rb
@@ -1,10 +1,18 @@
 class CreateNewThreadFollowers < ActiveRecord::Migration[7.2]
-  def change
+  def up
     create_table_new_thread_followers
     if column_exists?(:thread_followers, :post_id)
       move_rows_with_non_nil_post_id
       remove_post_id_column_from_thread_followers
     end
+  end
+
+  def down
+    if !column_exists?(:thread_followers, :post_id)
+      add_post_id_column_to_thread_followers
+    end
+    move_rows_back_from_new_thread_followers
+    delete_table_new_thread_followers
   end
 
   def create_table_new_thread_followers
@@ -29,5 +37,25 @@ class CreateNewThreadFollowers < ActiveRecord::Migration[7.2]
 
   def remove_post_id_column_from_thread_followers
     remove_reference :thread_followers, :post, index: true, foreign_key: true
+  end
+
+  def add_post_id_column_to_thread_followers
+    add_reference :thread_followers, :post, :index: true, foreign_key: true
+  end
+
+  def move_rows_back_from_new_thread_followers
+    ThreadFollower.insert_all(
+      NewThreadFollower.select(:user_id, :post_id, :created_at, :updated_at)
+        .to_a
+        .map(&:attributes)
+    )
+    NewThreadFollower.delete_all
+  end
+
+  def delete_table_new_thread_followers
+    remove_index :new_thread_followers, [:user_id, :post_id], if_exists: true
+    remove_reference :new_thread_followers, :user_id, foreign_key: true
+    remove_reference :new_thread_followers, :post_id, foreign_key: true
+    drop_table :new_thread_followers
   end
 end
