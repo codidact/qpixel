3f1b69e26855165ddc673cb8547b6c49488c7885
Oleg Valter <o.a.philippov@gmail.com>
2025-12-22 03:42:56 +0300

    creating/destroying ThreadFollowers should bump their threads last activity timestamp

--- app/models/thread_follower.rb
+++ app/models/thread_follower.rb
@@ -3,9 +3,17 @@ class ThreadFollower < ApplicationRecord
   belongs_to :post, optional: true
   belongs_to :user
 
+  after_create :bump_thread_last_activity_at
+  before_destroy :bump_thread_last_activity_at
+
   validate :thread_or_post
 
   private
+
+  def bump_thread_last_activity_at
+    comment_thread&.bump_last_activity_at
+    comment_thread&.save
+  end
 
   def thread_or_post
     if comment_thread.nil? && post.nil?
