c19a3a7506d3c5eb562eb274ce3916a21e31d87b
trichoplax <xalpohcirt@gmail.com>
2025-12-26 19:41:04 +0000

    Remove validation now that post_id column has been removed

--- app/models/thread_follower.rb
+++ app/models/thread_follower.rb
@@ -5,17 +5,9 @@ class ThreadFollower < ApplicationRecord
   after_create :bump_thread_last_activity
   before_destroy :bump_thread_last_activity
 
-  validate :thread_or_post
-
   private
 
   def bump_thread_last_activity
     comment_thread&.bump_last_activity(persist_changes: true)
-  end
-
-  def thread_or_post
-    if comment_thread.nil? && post.nil?
-      errors.add(:base, 'Must refer to either a comment thread or a post.')
-    end
   end
 end
