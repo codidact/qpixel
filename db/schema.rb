88fd12def355ada55a0252d39fe9079aca482eed
Oleg Valter <o.a.philippov@gmail.com>
2026-01-20 08:31:06 +0300

    dups should have a required title & optional comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_21_142105) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_20_032240) do
   create_table "abilities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "community_id"
     t.string "name"
@@ -265,6 +265,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_21_142105) do
     t.index ["report_type"], name: "index_complaints_on_report_type"
     t.index ["status"], name: "index_complaints_on_status"
     t.index ["user_id"], name: "index_complaints_on_user_id"
+  end
+
+  create_table "dumps", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "email_logs", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
