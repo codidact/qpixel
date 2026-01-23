c83992281b028540332dee8ac98cfec8b9bbe69a
Oleg Valter <o.a.philippov@gmail.com>
2026-01-23 07:21:25 +0300

    added present/past/future filters to pinned links

--- app/models/pinned_link.rb
+++ app/models/pinned_link.rb
@@ -7,6 +7,31 @@ class PinnedLink < ApplicationRecord
     includes(:post, post: [:community])
   }
 
+  # a past link is one that ended in the past
+  scope :past, lambda {
+    timed.where('shown_before < ?', DateTime.now)
+  }
+
+  # a present link is not timed or started in the past and ends in the future
+  scope :present, lambda {
+    where(shown_after: nil, shown_before: nil).or(
+      timed.where('shown_after <= ?', DateTime.now)
+           .where('shown_before > ?', DateTime.now)
+    )
+  }
+
+  # a future link is one that both starts and ends in the future
+  scope :future, lambda {
+    timed.where('shown_after > ?', DateTime.now)
+         .where('shown_before > ?', DateTime.now)
+  }
+
+  scope :timed, lambda {
+    after = where.not(shown_after: nil)
+    before = where.not(shown_before: nil)
+    before.or(after)
+  }
+
   validate :check_post_or_url
 
   # Is the link time-constrained?
