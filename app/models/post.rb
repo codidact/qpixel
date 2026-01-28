b67cd6c78b1c780d300c6dca3a15f790948dcc98
Oleg Valter <o.a.philippov@gmail.com>
2026-01-28 10:28:43 +0300

    simplified post exclusion search regex

--- app/models/post.rb
+++ app/models/post.rb
@@ -76,7 +76,7 @@ class Post < ApplicationRecord
   # @param term [String] the search term
   # @return [ActiveRecord::Relation<Post>]
   def self.search(term)
-    if term.match(/^(?:\s*?-\S+?)+?$/)
+    if term.match(/^(?:-\S+?\s*?)+$/)
       exclusion_search term, posts: [:body_markdown, :title]
     else
       match_search term, posts: [:body_markdown, :title]
