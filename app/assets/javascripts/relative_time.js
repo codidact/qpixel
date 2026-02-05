97939d6b5ea5fdfa140f2e4ac2e909044acc1fb5
Oleg Valter <o.a.philippov@gmail.com>
2026-02-05 14:25:17 +0300

    relative time should also be updated on mutations

--- app/assets/javascripts/relative_time.js
+++ app/assets/javascripts/relative_time.js
@@ -3,6 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
   let lastRunAt = -1;
 
   /**
+   * @param {Node} el element to process
+   */
+  const processNode = (el) => {
+    if (!QPixel.DOM?.isHTMLElement(el)) {
+      return;
+    }
+
+    const { relstamp } = el.dataset;
+
+    if (!relstamp) {
+      return;
+    }
+
+    el.textContent = `${QPixel.DOM.formatTimestamp(relstamp)} (${moment(relstamp).fromNow()})`;
+  };
+
+  /**
    * @type {FrameRequestCallback}
    */
   const updateRelativeTime = (timestamp) => {
@@ -13,23 +30,18 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
 
-    document.querySelectorAll('[data-relstamp]').forEach((el) => {
-      if (!QPixel.DOM?.isHTMLElement(el)) {
-        return;
-      }
-
-      const { relstamp } = el.dataset;
-
-      if (!relstamp) {
-        return;
-      }
-
-      el.textContent = `${QPixel.DOM.formatTimestamp(relstamp)} (${moment(relstamp).fromNow()})`;
-    });
+    document.querySelectorAll('[data-relstamp]').forEach(processNode);
 
     lastRunAt = timestamp;
     requestAnimationFrame(updateRelativeTime);
   };
 
   requestAnimationFrame(updateRelativeTime);
+
+  new MutationObserver(() => {
+    document.querySelectorAll('[data-relstamp]').forEach(processNode);
+  }).observe(document, {
+    attributes: true,
+    subtree: true,
+  });
 });
