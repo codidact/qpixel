68dc07e347a86cf5d53444f6e1a765263f8ff5e1
Oleg Valter <o.a.philippov@gmail.com>
2026-02-07 19:36:47 +0300

    switched server-side provided max upload size strings to human_max_upload_size (based on MaxRequestBodySize)

--- app/helpers/site_settings_helper.rb
+++ app/helpers/site_settings_helper.rb
@@ -6,4 +6,10 @@ module SiteSettingsHelper
     raw_description = setting.description || ''
     sanitize(render_markdown(raw_description))
   end
+
+  # Formats max upload size (in bytes) into a human-friendly representation
+  # @return [String] formatted value
+  def human_max_upload_size
+    number_to_human_size(SiteSetting['MaxRequestBodySize'])
+  end
 end
