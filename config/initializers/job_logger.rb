08afbfdb71192455bc23fface404651e18d2463a
ArtOfCode- <hello@artofcode.co.uk>
2026-02-27 13:26:02 +0000

    Add tagged job logger for ApplicationJobs

---
+++ config/initializers/job_logger.rb
@@ -0,0 +1,15 @@
+module Rails
+  class << self
+    @job_logger = nil
+
+    def job_logger
+      if @job_logger.nil?
+        logger = ActiveSupport::Logger.new(Rails.root.join('log/jobs.log'))
+        logger.level = ActiveSupport::Logger::INFO
+        @job_logger = ActiveSupport::TaggedLogging.new(logger)
+      else
+        @job_logger
+      end
+    end
+  end
+end
