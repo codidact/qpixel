1d978142adab07e07a2f95b8c67548fe94651eff
ArtOfCode- <hello@artofcode.co.uk>
2026-03-02 17:05:50 +0000

    Add argument to job

--- test/jobs/recalc_abilities_job_test.rb
+++ test/jobs/recalc_abilities_job_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class RecalcAbilitiesJobTest < ActiveJob::TestCase
   test 'should run job successfully' do
     perform_enqueued_jobs do
-      RecalcAbilitiesJob.perform_later
+      RecalcAbilitiesJob.perform_later OpenStruct.new
     end
     assert_performed_jobs 1
   end
