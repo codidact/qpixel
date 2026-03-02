a135a4a20d4fd422bbaf888eabfee8abbe3b3ef1
ArtOfCode- <hello@artofcode.co.uk>
2026-03-02 18:10:28 +0000

    Allow net connections for Stripe test

--- test/jobs/mail_uncaptured_donations_job_test.rb
+++ test/jobs/mail_uncaptured_donations_job_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class MailUncapturedDonationsJobTest < ActiveJob::TestCase
+  setup do
+    WebMock.allow_net_connect!
+  end
+
+  teardown do
+    WebMock.disable_net_connect!
+  end
+
   test 'should run job successfully' do
+    WebMock.allow_net_connect!
     perform_enqueued_jobs do
       MailUncapturedDonationsJob.perform_later
     end
