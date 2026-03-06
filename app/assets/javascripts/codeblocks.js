d6192311e6f7378f4d50a0206157666a11c7f2e9
Oleg Valter <o.a.philippov@gmail.com>
2026-03-07 00:52:16 +0300

    fixed copy code button size when over oneline code blocks

--- app/assets/javascripts/codeblocks.js
+++ app/assets/javascripts/codeblocks.js
@@ -1,16 +1,29 @@
 $(() => {
-  $(".post--content pre > code")
+  const buttonTemplate = `<button class="copy-button button is-muted is-outlined has-margin-2">Copy</button>`;
+
+  $('.post--content pre > code')
     .parent()
-    .each(function() {
-      const content = $(this).text()
+    .each(function () {
+      const $button = $(buttonTemplate);
+      const $content = $(this).text();
+      const numLines = $content.trim().split(/\r?\n/).length
+
+      if (numLines <= 1) {
+        $button.addClass('is-small');
+      }
+
       $(this)
         .wrap('<div style="position:relative;"></div>')
         .parent()
-        .prepend($('<button class="copy-button button is-muted is-outlined has-margin-2">Copy</button>')
-          .click(function () {
-            navigator.clipboard.writeText(content);
+        .prepend(
+          $button.click(function () {
+            navigator.clipboard.writeText($content);
             $(this).text('Copied!');
-            setTimeout(() => { $(this).text('Copy'); }, 2000);
-          }))
-  });
+            setTimeout(() => {
+              $(this).text('Copy');
+            }, 2000);
+          }),
+        );
+    });
 });
+
