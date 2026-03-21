539d970afcbc498fefcec79a391e4dd10768cfb8
Oleg Valter <o.a.philippov@gmail.com>
2026-03-21 21:46:43 +0300

    implemented network push for ability updates

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -17,7 +17,18 @@ class AbilitiesController < ApplicationController
   def edit; end
 
   def update
-    if @ability.update(ability_update_params)
+    if push_to_network?(@ability)
+      abilities = Ability.unscoped.where(internal_id: @ability.internal_id,
+                                         description: @ability.description)
+
+      if do_update_network(@ability, abilities)
+        do_delete_draft(current_user, URI(request.referer || '').path)
+        flash[:success] = "#{helpers.pluralize(abilities.to_a.size, 'ability')} updated."
+        redirect_to ability_path(id: @ability.internal_id)
+      else
+        render :edit, status: :bad_request
+      end
+    elsif @ability.update(ability_update_params)
       do_delete_draft(current_user, URI(request.referer || '').path)
       flash[:success] = I18n.t('abilities.success.update_generic')
       redirect_to ability_path(id: @ability.internal_id)
@@ -35,6 +46,34 @@ class AbilitiesController < ApplicationController
 
   def ability_update_params
     params.require(:ability).permit(:description)
+  end
+
+  # Actually update a given set of abilities network-wide
+  # @param ability [Ability] ability from which the push is initiated
+  # @param abilities [ActiveRecord::Relation<Ability>] network abilities to update
+  # @return [Boolean] status of the operation
+  def do_update_network(ability, abilities)
+    Ability.transaction do
+      abilities.each do |network_ability|
+        unless network_ability.update(ability_update_params)
+          ability.errors.merge!(network_ability.errors)
+          raise ActiveRecord::Rollback
+        end
+      end
+      true
+    rescue
+      false
+    end
+  end
+
+  # Should push to network?
+  # @param ability [Ability] ability to check
+  # @return [Boolean] check result
+  def push_to_network?(ability)
+    return false unless params[:network_push] == 'true'
+    return false unless current_user.present?
+
+    current_user.can_push_to_network?(ability)
   end
 
   def set_ability
