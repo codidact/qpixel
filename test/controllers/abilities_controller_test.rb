1a1a03c787ecb1b07fb128d9dbcd802875c65a82
Oleg Valter <o.a.philippov@gmail.com>
2026-03-21 04:45:24 +0300

    added tests for abilities controller's update action

--- test/controllers/abilities_controller_test.rb
+++ test/controllers/abilities_controller_test.rb
@@ -65,4 +65,40 @@ class AbilitiesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:user)
     assert_not_nil assigns(:your_ability)
   end
+
+  test ':update should require authentication' do
+    ability = abilities(:everyone)
+    try_update_ability(ability, description: 'anonymous')
+    assert_redirected_to_sign_in
+  end
+
+  test ':update should correctly update abilities' do
+    ability = abilities(:everyone)
+
+    users.each do |user|
+      description = "#{user.name}'s edit"
+
+      sign_in user
+      try_update_ability(ability, description: description)
+
+      if user.deleted? || user.community_user.deleted?
+        assert_redirected_to_sign_in
+      elsif user.can_edit_abilities?
+        assert_redirected_to ability_url(id: ability.internal_id)
+        assert_equal assigns(:ability).description, description
+      else
+        assert_response(:not_found)
+      end
+    end
+  end
+
+  private
+
+  # @param ability [Ability] ability to update
+  def try_update_ability(ability, **opts)
+    patch :update, params: {
+      ability: {}.merge(opts),
+      id: ability.internal_id
+    }
+  end
 end
