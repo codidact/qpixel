1740d3c9c985768ea560b14d106598baedc37e10
ArtOfCode- <hello@artofcode.co.uk>
2026-05-14 22:10:38 +0100

    If this worked the whole time I'm going to be annoyed

--- test/jobs/data_dump_job_test.rb
+++ test/jobs/data_dump_job_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class DataDumpJobTest < ActiveJob::TestCase
-  setup :i_know_better_than_activerecord
-  teardown :i_dont_know_better_than_activerecord
+  self.use_transactional_tests = false
 
   test 'job runs successfully' do
     perform_enqueued_jobs do
@@ -33,38 +32,6 @@ class DataDumpJobTest < ActiveJob::TestCase
         # a default value was applied), for us to be happy that there is no true data in the column.
         assert col.all?(&:nil?) || col.uniq.size <= 1, "Distinct non-null data present in column `#{table}`.`#{cols[i]}`"
       end
-    end
-  end
-
-  private
-
-  ##
-  # This is definitely not a terrible idea that will come back to bite me later on.
-  # For context, this is necessary because the +create+ call in DataDumpJob is run by ActiveRecord in a transaction
-  # (well, ish - using savepoints). However, the DDL modifications that the data dump performs automatically release
-  # the savepoints, which then causes the RELEASE query to fail. I think. There's no convenient way to have AR not run
-  # the +create+ call in a transaction, so we have to monkeypatch it out.
-  def i_know_better_than_activerecord
-    ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
-      def create_savepoint(*)
-        logger.warn 'create_savepoint ignored: this should only happen during data dump tests'
-      end
-
-      def rollback_to_savepoint(*)
-        logger.warn 'rollback_to_savepoint ignored: this should only happen during data dump tests'
-      end
-
-      def release_savepoint(*)
-        logger.warn 'release_savepoint ignored: this should only happen during data dump tests'
-      end
-    end
-  end
-
-  ##
-  # Let's not let the terrible idea affect everything else too.
-  def i_dont_know_better_than_activerecord
-    ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
-      include ActiveRecord::ConnectionAdapters::Savepoints
     end
   end
 end
