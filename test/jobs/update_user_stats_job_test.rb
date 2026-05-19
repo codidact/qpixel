5cc919e9720f25b85cdbbfe8539ada546d3638c2
ArtOfCode- <hello@artofcode.co.uk>
2026-05-19 16:50:52 +0100

    More tests

--- test/jobs/update_user_stats_job_test.rb
+++ test/jobs/update_user_stats_job_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UpdateUserStatsJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'job runs without errors' do
+    perform_enqueued_jobs do
+      UpdateUserStatsJob.perform_later
+    end
+  end
 end
