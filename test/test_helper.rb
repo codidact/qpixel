4015f064c3836e0e2e93b7700986078c6f9bdae4
Oleg Valter <o.a.philippov@gmail.com>
2026-05-20 02:14:21 +0300

    Remove '$VERBOSE' silencing as it's no longer needed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,3 @@
-# Silence the irritating already initialized constant warning
-$VERBOSE = nil
-
 require 'simplecov'
 require 'simplecov_json_formatter'
 SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
