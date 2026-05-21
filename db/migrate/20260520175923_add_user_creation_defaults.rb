112c6967d965dcc729b3a337b802e85e73283b9e
Oleg Valter <o.a.philippov@gmail.com>
2026-05-21 08:37:49 +0300

    Make add_user_creation_defaults migration reversible

--- db/migrate/20260520175923_add_user_creation_defaults.rb
+++ db/migrate/20260520175923_add_user_creation_defaults.rb
@@ -1,8 +1,8 @@
 class AddUserCreationDefaults < ActiveRecord::Migration[7.2]
   def change
-    change_column_default :users, :created_at, '2000-01-01T00:00:00.000000Z'
-    change_column_default :users, :updated_at, '2000-01-01T00:00:00.000000Z'
-    change_column_default :community_users, :created_at, '2000-01-01T00:00:00.000000Z'
-    change_column_default :community_users, :updated_at, '2000-01-01T00:00:00.000000Z'
+    change_column_default :users, :created_at, from: nil, to: '2000-01-01T00:00:00.000000Z'
+    change_column_default :users, :updated_at, from: nil, to: '2000-01-01T00:00:00.000000Z'
+    change_column_default :community_users, :created_at, from: nil, to: '2000-01-01T00:00:00.000000Z'
+    change_column_default :community_users, :updated_at, from: nil, to: '2000-01-01T00:00:00.000000Z'
   end
 end
