1ac7484b207cc96f0fe83590ec0aafade6210371
Oleg Valter <o.a.philippov@gmail.com>
2026-06-26 07:34:08 +0300

    Add test for the new DatabaseBackupJob

--- test/jobs/database_backup_job_test.rb
+++ test/jobs/database_backup_job_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class DatabaseBackupJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    AppConfig.server_settings['db_backups_path'] = backup_dirname
+    File.delete(*backup_basenames.map { |basename| backup_dirname.join(basename) })
+  end
+
+  test 'job runs successfully' do
+    perform_enqueued_jobs do
+      DatabaseBackupJob.perform_later
+    end
+    assert_performed_jobs 1
+
+    backups = backup_basenames
+
+    assert_equal 1, backups.length
+  end
+
+  private
+
+  def backup_dirname
+    Rails.root.join('tmp/storage')
+  end
+
+  def backup_basenames
+    Dir.glob('qpixel-*.sql', base: backup_dirname)
+  end
 end
