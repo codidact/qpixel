d7eab72ff1055e03a33a00f5fe646267b3392a21
Oleg Valter <o.a.philippov@gmail.com>
2026-07-11 00:11:29 +0300

    Simplify AbilityQueue#add & AbilityQueue#add! methods + add tests

--- app/models/ability_queue.rb
+++ app/models/ability_queue.rb
@@ -3,26 +3,29 @@ class AbilityQueue < ApplicationRecord
   scope :pending, -> { where(completed: false) }
 
   ##
-  # Adds a new ability queue entry for a user if one does not already exist.
-  # @param user [User] user to add queue entry for
-  # @param comment [String] comment to add to queue entry
-  # @param bang [Boolean] whether to execute the create as an ActiveRecord bang method or not (default: false)
-  def self.add(user, comment, bang: false)
-    unless AbilityQueue.pending.where(community_user: user.community_user).any?
-      if bang
-        AbilityQueue.create!(community_user: user.community_user, comment: comment, completed: false)
-      else
-        AbilityQueue.create(community_user: user.community_user, comment: comment, completed: false)
-      end
-    end
+  # Are there pending ability queue entries for a given user?
+  # @param user [User] user to check the queue for
+  # @return [Boolean] check result
+  def self.pending_for?(user)
+    AbilityQueue.pending.where(community_user: user.community_user).any?
   end
 
   ##
-  # Adds a new ability queue entry for a user if one does not already exist. Executes create as an ActiveRecord bang
-  # method.
+  # Adds a new ability queue entry for a user if one does not already exist.
+  # @param user [User] user to add queue entry for
+  # @param comment [String] comment to add to queue entry
+  def self.add(user, comment)
+    return if AbilityQueue.pending_for?(user)
+    AbilityQueue.create(community_user: user.community_user, comment: comment, completed: false)
+  end
+
+  ##
+  # Adds a new ability queue entry for a user if one does not already exist.
+  # Executes create as an ActiveRecord bang method.
   # @param user [User] user to add queue entry for
   # @param comment [String] comment to add to queue entry
   def self.add!(user, comment)
-    AbilityQueue.add(user, comment, bang: true)
+    return if AbilityQueue.pending_for?(user)
+    AbilityQueue.create!(community_user: user.community_user, comment: comment, completed: false)
   end
 end
--- test/models/ability_queue_test.rb
+++ test/models/ability_queue_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class AbilityQueueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should correctly add queue entries' do
+    flag = flags(:one)
+    std_usr = users(:standard_user)
+    comment = "Flag Handled ##{flag.id}"
+
+    valid = AbilityQueue.add(std_usr, comment)
+    assert valid.persisted?
+    assert_equal valid.comment, comment
+    assert_equal valid.community_user, std_usr.community_user
+  end
+
+  test 'should correctly add! queue entries' do
+    flag = flags(:one)
+    std_usr = users(:standard_user)
+    comment = "Flag Handled ##{flag.id}"
+
+    assert_nothing_raised do
+      AbilityQueue.add!(std_usr, comment)
+    end
+  end
 end
