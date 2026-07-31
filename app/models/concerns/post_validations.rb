6ebeec0ca95f50fa6f0a9f63dac5d99cda5c5234
trichoplax <xalpohcirt@gmail.com>
2026-07-31 07:12:48 +0100

    Normalize newlines in diff before_state and after_state

--- app/models/concerns/post_validations.rb
+++ app/models/concerns/post_validations.rb
@@ -3,8 +3,8 @@ module PostValidations
   extend ActiveSupport::Concern
 
   included do
-    normalizes :body_markdown, with: lambda { |body_markdown|
-      body_markdown.encode(body_markdown.encoding, universal_newline: true)
+    normalizes :body_markdown, with: lambda { |text|
+      text.encode(text.encoding, universal_newline: true)
     }
 
     validate :tags_in_tag_set, if: -> { post_type.has_tags }
