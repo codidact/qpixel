78cd4698d2c5d43d0ee13ed0022c69ddb570494c
trichoplax <xalpohcirt@gmail.com>
2026-08-03 01:42:41 +0100

    Fix concern to use class_methods do

--- app/models/concerns/post_normalizations.rb
+++ app/models/concerns/post_normalizations.rb
@@ -1,7 +1,13 @@
 module PostNormalizations
   extend ActiveSupport::Concern
 
-  def normalize_newlines(text)
-    text.encode(text.encoding, universal_newline: true).strip
+  included do
+    normalizes :before_state, :after_state, with: -> text { normalize_newlines(text) }
+  end
+
+  class_methods do
+    def normalize_newlines(text)
+      text.encode(text.encoding, universal_newline: true).strip
+    end
   end
 end
--- app/models/post_history.rb
+++ app/models/post_history.rb
@@ -12,9 +12,6 @@ class PostHistory < ApplicationRecord
   scope :of_type, ->(name) { joins(:post_history_type).where(post_history_types: { name: name }) }
   scope :on_undeleted, -> { joins(:post).where(posts: { deleted: false }) }
 
-  normalizes :before_state, with: :normalize_newlines
-  normalizes :after_state, with: :normalize_newlines
-
   def before_tags
     tags.where(post_history_tags: { relationship: 'before' })
   end
