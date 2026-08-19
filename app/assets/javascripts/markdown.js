bafd8106dfa3ffbd3a0ed9a82ba0fe59bfbfa93b
MoshiKoi <54333972+MoshiKoi@users.noreply.github.com>
2026-08-19 10:53:51 +0000

    Change editor controls to properly edit block markdown
    Block markdown should place the markdown symbols at the start of each line, rather than the middle.
    Blockquotes, lists, and headers fall under this category.
    
    Fixes #2051

--- app/assets/javascripts/markdown.js
+++ app/assets/javascripts/markdown.js
@@ -1,4 +1,96 @@
 $(() => {
+  /** @typedef {{apply: function(JQuery<HTMLTextAreaElement | HTMLInputElement>): void}} Action */
+
+  /** @implements {Action} */
+  class InlineAction {
+    constructor(start, end = '') {
+      this.start = start;
+      this.end = end;
+    }
+
+    /**
+     * @param {JQuery<HTMLTextAreaElement | HTMLInputElement>} $field 
+     */
+    apply($field) {
+      const preSelectionStart = $field[0].selectionStart;
+      const preSelectionEnd = $field[0].selectionEnd;
+      QPixel.MD.insertIntoField($field, this.start, this.end);
+      $field[0].selectionStart = preSelectionStart + this.start.length;
+      $field[0].selectionEnd = preSelectionEnd + this.end.length;
+    }
+  }
+
+  /** @implements {Action} */
+  class BlockAction {
+    /**
+     * @param {function(string, number): string} callback
+     * @param {boolean} skip_blanks
+     */
+    constructor(callback, skip_blanks) {
+      this.callback = callback;
+      this.skip_blanks = skip_blanks;
+    }
+
+    /**
+     * @param {JQuery<HTMLTextAreaElement | HTMLInputElement>} $field 
+     */
+    apply($field) {
+      // Override the selection range so it encompasses full lines
+      const [startPos, endPos] = this.#getBlockSelection($field);
+      $field[0].setSelectionRange(startPos, endPos);
+      const lines = $field.val().substring(startPos, endPos).split('\n');
+      let jdx = 0;
+      for (let idx = 0; idx < lines.length; ++idx) {
+        if (lines[idx] != '' || !this.skip_blanks) {
+          lines[idx] = this.callback(lines[idx], jdx);
+          ++jdx;
+        }
+      }
+      $field[0].setRangeText(lines.join('\n'));
+    }
+
+
+    /**
+     * @param {JQuery<HTMLTextAreaElement | HTMLInputElement>} $field 
+     * @returns {[number, number]} 
+     */
+    #getBlockSelection($field) {
+      const preSelectionStart = $field[0].selectionStart;
+      const preSelectionEnd = $field[0].selectionEnd;
+      const val = $field.val();
+      // Not particularly efficient, but seems to work fine
+      let startPos, endPos;
+      let idx = 0;
+      for (const line of val.split('\n')) {
+        const begin = idx;
+        const end = begin + line.length;
+        if (begin <= preSelectionStart && preSelectionStart <= end)
+          startPos = begin;
+        if (begin <= preSelectionEnd && preSelectionEnd <= end)
+          endPos = end;
+        // include newline
+        idx = end + 1;
+      }
+      return [startPos, endPos];
+    }
+  }
+
+  /**
+   * @type {{[key: string]: Action}}
+   */
+  const actions = {
+    bold: new InlineAction('**', '**'),
+    italic: new InlineAction('_', '_'),
+    code: new InlineAction('`', '`'),
+    quote: new BlockAction(l => `> ${l}`, false),
+    bullet: new BlockAction(l => `* ${l}`, true),
+    numbered: new BlockAction((l, i) => `${i + 1}. ${l}`, true),
+    heading: new BlockAction(l => `# ${l}`, true),
+    hr: new InlineAction('\n\n-----\n\n'),
+    table: new InlineAction('\n\n| Title1 | Title2 |\n|- | - |\n| row1_1 | row1_2 |\n\n'),
+    mathjax: new InlineAction('$', '$'),
+  };
+
   $(document).on('click', '.js-markdown-tool', (ev) => {
     const $tgt = $(ev.target);
     const $button = $tgt.is('a') ? $tgt : $tgt.parents('a');
@@ -7,25 +99,9 @@ $(() => {
     /** @type {JQuery<HTMLTextAreaElement | HTMLInputElement>} */
     const $field = $('.js-post-field');
 
-    const actions = {
-      bold: ['**', '**'],
-      italic: ['_', '_'],
-      code: ['`', '`'],
-      quote: ['\n > ', null],
-      bullet: ['\n * ', null],
-      numbered: ['\n 1. ', null],
-      heading: ['\n# ', null],
-      hr: ['\n\n-----\n\n', null],
-      table: ['\n\n| Title1 | Title2 |\n|- | - |\n| row1_1 | row1_2 |\n\n', null],
-      mathjax: ['$', '$'],
-    };
-
-    if (Object.keys(actions).indexOf(action) !== -1) {
-      const preSelection = [$field[0].selectionStart, $field[0].selectionEnd];
-      QPixel.MD.insertIntoField($field, actions[action][0], actions[action][1]);
-      $field.focus();
-      $field[0].selectionStart = preSelection[0] + actions[action][0].length;
-      $field[0].selectionEnd = preSelection[1] + actions[action][0].length;
+    if (action && action in actions) {
+      actions[action].apply($field);
+      $field.trigger("focus");
     }
   });
 
