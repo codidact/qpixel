66a1e934793cb11d70e30b6bf81bd2fd2873383c
MoshiKoi <54333972+MoshiKoi@users.noreply.github.com>
2026-08-21 06:41:18 +0000

    Make the dropdown and dashboard follow the user's preferred order

--- app/models/community.rb
+++ app/models/community.rb
@@ -5,4 +5,16 @@ class Community < ApplicationRecord
   default_scope { where(is_fake: false) }
 
   validates :host, uniqueness: { case_sensitive: false }
+
+  scope :user_preferred_order, lambda { |user|
+    if user.nil?
+      all
+    else
+      user_order = user.preference('community_order', community: false)
+                       .split(',')
+                       .map(&:to_i)
+
+      in_order_of(:id, user_order, filter: false)
+    end
+  }
 end
