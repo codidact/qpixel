f3975259f6c0d1d11e6b9ac5b3eff997f113ae5a
MoshiKoi <54333972+MoshiKoi@users.noreply.github.com>
2026-08-21 11:16:22 +0000

    Add buttons to reorder communities without dragging

--- app/assets/javascripts/community_order.js
+++ app/assets/javascripts/community_order.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   const pref = /** @type {HTMLElement} */(document.querySelector(".community-sortable"));
-  const sortable = /** @type {HTMLElement} */(pref.querySelector(".sortable"));
+  const el = /** @type {HTMLElement} */(pref.querySelector(".sortable"));
   const input = /** @type {HTMLInputElement} */(pref.querySelector(".js-user-pref"));
 
-  Sortable.create(sortable, {
-    onSort: () => {
-      const val = [...sortable.children]
-        .map(el => /** @type {HTMLElement} */(el).dataset['community'])
-        .join(',');
-
-      input.value = val;
-      input.dispatchEvent(new Event("change"));
+  const sortable = Sortable.create(el, {
+    store: {
+      get: () => input.value.split(','),
+      set: self => {
+        input.value = self.toArray().join(',');
+        input.dispatchEvent(new Event("change"));
+      }
     }
   });
+
+  document.querySelectorAll('.community-sortable-list-item').forEach(el => {
+    el.querySelector('.sort-up-btn')?.addEventListener('click', _ => {
+      el.previousElementSibling?.insertAdjacentElement('beforebegin', el);
+      sortable.save();
+    });
+    el.querySelector('.sort-down-btn')?.addEventListener('click', _ => {
+      el.nextElementSibling?.insertAdjacentElement('afterend', el);
+      sortable.save();
+    });
+  })
 });
