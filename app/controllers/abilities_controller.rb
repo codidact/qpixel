6a50a7b30aa3ebcc32bc4a8808cc5ec763fe22ce
Oleg Valter <o.a.philippov@gmail.com>
2026-09-18 22:00:29 +0300

    Simplify require_authenticate_user? & and its usage

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -1,18 +1,10 @@
 class AbilitiesController < ApplicationController
   include DraftManagement
 
-  before_action :authenticate_user!, if: ->(c) { require_authenticate_user?(c) }
+  before_action :authenticate_user!, if: :require_authenticate_user?
   before_action :set_ability, only: [:show, :edit, :update]
   before_action :set_user
   before_action :verify_moderator, only: [:edit, :recalc, :update]
-
-  def require_authenticate_user?(callback)
-    if callback.action_name == 'show'
-      return SiteSetting['RequireSignInToViewUserAbilities'] && params[:for].present?
-    end
-
-    callback.action_name != 'index'
-  end
 
   def index
     @abilities = Ability.all
@@ -82,6 +74,16 @@ class AbilitiesController < ApplicationController
     return false unless current_user.present?
 
     current_user.can_push_to_network?(ability)
+  end
+
+  # Should the user be required to authenticate?
+  # @return [Boolean] check result
+  def require_authenticate_user?
+    if action_name == 'show'
+      return SiteSetting['RequireSignInToViewUserAbilities'] && params[:for].present?
+    end
+
+    action_name != 'index'
   end
 
   def set_ability
